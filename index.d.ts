8140fc386c152bdf41bae47d4903a7782fa34285
Lucas Garron <code@garron.net>
2018-09-19 01:04:57 -0700

    Add TODO for issue #1.

--- index.d.ts
+++ index.d.ts
@@ -8,4 +8,6 @@ export {alg as alg};
 export {kpuzzle as kpuzzle};
 export {twisty as twisty};
 export {cuble as cuble};
+// TODO: Export `puzzle-geometry` as `puzzle-geometry`, if at all possible.
+// https://github.com/cubing/cubing.js/issues/1
 export {puzzleGeometry as puzzleGeometry};
