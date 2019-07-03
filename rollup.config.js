859c2bcfafeb2fda2301594b4b86ed31f8179bb3
Lucas Garron <code@garron.net>
2019-07-03 10:17:59 -0700

    FREEZE.unindexed

--- rollup.config.js
+++ rollup.config.js
@@ -23,40 +23,46 @@ if (!process.env.ROLLUP_WATCH) {
   plugins.push(terser())
 }
 
-export default [
-  {
-    input: {
-      "alg": "src/alg/index.ts",
-      "bluetooth": "src/bluetooth/index.ts",
-      "cubing": "src/cubing/index.ts",
-      "kpuzzle": "src/kpuzzle/index.ts",
-      "puzzle-geometry": "src/puzzle-geometry/index.ts",
-      "twisty": "src/twisty/index.ts"
-    },
-    output: [
-      {
-        dir: "dist/esm",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    external: ["three"],
-    plugins
+const esm = {
+  input: {
+    "alg": "src/alg/index.ts",
+    "bluetooth": "src/bluetooth/index.ts",
+    "cubing": "src/cubing/index.ts",
+    "kpuzzle": "src/kpuzzle/index.ts",
+    "puzzle-geometry": "src/puzzle-geometry/index.ts",
+    "twisty": "src/twisty/index.ts"
   },
-  {
-    input: "src/cubing/index.ts",
-    output: [
-      {
-        file: "dist/umd/cubing.umd.js",
-        format: "umd",
-        name: "cubing"
-      }
-    ],
-    plugins: [
-      ...plugins,
-      resolve({
-        only: ["three"]
-      })
-    ]
-  }
-]
+  output: [
+    {
+      dir: "dist/esm",
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  external: ["three"],
+  plugins
+}
+
+const umd = {
+  input: "src/cubing/index.ts",
+  output: [
+    {
+      file: "dist/umd/cubing.umd.js",
+      format: "umd",
+      name: "cubing"
+    }
+  ],
+  plugins: [
+    ...plugins,
+    resolve({
+      only: ["three"]
+    })
+  ]
+}
+
+const configs = [esm]
+if (!process.env.ROLLUP_WATCH) {
+  configs.push(umd)
+}
+
+export default configs;
--- src/alg/parser-source/parser-source.pegjs
+++ src/alg/parser-source/parser-source.pegjs
@@ -6,3 +6,25 @@ top_level_alg
 
 family
     = family:[A-Z]+ { return {type: "sequence", nestedUnits: [{type: "blockMove", family: "R", amount: 1}], amount: 1}; }
+
+// WHITESPACE = [^\S\r\n]+
+NUMBER = [0-9]+
+DASH = "-"
+
+LONG_FAMILY = [_A-Za-z]+
+
+
+PRIME = "'"
+PAUSE = "."
+
+
+COMMENT_SHORT = "//"[^\n\r]*
+COMMENT_LONG = "/\*[^\*]*\*/"
+NEWLINE = [\n\r]
+
+OPEN_BRACKET = "["
+CLOSE_BRACKET = "]"
+OPEN_PARENTHESIS = "("
+CLOSE_PARENTHESIS = ")"
+COMMA = ","
+COLON = ":"
