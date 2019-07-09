b361c33cc4fdd3dccb53900f4a66504174b29fb8
Lucas Garron <code@garron.net>
2019-07-09 15:28:35 +1000

    Add puzzle-geometry (Quat) test.

---
+++ src/puzzle-geometry/Quat.spec.ts
@@ -0,0 +1,10 @@
+import {Quat} from "./Quat"
+
+describe("Quat", () => {
+  it("should multiply", () => {
+    const a = new Quat(3, 1, 4, 1);
+    const b = new Quat(5, 9, 2, 6);
+    const c = a.mul(b) ;
+    expect(c.dist(new Quat(-8, 54, 29, -11))).toBe(0);
+  });
+});
