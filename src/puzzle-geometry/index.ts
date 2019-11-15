b3677e381f8d08f61c8070296b5e1fcc829b347c
Lucas Garron <code@garron.net>
2019-11-15 12:38:27 -0800

    Run `puzzle-geometry` files through formatter.

--- src/puzzle-geometry/index.ts
+++ src/puzzle-geometry/index.ts
@@ -1,18 +1,6 @@
-export {
-  Quat,
-} from "./Quat";
-export {
-   PlatonicGenerator,
-} from "./PlatonicGenerator";
-export {
-   PuzzleGeometry,
-} from "./PuzzleGeometry";
-export {
-   Perm,
-} from "./Perm";
-export {
-   OrbitDef, OrbitsDef, Orbit, Transformation, VisibleState,
-} from "./PermOriSet";
-export {
-   SchreierSims,
-} from "./SchreierSims" ;
+export { Perm } from "./Perm";
+export { Orbit, OrbitDef, OrbitsDef, Transformation, VisibleState } from "./PermOriSet";
+export { PlatonicGenerator } from "./PlatonicGenerator";
+export { PuzzleGeometry } from "./PuzzleGeometry";
+export { Quat } from "./Quat";
+export { SchreierSims } from "./SchreierSims";
