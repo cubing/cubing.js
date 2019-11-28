69354afbe263780d937eb4ae72faf0b72e25520d
Lucas Garron <code@garron.net>
2019-11-27 16:36:41 -0800

    [vr] Change line attribute setter per `three.js` deprecation.

--- app/vr/vr-input.ts
+++ app/vr/vr-input.ts
@@ -19,11 +19,11 @@ export enum OculusButton {
 export const controllerDirection: Vector3 = new Vector3(0, 0, -1);
 
 const geometry = new BufferGeometry();
-geometry.addAttribute("position", new Float32BufferAttribute([
+geometry.setAttribute("position", new Float32BufferAttribute([
   0, 0, 0,
   controllerDirection.x, controllerDirection.y, controllerDirection.z,
 ], 3));
-geometry.addAttribute("color", new Float32BufferAttribute([
+geometry.setAttribute("color", new Float32BufferAttribute([
   0.5, 0.5, 0.5,
   0, 0, 0,
 ], 3));
