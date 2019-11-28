222b4baf06c6a133aa20fc907ba4ff1234cbb8c5
Lucas Garron <code@garron.net>
2019-11-28 05:04:03 -0800

    [puzzle-geometry] Fix exports.

--- src/puzzle-geometry/index.ts
+++ src/puzzle-geometry/index.ts
@@ -1,6 +1,5 @@
 export { Perm } from "./Perm";
 export { Orbit, OrbitDef, OrbitsDef, Transformation, VisibleState } from "./PermOriSet";
-export { PlatonicGenerator } from "./PlatonicGenerator";
 export { getpuzzle, getPuzzleGeometryByDesc, getPuzzleGeometryByName, getpuzzles, parsedesc, PuzzleGeometry } from "./PuzzleGeometry";
 export { Quat } from "./Quat";
-export { SchreierSims } from "./SchreierSims";
+export { schreierSims } from "./SchreierSims";
