642d77ee673cfeea9555bb8e5bbf2b0f5af7165c
Lucas Garron <code@garron.net>
2019-11-27 17:03:28 -0800

    [vr] Remove a `console.log()`.

--- app/vr/vr-pg3d.ts
+++ app/vr/vr-pg3d.ts
@@ -98,7 +98,6 @@ export class VRPG3D {
     // TODO: Better abstraction over controllers.
     // this.vrInput.addSingleButtonListener({ controllerIdx: 1, buttonIdx: OculusButton.Grip }, this.gripStart.bind(this, 1), this.gripContinued.bind(this, 1));
 
-    console.log({ daydream });
     // this.vrInput.addSingleButtonListener({ controllerIdx: 0, buttonIdx: daydream ? 0 : OculusButton.Trigger }, this.onPress.bind(this, 0));
     // this.vrInput.addSingleButtonListener({ controllerIdx: 1, buttonIdx: daydream ? 0 : OculusButton.Trigger }, this.onPress.bind(this, 1));
     // TODO: Generalize this to multiple platforms.
