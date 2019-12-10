a352ae484256b3b590f4cf01bce4bcac5c0a7244
Lucas Garron <code@garron.net>
2019-12-09 19:39:48 -0800

    [bluetooth] Fall back to `acceptAllDevices()` after two consecutive connection failures.

--- src/bluetooth/connect.ts
+++ src/bluetooth/connect.ts
@@ -16,9 +16,9 @@ function requestOptions(acceptAllDevices: boolean = false): RequestDeviceOptions
     acceptAllDevices: true,
     optionalServices: [] as BluetoothServiceUUID[],
   } : {
-    filters: [] as BluetoothRequestDeviceFilter[],
-    optionalServices: [] as BluetoothServiceUUID[],
-  };
+      filters: [] as BluetoothRequestDeviceFilter[],
+      optionalServices: [] as BluetoothServiceUUID[],
+    };
   for (const config of [
     ganConfig,
     giiKERConfig,
@@ -39,10 +39,29 @@ interface BluetoothConnectOptions {
   acceptAllDevices?: boolean;
 }
 
+// We globally track the number of connection failures,
+// in order to offer the user recourse (accept all devices) if they're having issues.
+// This allows us to future-proof situations where a device might not show up in
+// the chooser, but works if we connect.
+let consecutiveFailures = 0;
+const MAX_FAILURES_BEFORE_ACCEPT_ALL_FALLBACK = 2;
+
 // TODO: Debug options to allow connecting to any device?
 export async function connect(options: BluetoothConnectOptions = {}): Promise<BluetoothPuzzle> {
   debugLog("Attempting to pair.");
-  const device = await navigator.bluetooth.requestDevice(requestOptions(options.acceptAllDevices));
+  let device;
+  try {
+    let acceptAllDevices = options.acceptAllDevices;
+    if (!acceptAllDevices && consecutiveFailures >= MAX_FAILURES_BEFORE_ACCEPT_ALL_FALLBACK) {
+      console.info(`The last ${MAX_FAILURES_BEFORE_ACCEPT_ALL_FALLBACK} Bluetooth puzzle connection attempts failed. This time, the Bluetooth prompt will show all possible devices.`);
+      acceptAllDevices = true;
+    }
+    device = await navigator.bluetooth.requestDevice(requestOptions(acceptAllDevices));
+    consecutiveFailures = 0;
+  } catch (e) {
+    consecutiveFailures++;
+    throw new Error(e);
+  }
   debugLog("Device:", device);
 
   if (typeof device.gatt === "undefined") {
