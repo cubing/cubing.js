62a943717246fe3db429fde13e236431e0c1b44f
Lucas Garron <code@garron.net>
2020-06-01 22:57:07 -0700

    [test] Transpile `three` modules files during test.

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
     "^.+\\.pegjs$": "pegjs-jest",
     "\\.svg$": "jest-raw-loader",
   },
+  // By default, Jest doesn't transpile deps. But we're importing some module
+  // deps, so we define the ignore pattern to un-ignore those.
+  transformIgnorePatterns: ["node_modules/(?!(three/examples/jsm)/)"],
 };
