94f4255272d13acd339ebc87f3e6679b48d7c621
rokicki <rokicki@gmail.com>
2020-07-18 09:37:39 -0700

    [pg3d-orientation] Fix a typo in the test app.

--- app/twisty/pg3d-orientation.ts
+++ app/twisty/pg3d-orientation.ts
@@ -4,19 +4,20 @@ import { TwistyPlayer } from "../../src/twisty";
 import { parse } from "../../src/alg/";
 import { Vector3 } from "three";
 
-const pg = getPuzzleGeometryByName("4x4x4", ["orientcenters", "true"]);
+const pg = getPuzzleGeometryByName("megaminx", ["orientcenters", "true"]);
 const stickerDat = pg.get3d(0.0131);
 
 const kpuzzle = kpuzzleParse(pg.writeksolve("TwizzlePuzzle", true));
 
-const cameraPosition = new Vector3(1.25, 2.5, 2.5);
+// const cameraPosition = new Vector3(1.25, 2.5, 2.5);
+const cameraPosition = new Vector3(0, 0, 4);
 const cameraLookAt = new Vector3(0, 0, 0);
 
 const cube3DPlayer = new TwistyPlayer();
 document.body.appendChild(cube3DPlayer);
 
 const pg3dPlayer = new TwistyPlayer({
-  alg: parse("[[U', R], [U, R']]"),
+  alg: parse("U F R"),
   puzzle: kpuzzle,
   playerConfig: {
     visualizationFormat: "PG3D",
@@ -49,5 +50,5 @@ setTimeout(() => {
     .experimentalGetPlayer()
     .pg3DView.experimentalGetPG3D()
     .experimentalGetVantages()[0]
-    .camera.position.copy(cameraLookAt);
+    .camera.lookAt(cameraLookAt);
 }, 100);
