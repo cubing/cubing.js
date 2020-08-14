19187319320fdd9dcc6f4c151b8b6731c5e5358f
Lucas Garron <code@garron.net>
2020-08-13 18:41:33 -0700

    [twisty] Remove old commented draft in `Twisty3DPuzzle`.

--- src/twisty/3D/puzzles/Twisty3DPuzzle.ts
+++ src/twisty/3D/puzzles/Twisty3DPuzzle.ts
@@ -4,19 +4,3 @@ import { PuzzlePosition } from "../../animation/alg/CursorTypes";
 export interface Twisty3DPuzzle extends Object3D {
   onPositionChange(position: PuzzlePosition): void;
 }
-
-// export abstract class Twisty3DPuzzle extends Object3D
-//   implements PositionListener {
-//   constructor(
-//     protected twisty3DScene: Twisty3DScene,
-//     cursor: PositionDispatcher,
-//   ) {
-//     super();
-//     /*...*/
-//     cursor.addPositionListener(this);
-//   }
-
-//   onPositionChange(_position: PuzzlePosition): void {
-//     this.twisty3DScene.scheduleRender();
-//   }
-// }
