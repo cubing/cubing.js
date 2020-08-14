cd5722ba350d8660c7d95e1672e28154a80bb3aa
Lucas Garron <code@garron.net>
2020-08-14 02:31:17 -0700

    [protocol] Export `twizzleBinaryToReid3x3x3`.

--- src/protocol/index.ts
+++ src/protocol/index.ts
@@ -1,2 +1,5 @@
-export { reid3x3x3ToTwizzleBinary } from "./binary/binary3x3x3";
+export {
+  reid3x3x3ToTwizzleBinary,
+  twizzleBinaryToReid3x3x3,
+} from "./binary/binary3x3x3";
 export { bufferToSpacedHex, spacedHexToBuffer } from "./binary/hex";
