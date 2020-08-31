5c74d1bdaeb586b50d0575110bfd8f8470d3d106
Lucas Garron <code@garron.net>
2020-08-30 20:40:45 -0700

    [twisty] Use exact golden ratio for default camera position.

--- src/twisty/dom/TwistyPlayerConfig.ts
+++ src/twisty/dom/TwistyPlayerConfig.ts
@@ -7,6 +7,10 @@ import {
 } from "./element/ElementConfig";
 import { BackViewLayout, backViewLayouts } from "./viewers/TwistyViewerWrapper";
 
+const DEFAULT_CAMERA_Z = 5;
+// Golden ratio is perfect for FTO and Megaminx.
+const DEFAULT_CAMERA_Y = DEFAULT_CAMERA_Z * (2 / (1 + Math.sqrt(5)));
+
 // TODO: turn these maps into lists?
 export const visualizationFormats = {
   "3D": true, // default
@@ -129,7 +133,7 @@ export class TwistyPlayerConfig {
         initialValues["backView"],
       ),
       "camera-position": new Vector3Attribute(
-        new Vector3(0, 3.1, 5),
+        new Vector3(0, DEFAULT_CAMERA_Y, DEFAULT_CAMERA_Z),
         initialValues["cameraPosition"],
       ),
     };
