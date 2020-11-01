18bbb65dc9ffd6fb38aaab1aebd3e6d961ceba49
Lucas Garron <code@garron.net>
2020-10-31 21:21:02 -0700

    [app] Add algs to all stickering demos.

--- app/twisty/cube3d-stickering.ts
+++ app/twisty/cube3d-stickering.ts
@@ -1,4 +1,4 @@
-import { parse } from "../../src/alg";
+import { invert, parse } from "../../src/alg";
 import {
   Cube3D,
   experimentalSetShareAllNewRenderers,
@@ -14,6 +14,7 @@ function addAlg(stickering: ExperimentalStickering, s: string): Cube3D {
   const div = content.appendChild(document.createElement("div"));
   div.classList.add("case");
   const twistyPlayer = new TwistyPlayer({
+    experimentalStartSetup: invert(parse(s)),
     alg: parse(s),
     experimentalStickering: stickering,
   });
@@ -22,25 +23,25 @@ function addAlg(stickering: ExperimentalStickering, s: string): Cube3D {
   return twistyPlayer.twisty3D as Cube3D;
 }
 
-addAlg("full", "");
-addAlg("centers-only", "");
+addAlg("full", "y L' U R' F' U L2 U2' L' U' L U2' D R' D' F2 R2 U'");
+addAlg("centers-only", "(x y)3");
 addAlg("PLL", "R U R' U' R' F R2 U' R' U' R U R' F'");
 addAlg("CLS", "R U R' U' R U R' U R U' R'");
 addAlg("OLL", "r U R' U R U2 r'");
 addAlg("ELS", "[r U' r': U]");
 addAlg("LL", "R' F R F2' U F R U R' F' U' F");
-addAlg("F2L", "");
+addAlg("F2L", "R2' u R2 u' R2'");
 addAlg("ZBLL", "R' F R U' R' U' R U R' F' R U R' U' R' F R F' R");
 addAlg("ZBLS", "x' R2 U' R' U l'");
 addAlg("WVLS", "R U R' U R U' R'");
 addAlg("VLS", "R' F2 R F2' L' U2 L");
-addAlg("LS", "R U' R'");
-addAlg("EO", "");
-addAlg("CMLL", "");
-addAlg("L6E", "");
+addAlg("LS", "U' R U' R' U R U R'");
+addAlg("EO", "R' F R");
+addAlg("CMLL", "F R U R' U' F'");
+addAlg("L6E", "U M2' U' M2'");
 addAlg("L6EO", "(U' M U' M')2");
 addAlg("Daisy", "S2 R2 L2");
 addAlg("Cross", "");
-addAlg("2x2x2", "");
-addAlg("2x2x3", "");
+addAlg("2x2x2", "y2");
+addAlg("2x2x3", "y");
 addAlg("Void Cube", "");
