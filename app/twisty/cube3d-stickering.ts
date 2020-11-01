d3d59b67f0558f8740450dadfdb0ec7a5f7b3566
Lucas Garron <code@garron.net>
2020-10-31 20:42:24 -0700

    [app] Update stickering demo.

--- app/twisty/cube3d-stickering.ts
+++ app/twisty/cube3d-stickering.ts
@@ -22,18 +22,25 @@ function addAlg(stickering: ExperimentalStickering, s: string): Cube3D {
   return twistyPlayer.twisty3D as Cube3D;
 }
 
+addAlg("full", "");
+addAlg("centers-only", "");
 addAlg("PLL", "R U R' U' R' F R2 U' R' U' R U R' F'");
 addAlg("CLS", "R U R' U' R U R' U R U' R'");
 addAlg("OLL", "r U R' U R U2 r'");
 addAlg("ELS", "[r U' r': U]");
 addAlg("LL", "R' F R F2' U F R U R' F' U' F");
+addAlg("F2L", "");
 addAlg("ZBLL", "R' F R U' R' U' R U R' F' R U R' U' R' F R F' R");
 addAlg("ZBLS", "x' R2 U' R' U l'");
 addAlg("WVLS", "R U R' U R U' R'");
 addAlg("VLS", "R' F2 R F2' L' U2 L");
 addAlg("LS", "R U' R'");
 addAlg("EO", "");
+addAlg("CMLL", "");
+addAlg("L6E", "");
 addAlg("L6EO", "(U' M U' M')2");
 addAlg("Daisy", "S2 R2 L2");
-addAlg("2x2x2", "y2'");
-addAlg("2x2x3", "y'");
+addAlg("Cross", "");
+addAlg("2x2x2", "");
+addAlg("2x2x3", "");
+addAlg("Void Cube", "");
