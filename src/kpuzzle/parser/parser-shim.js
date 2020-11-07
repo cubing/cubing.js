f1b6eba24e896d1d2e753a4cb0e026bac99e74b2
Lucas Garron <code@garron.net>
2020-11-07 01:34:40 -0800

    Simplify parser shims.

--- src/kpuzzle/parser/parser-shim.js
+++ src/kpuzzle/parser/parser-shim.js
@@ -1,4 +1,2 @@
 // Workaround per https://github.com/pegjs/pegjs/pull/481
-import parser from "./parser-pegjs";
-const { parse } = parser;
-export { parse };
+export { parse } from "./parser-pegjs";
