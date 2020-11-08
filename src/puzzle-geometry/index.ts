94158322773ae730d38f6ae1dafad30868a594fc
Lucas Garron <code@garron.net>
2020-11-07 20:22:58 -0800

    Remove `regenerator-runtime`.

--- src/puzzle-geometry/index.ts
+++ src/puzzle-geometry/index.ts
@@ -1,5 +1,3 @@
-import "regenerator-runtime/runtime"; // Prevent `regeneratorRuntime is not defined` error. https://github.com/babel/babel/issues/5085
-
 export { Perm } from "./Perm";
 export {
   Orbit,
--- src/stream/index.ts
+++ src/stream/index.ts
@@ -1,5 +1,3 @@
-import "regenerator-runtime/runtime"; // Prevent `regeneratorRuntime is not defined` error. https://github.com/babel/babel/issues/5085
-
 export {
   ProxyEvent,
   ProxyMoveEvent,
