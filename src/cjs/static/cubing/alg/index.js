e834824611f8f70c9a98aea5db755ee0f0a4e6d2
Lucas Garron <code@garron.net>
2020-11-08 00:00:45 -0800

    Exclude files from linter.

--- src/cjs/static/cubing/alg/index.js
+++ src/cjs/static/cubing/alg/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cubing = require("../../../../gen/cjs/index");
 
 module.exports = cubing.alg;
--- src/cjs/static/cubing/puzzle-geometry/index.js
+++ src/cjs/static/cubing/puzzle-geometry/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cubing = require("../index");
 
 module.exports = cubing.puzzleGeometry;
