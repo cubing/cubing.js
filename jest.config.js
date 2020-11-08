fe7bf283ab1d1ef24b7b6e901f0b2243b0f798f9
Lucas Garron <code@garron.net>
2020-11-08 04:22:11 -0800

    [jest] Remove uninstalled plugins from config.

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ export default {
   transform: {
     "^.+\\.js$": "babel-jest",
     "^.+\\.ts$": "ts-jest",
-    "^.+\\.pegjs$": "pegjs-jest",
-    "\\.svg$": "jest-raw-loader",
   },
   // By default, Jest doesn't transpile deps. But we're importing some module
   // deps, so we define the ignore pattern to un-ignore those.
