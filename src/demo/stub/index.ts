22c571cf543fb82d1edb95ad8fedb99b8b756f15
Lucas Garron <code@garron.net>
2020-11-23 01:04:29 -0800

    Restore stub.

--- src/demo/stub/index.ts
+++ src/demo/stub/index.ts
@@ -1,9 +1,2 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
-import { TwistyPlayer } from "../../cubing/twisty";
-
-const player = new TwistyPlayer({ puzzle: "3x3x3", visualization: "PG3D" });
-document.body.appendChild(player);
-// player.backView = "side-by-side";
-// player.puzzle = "clock";
