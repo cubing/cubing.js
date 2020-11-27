93893fdc23c263af46701b22c0f762cf35148773
Lucas Garron <code@garron.net>
2020-11-27 09:09:37 -0800

    /Users/lgarron/Code/cubing.js/src/cubing/twisty/3D/Twisty3DScene.ts

--- src/cubing/twisty/3D/Twisty3DScene.ts
+++ src/cubing/twisty/3D/Twisty3DScene.ts
@@ -1,6 +1,6 @@
-import { Scene as ThreeScene, PointLight } from "three";
-import { Twisty3DRenderTarget } from "./Twisty3DRenderTarget";
+import { Scene as ThreeScene } from "three";
 import { Twisty3DPuzzle } from "./puzzles/Twisty3DPuzzle";
+import { Twisty3DRenderTarget } from "./Twisty3DRenderTarget";
 
 export class Twisty3DScene extends ThreeScene implements Twisty3DRenderTarget {
   private renderTargets: Set<Twisty3DRenderTarget> = new Set();
@@ -8,19 +8,6 @@ export class Twisty3DScene extends ThreeScene implements Twisty3DRenderTarget {
 
   constructor() {
     super();
-
-    const lights = [];
-    lights[0] = new PointLight(0xffffff, 1, 0);
-    lights[1] = new PointLight(0xffffff, 1, 0);
-    lights[2] = new PointLight(0xffffff, 1, 0);
-
-    lights[0].position.set(0, 200, 0);
-    lights[1].position.set(100, 200, 100);
-    lights[2].position.set(-100, -200, -100);
-
-    this.add(lights[0]);
-    this.add(lights[1]);
-    this.add(lights[2]);
   }
 
   addRenderTarget(renderTarget: Twisty3DRenderTarget): void {
