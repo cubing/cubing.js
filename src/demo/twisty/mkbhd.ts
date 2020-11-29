339edd835fd3c111da3943491e910064d1bbda2b
Lucas Garron <code@garron.net>
2020-11-29 01:50:54 -0800

    [demo] Split up shared picture demo code again.
    The shared code was breaking in the production build.

--- src/demo/twisty/mkbhd.ts
+++ src/demo/twisty/mkbhd.ts
@@ -8,7 +8,18 @@ import mkbhdHintSpriteURL from "url:./mkbhd-sprite-red-hint.png";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import mkbhdSpriteURL from "url:./mkbhd-sprite-red.png";
-import { cubeDemo } from "./picture-cube-demo";
+import { parseAlg } from "../../cubing/alg";
+import {
+  connect,
+  debugKeyboardConnect,
+  MoveEvent,
+} from "../../cubing/bluetooth";
+import {
+  Cube3D,
+  TimelineActionEvent,
+  TimestampLocationType,
+  TwistyPlayer,
+} from "../../cubing/twisty";
 
 (window as any).m12Cube = m12Cube;
 
@@ -18,4 +29,77 @@ const spriteURL =
 const hintSpriteURL =
   new URL(location.href).searchParams.get("hint-sprite") ?? mkbhdHintSpriteURL;
 
-cubeDemo(spriteURL, hintSpriteURL);
+// Common picture cube demo code.
+
+let haveHadMoveInput = false;
+
+const twistyPlayer = document.querySelector("twisty-player")! as TwistyPlayer;
+twistyPlayer.timeline.jumpToStart();
+twistyPlayer.timeline.play(); // TODO: add autoplay
+twistyPlayer.timeline.addActionListener({
+  onTimelineAction: (actionEvent: TimelineActionEvent) => {
+    if (haveHadMoveInput) {
+      return;
+    }
+    if (actionEvent.locationType === TimestampLocationType.EndOfTimeline) {
+      twistyPlayer.timeline.jumpToStart();
+      twistyPlayer.timeline.play();
+    }
+  },
+});
+
+(async () => {
+  const kb = await debugKeyboardConnect();
+  kb.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = parseAlg("");
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+})();
+
+let lastTimestamp = performance.now();
+const ROTATION_RATE = (2 * Math.PI) / 15;
+let haveTriedToSetSpriteURL = false;
+function rotate() {
+  if (twistyPlayer.twisty3D && !haveTriedToSetSpriteURL) {
+    haveTriedToSetSpriteURL = true;
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetStickerSpriteURL(
+      spriteURL,
+    );
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetHintStickerSpriteURL(
+      hintSpriteURL,
+    );
+  }
+
+  const newTimestamp = performance.now();
+  twistyPlayer.twisty3D?.rotateY(
+    ((newTimestamp - lastTimestamp) / 1000) * ROTATION_RATE,
+  );
+  if (
+    !(twistyPlayer.viewerElems[0] as any)?.orbitControls
+      .experimentalHasBeenMoved
+  ) {
+    requestAnimationFrame(rotate);
+    lastTimestamp = newTimestamp;
+  }
+}
+requestAnimationFrame(rotate);
+
+async function connectBluetooth(): Promise<void> {
+  const bluetoothPuzzle = await connect();
+  document.body.removeEventListener("click", connectBluetooth);
+  bluetoothPuzzle.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = parseAlg("");
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+}
+if (new URL(location.href).searchParams.get("bluetooth") === "true") {
+  document.body.addEventListener("click", connectBluetooth);
+}
--- src/demo/twisty/supercube.ts
+++ src/demo/twisty/supercube.ts
@@ -2,9 +2,18 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import supercubeSprite from "url:./supercube-sprite.png";
-import { cubeDemo } from "./picture-cube-demo";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { parseAlg } from "../../cubing/alg";
+import {
+  connect,
+  debugKeyboardConnect,
+  MoveEvent,
+} from "../../cubing/bluetooth";
+import {
+  Cube3D,
+  TimelineActionEvent,
+  TimestampLocationType,
+  TwistyPlayer,
+} from "../../cubing/twisty";
 
 const spriteURL =
   new URL(location.href).searchParams.get("sprite") ?? supercubeSprite;
@@ -12,4 +21,77 @@ const spriteURL =
 const hintSpriteURL =
   new URL(location.href).searchParams.get("hint-sprite") ?? "";
 
-cubeDemo(spriteURL, hintSpriteURL);
+// Common picture cube demo code.
+
+let haveHadMoveInput = false;
+
+const twistyPlayer = document.querySelector("twisty-player")! as TwistyPlayer;
+twistyPlayer.timeline.jumpToStart();
+twistyPlayer.timeline.play(); // TODO: add autoplay
+twistyPlayer.timeline.addActionListener({
+  onTimelineAction: (actionEvent: TimelineActionEvent) => {
+    if (haveHadMoveInput) {
+      return;
+    }
+    if (actionEvent.locationType === TimestampLocationType.EndOfTimeline) {
+      twistyPlayer.timeline.jumpToStart();
+      twistyPlayer.timeline.play();
+    }
+  },
+});
+
+(async () => {
+  const kb = await debugKeyboardConnect();
+  kb.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = parseAlg("");
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+})();
+
+let lastTimestamp = performance.now();
+const ROTATION_RATE = (2 * Math.PI) / 15;
+let haveTriedToSetSpriteURL = false;
+function rotate() {
+  if (twistyPlayer.twisty3D && !haveTriedToSetSpriteURL) {
+    haveTriedToSetSpriteURL = true;
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetStickerSpriteURL(
+      spriteURL,
+    );
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetHintStickerSpriteURL(
+      hintSpriteURL,
+    );
+  }
+
+  const newTimestamp = performance.now();
+  twistyPlayer.twisty3D?.rotateY(
+    ((newTimestamp - lastTimestamp) / 1000) * ROTATION_RATE,
+  );
+  if (
+    !(twistyPlayer.viewerElems[0] as any)?.orbitControls
+      .experimentalHasBeenMoved
+  ) {
+    requestAnimationFrame(rotate);
+    lastTimestamp = newTimestamp;
+  }
+}
+requestAnimationFrame(rotate);
+
+async function connectBluetooth(): Promise<void> {
+  const bluetoothPuzzle = await connect();
+  document.body.removeEventListener("click", connectBluetooth);
+  bluetoothPuzzle.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = parseAlg("");
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+}
+if (new URL(location.href).searchParams.get("bluetooth") === "true") {
+  document.body.addEventListener("click", connectBluetooth);
+}
