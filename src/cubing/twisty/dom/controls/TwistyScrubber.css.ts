4e50e9cf3030fcd5eb179aeb52c2fa0cbf2a1880
Lucas Garron <code@garron.net>
2021-01-10 16:26:59 -0800

    [twisty] Improve slider styling.

--- src/cubing/twisty/dom/controls/TwistyScrubber.css.ts
+++ src/cubing/twisty/dom/controls/TwistyScrubber.css.ts
@@ -11,4 +11,30 @@ export const twistyScrubberCSS = new CSSSource(`
 input {
   margin: 0; width: 100%;
 }
+
+input {
+  background: none;
+}
+
+::-moz-range-track {
+  background: rgba(0, 0, 0, 0.25);
+  height: 50%;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+::-webkit-slider-runnable-track {
+  background: rgba(0, 0, 0, 0.05);
+}
+
+::-moz-range-progress {
+  background: #3273F6;
+  height: 50%;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+::-ms-fill-lower {
+  background: #3273F6;
+  height: 50%;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
 `);
