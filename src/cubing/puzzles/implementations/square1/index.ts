b4b192d27fab739fcee38a512de3320a7bfa7bf3
Lucas Garron <code@garron.net>
2021-01-17 23:49:51 -0800

    [puzzles] Fix accents on Square-1 inventors.

--- src/cubing/puzzles/implementations/square1/index.ts
+++ src/cubing/puzzles/implementations/square1/index.ts
@@ -3,7 +3,7 @@ import { PuzzleManager } from "../../PuzzleManager";
 export const square1: PuzzleManager = {
   id: "square1",
   fullName: "Square-1",
-  inventedBy: ["Karel Hrsel", "Vojtech Kopsky"],
+  inventedBy: ["Karel Hršel", "Vojtech Kopský"],
   inventionYear: 1990, // Czech patent application year: http://spisy.upv.cz/Patents/FullDocuments/277/277266.pdf
   def: async () => {
     return await import("./sq1-hyperorbit.kpuzzle.json");
