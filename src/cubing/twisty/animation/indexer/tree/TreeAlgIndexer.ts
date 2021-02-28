63a1d4f0c357aecd0b3659cc186ac666a7746461
Lucas Garron <code@garron.net>
2021-02-27 23:17:25 -0800

    [twisty] Tree indexer: Chunk nested algs.

--- src/cubing/twisty/animation/indexer/tree/TreeAlgIndexer.ts
+++ src/cubing/twisty/animation/indexer/tree/TreeAlgIndexer.ts
@@ -1,25 +1,81 @@
-import { Alg, Grouping, Move } from "../../../../alg";
+import {
+  Alg,
+  Commutator,
+  Conjugate,
+  Grouping,
+  LineComment,
+  Move,
+  Newline,
+  Pause,
+  TraversalUp,
+  Unit,
+} from "../../../../alg";
 import { AlgBuilder } from "../../../../alg/AlgBuilder";
 import { PuzzleWrapper, State } from "../../../3D/puzzles/KPuzzleWrapper";
 import { Duration, Timestamp } from "../../cursor/CursorTypes";
 import { AlgIndexer } from "../AlgIndexer";
 import { AlgPartDecoration, AlgWalker, DecoratorConstructor } from "./walker";
 
-// TODO: This only chunks the top-level alg. It won't help when a long alg is inside a container.
-function chunkAlg(alg: Alg): Alg {
-  const chunkMaxLength = Math.floor(Math.sqrt(alg.experimentalNumUnits()));
-  const mainAlgBuilder = new AlgBuilder();
-  const chunkAlgBuilder = new AlgBuilder();
-  for (const unit of alg.units()) {
-    chunkAlgBuilder.push(unit);
-    if (chunkAlgBuilder.experimentalNumUnits() >= chunkMaxLength) {
-      mainAlgBuilder.push(new Grouping(chunkAlgBuilder.toAlg()));
-      chunkAlgBuilder.reset();
+class ChunkAlgs extends TraversalUp<Alg, Unit> {
+  traverseAlg(alg: Alg): Alg {
+    const chunkMaxLength = Math.floor(Math.sqrt(alg.experimentalNumUnits()));
+    const mainAlgBuilder = new AlgBuilder();
+    const chunkAlgBuilder = new AlgBuilder();
+    for (const unit of alg.units()) {
+      chunkAlgBuilder.push(unit);
+      if (chunkAlgBuilder.experimentalNumUnits() >= chunkMaxLength) {
+        mainAlgBuilder.push(new Grouping(chunkAlgBuilder.toAlg()));
+        chunkAlgBuilder.reset();
+      }
     }
+    mainAlgBuilder.push(new Grouping(chunkAlgBuilder.toAlg()));
+    return mainAlgBuilder.toAlg();
   }
-  mainAlgBuilder.push(new Grouping(chunkAlgBuilder.toAlg()));
-  return mainAlgBuilder.toAlg();
+
+  traverseGrouping(grouping: Grouping): Unit {
+    return new Grouping(
+      this.traverseAlg(grouping.experimentalAlg),
+      grouping.experimentalEffectiveAmount, // TODO
+    );
+  }
+
+  traverseMove(move: Move): Unit {
+    return move;
+  }
+
+  traverseCommutator(commutator: Commutator): Unit {
+    return new Conjugate(
+      this.traverseAlg(commutator.A),
+      this.traverseAlg(commutator.B),
+      commutator.experimentalEffectiveAmount, // TODO
+    );
+  }
+
+  traverseConjugate(conjugate: Conjugate): Unit {
+    return new Conjugate(
+      this.traverseAlg(conjugate.A),
+      this.traverseAlg(conjugate.B),
+      conjugate.experimentalEffectiveAmount, // TODO
+    );
+  }
+
+  traversePause(pause: Pause): Unit {
+    return pause;
+  }
+
+  traverseNewline(newline: Newline): Unit {
+    return newline;
+  }
+
+  traverseLineComment(comment: LineComment): Unit {
+    return comment;
+  }
 }
+
+const chunkAlgsInstance = new ChunkAlgs();
+export const chunkAlgs = chunkAlgsInstance.traverseAlg.bind(
+  chunkAlgsInstance,
+) as (alg: Alg) => Alg;
 
 export class TreeAlgIndexer implements AlgIndexer<PuzzleWrapper> {
   private decoration: AlgPartDecoration<PuzzleWrapper>;
@@ -27,7 +83,7 @@ export class TreeAlgIndexer implements AlgIndexer<PuzzleWrapper> {
   constructor(private puzzle: PuzzleWrapper, alg: Alg) {
     const deccon = new DecoratorConstructor<PuzzleWrapper>(this.puzzle);
 
-    const chunkedAlg = chunkAlg(alg);
+    const chunkedAlg = chunkAlgs(alg);
 
     this.decoration = deccon.traverseAlg(chunkedAlg);
     this.walker = new AlgWalker<PuzzleWrapper>(
