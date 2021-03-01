83642e6d39aac1c2902727935aa6343ed0d30ba8
Lucas Garron <code@garron.net>
2021-02-28 19:10:05 -0800

    [twisty] Tree alg indexer: limit layers to 10 units.

---
+++ src/cubing/twisty/animation/indexer/tree/chunkAlgs.ts
@@ -0,0 +1,82 @@
+import {
+  Alg,
+  Commutator,
+  Conjugate,
+  Grouping,
+  LineComment,
+  Move,
+  Newline,
+  Pause,
+  TraversalUp,
+  Unit,
+} from "../../../../alg";
+import { AlgBuilder } from "../../../../alg/AlgBuilder";
+
+const MAX_NODE_LENGTH = 10;
+
+function chunkifyAlg(alg: Alg, chunkMaxLength: number): Alg {
+  const mainAlgBuilder = new AlgBuilder();
+  const chunkAlgBuilder = new AlgBuilder();
+  for (const unit of alg.units()) {
+    chunkAlgBuilder.push(unit);
+    if (chunkAlgBuilder.experimentalNumUnits() >= chunkMaxLength) {
+      mainAlgBuilder.push(new Grouping(chunkAlgBuilder.toAlg()));
+      chunkAlgBuilder.reset();
+    }
+  }
+  mainAlgBuilder.push(new Grouping(chunkAlgBuilder.toAlg()));
+  return mainAlgBuilder.toAlg();
+}
+
+class ChunkAlgs extends TraversalUp<Alg, Unit> {
+  traverseAlg(alg: Alg): Alg {
+    while (alg.experimentalNumUnits() > MAX_NODE_LENGTH) {
+      alg = chunkifyAlg(alg, MAX_NODE_LENGTH);
+    }
+    return alg;
+  }
+
+  traverseGrouping(grouping: Grouping): Unit {
+    return new Grouping(
+      this.traverseAlg(grouping.experimentalAlg),
+      grouping.experimentalEffectiveAmount, // TODO
+    );
+  }
+
+  traverseMove(move: Move): Unit {
+    return move;
+  }
+
+  traverseCommutator(commutator: Commutator): Unit {
+    return new Conjugate(
+      this.traverseAlg(commutator.A),
+      this.traverseAlg(commutator.B),
+      commutator.experimentalEffectiveAmount, // TODO
+    );
+  }
+
+  traverseConjugate(conjugate: Conjugate): Unit {
+    return new Conjugate(
+      this.traverseAlg(conjugate.A),
+      this.traverseAlg(conjugate.B),
+      conjugate.experimentalEffectiveAmount, // TODO
+    );
+  }
+
+  traversePause(pause: Pause): Unit {
+    return pause;
+  }
+
+  traverseNewline(newline: Newline): Unit {
+    return newline;
+  }
+
+  traverseLineComment(comment: LineComment): Unit {
+    return comment;
+  }
+}
+
+const chunkAlgsInstance = new ChunkAlgs();
+export const chunkAlgs = chunkAlgsInstance.traverseAlg.bind(
+  chunkAlgsInstance,
+) as (alg: Alg) => Alg;
