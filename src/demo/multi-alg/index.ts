556edf3a1fe3abfdc6a86c47e1fa92a180e13662
Lucas Garron <code@garron.net>
2021-03-14 03:40:44 -0700

    Download multiple screenshots (hardcoded).

--- src/demo/multi-alg/index.ts
+++ src/demo/multi-alg/index.ts
@@ -1,9 +1,23 @@
+import { Alg } from "../../cubing/alg";
 import "../../cubing/twisty"
 import {Twisty3DCanvas, TwistyPlayer} from "../../cubing/twisty"
 
 const player: TwistyPlayer = document.querySelector("twisty-player")!;
 
-document.querySelector("#screenshot")?.addEventListener("click", () => {
+function downloadDataURL(url: string): void {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "screenshot.png";
+  a.click();
+}
+
+function getScreenshot(alg: Alg) {
+  player.alg = alg;
   const twisty3DCanvas = player.viewerElems[0] as Twisty3DCanvas;
-  console.log(twisty3DCanvas.renderToDataURL({squareCrop: true}));
+  downloadDataURL(twisty3DCanvas.renderToDataURL({squareCrop: true}));
+}
+
+document.querySelector("#screenshot")?.addEventListener("click", () => {
+  getScreenshot(new Alg("R U R' U R U2' R'"))
+  getScreenshot(new Alg("R U R' U' R' F R F'"))
 })
