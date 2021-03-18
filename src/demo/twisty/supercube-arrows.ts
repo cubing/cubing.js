cubing/cubing.js

commit 541d4f0b73ab71432747071ed158770d1262cd67

[demo] Avoid impoorting `twisty` directly from HTML.
This is supposed to be fine, but regularly breaks development due to https://github.com/parcel-bundler/parcel/issues/5683#issuecomment-766450652

src/demo/twisty/supercube-arrows.html
@@ -4,7 +4,7 @@
     <meta charset="utf8" />
     <meta name="theme-color" value="#141414" />
     <title>cubing.js | Pochmann Supercube</title>
-    <script src="../../cubing/twisty"></script>
+    <script src="./supercube-arrows.ts"></script>
     <style>
       html,
       body {

src/demo/twisty/supercube.html
@@ -4,7 +4,7 @@
     <meta charset="utf8" />
     <meta name="theme-color" value="#141414" />
     <title>cubing.js | Pochmann Supercube</title>
-    <script src="../../cubing/twisty"></script>
+    <script src="./supercube.ts"></script>
     <style>
       html,
       body {

src/demo/twisty/supercube.ts
@@ -1,97 +1,2 @@
-// Parcel-ism.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import supercubeSprite from "url:./supercube-sprite.png";
-import { parseAlg } from "../../cubing/alg";
-import {
-  connect,
-  debugKeyboardConnect,
-  MoveEvent,
-} from "../../cubing/bluetooth";
-import {
-  Cube3D,
-  TimelineActionEvent,
-  TimestampLocationType,
-  TwistyPlayer,
-} from "../../cubing/twisty";
-
-const spriteURL =
-  new URL(location.href).searchParams.get("sprite") ?? supercubeSprite;
-
-const hintSpriteURL =
-  new URL(location.href).searchParams.get("hint-sprite") ?? "";
-
-// Common picture cube demo code.
-
-let haveHadMoveInput = false;
-
-const twistyPlayer = document.querySelector("twisty-player")! as TwistyPlayer;
-twistyPlayer.timeline.jumpToStart();
-twistyPlayer.timeline.play(); // TODO: add autoplay
-twistyPlayer.timeline.addActionListener({
-  onTimelineAction: (actionEvent: TimelineActionEvent) => {
-    if (haveHadMoveInput) {
-      return;
-    }
-    if (actionEvent.locationType === TimestampLocationType.EndOfTimeline) {
-      twistyPlayer.timeline.jumpToStart();
-      twistyPlayer.timeline.play();
-    }
-  },
-});
-
-(async () => {
-  const kb = await debugKeyboardConnect();
-  kb.addMoveListener((e: MoveEvent) => {
-    if (!haveHadMoveInput) {
-      twistyPlayer.timeline.pause();
-      twistyPlayer.alg = parseAlg("");
-      haveHadMoveInput = true;
-    }
-    twistyPlayer.experimentalAddMove(e.latestMove);
-  });
-})();
-
-let lastTimestamp = performance.now();
-const ROTATION_RATE = (2 * Math.PI) / 15;
-let haveTriedToSetSpriteURL = false;
-function rotate() {
-  if (twistyPlayer.twisty3D && !haveTriedToSetSpriteURL) {
-    haveTriedToSetSpriteURL = true;
-    (twistyPlayer.twisty3D as Cube3D).experimentalSetStickerSpriteURL(
-      spriteURL,
-    );
-    (twistyPlayer.twisty3D as Cube3D).experimentalSetHintStickerSpriteURL(
-      hintSpriteURL,
-    );
-  }
-
-  const newTimestamp = performance.now();
-  twistyPlayer.twisty3D?.rotateY(
-    ((newTimestamp - lastTimestamp) / 1000) * ROTATION_RATE,
-  );
-  if (
-    !(twistyPlayer.viewerElems[0] as any)?.orbitControls
-      .experimentalHasBeenMoved
-  ) {
-    requestAnimationFrame(rotate);
-    lastTimestamp = newTimestamp;
-  }
-}
-requestAnimationFrame(rotate);
-
-async function connectBluetooth(): Promise<void> {
-  const bluetoothPuzzle = await connect();
-  document.body.removeEventListener("click", connectBluetooth);
-  bluetoothPuzzle.addMoveListener((e: MoveEvent) => {
-    if (!haveHadMoveInput) {
-      twistyPlayer.timeline.pause();
-      twistyPlayer.alg = parseAlg("");
-      haveHadMoveInput = true;
-    }
-    twistyPlayer.experimentalAddMove(e.latestMove);
-  });
-}
-if (new URL(location.href).searchParams.get("bluetooth") === "true") {
-  document.body.addEventListener("click", connectBluetooth);
-}
+// This file is a workaround for https://github.com/parcel-bundler/parcel/issues/5683#issuecomment-766450652
+import "../../cubing/twisty"

src/demo/twisty/2d-ll-stickering.html
@@ -4,7 +4,7 @@
     <meta charset="utf8" />
     <title>2D LL Stickering | twisty.js</title>
     <link href="./demo-grid.css" rel="stylesheet" type="text/css" />
-    <script src="../../cubing/twisty/index.ts"></script>
+    <script src="./2d-ll-stickering.ts"></script>
   </head>
 
   <body>

src/demo/twisty/cube3d.html
@@ -4,7 +4,7 @@
     <meta charset="utf8" />
     <title>Full-Page Test | twisty.js</title>
     <link href="./full-page.css" rel="stylesheet" type="text/css" />
-    <script src="../../cubing/twisty/index.ts"></script>
+    <script src="./cube3d.ts"></script>
   </head>
 
   <body>

src/demo/twisty/elements.html
@@ -4,7 +4,7 @@
     <meta charset="utf8" />
     <title>Inline Test | twisty.js</title>
     <link href="./inline.css" rel="stylesheet" type="text/css" />
-    <script src="../../cubing/twisty/index.ts"></script>
+    <script src="./elements.ts"></script>
   </head>
 
   <body>

commit fa1c002529334148e066f0097477ccff628b1f8b

[demo] Fix supercube demos that were accidentally broken in 541d4f0b73ab71432747071ed158770d1262cd67.

src/demo/twisty/supercube-arrows.html
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf8" />
-    <meta name="theme-color" value="#141414" />
-    <title>cubing.js | Pochmann Supercube</title>
-    <script src="./supercube-arrows.ts"></script>
-    <style>
-      html,
-      body {
-        margin: 0;
-        height: 100%;
-      }
-      body {
-        background-color: #303030;
-        background-repeat: repeat;
-        box-shadow: inset 0 0 1em black;
-      }
-      twisty-player {
-        width: 100%;
-        height: 100%;
-      }
-    </style>
-    <script src="./supercube-arrows.ts" defer></script>
-  </head>
 
-  <body>
-    <twisty-player
-      alg="(L F2 U2 R2 L' F U2 F D2 B2 U' R U2 R' D2 F2 // inspection
+<head>
+  <meta charset="utf8" />
+  <meta name="theme-color" value="#141414" />
+  <title>cubing.js | Pochmann Supercube</title>
+  <style>
+    html,
+    body {
+      margin: 0;
+      height: 100%;
+    }
+
+    body {
+      background-color: #303030;
+      background-repeat: repeat;
+      box-shadow: inset 0 0 1em black;
+    }
+
+    twisty-player {
+      width: 100%;
+      height: 100%;
+    }
+  </style>
+  <script src="./supercube-arrows.ts" defer></script>
+</head>
+
+<body>
+  <twisty-player alg="(L F2 U2 R2 L' F U2 F D2 B2 U' R U2 R' D2 F2 // inspection
 U R2 U' F' L F' U' L' // XX-Cross + EO
 U' R U R' // 3rd slot
 R' U R U2' R' U R // 4th slot
 U R' U' R U' R' U2 R // OLL / ZBLL
 U // AUF
-)4"
-      control-panel="none"
-      experimental-stickering="picture"
-    ></twisty-player>
-  </body>
+)4" control-panel="none" experimental-stickering="picture"></twisty-player>
+</body>
+
 </html>

src/demo/twisty/supercube-arrows.ts
@@ -1,2 +1,109 @@
-// This file is a workaround for https://github.com/parcel-bundler/parcel/issues/5683#issuecomment-766450652
-import "../../cubing/twisty"
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import m12Cube from "url:./M12Cube.gif";
+// Supercube arrow image from Ben Whitmore.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import supercubeArrowsSprite from "url:./supercube-arrows-sprite.png";
+// Parcel-ism.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import mkbhdSpriteURL from "url:./mkbhd-sprite-red.png";
+import { Alg } from "../../cubing/alg";
+import {
+  connect,
+  debugKeyboardConnect,
+  MoveEvent,
+} from "../../cubing/bluetooth";
+import {
+  Cube3D,
+  TimelineActionEvent,
+  TimestampLocationType,
+  TwistyPlayer,
+} from "../../cubing/twisty";
+
+(window as any).m12Cube = m12Cube;
+
+const spriteURL =
+  new URL(location.href).searchParams.get("sprite") ?? supercubeArrowsSprite;
+
+const hintSpriteURL =
+  new URL(location.href).searchParams.get("hint-sprite") ?? "";
+
+// Common picture cube demo code.
+
+let haveHadMoveInput = false;
+
+const twistyPlayer = document.querySelector("twisty-player")! as TwistyPlayer;
+twistyPlayer.experimentalSetCursorIndexer("simultaneous");
+twistyPlayer.timeline.jumpToStart();
+setTimeout(() => {
+  twistyPlayer.timeline.play(); // TODO: add autoplay
+}, 1000);
+twistyPlayer.timeline.addActionListener({
+  onTimelineAction: (actionEvent: TimelineActionEvent) => {
+    if (haveHadMoveInput) {
+      return;
+    }
+    if (actionEvent.locationType === TimestampLocationType.EndOfTimeline) {
+      twistyPlayer.timeline.jumpToStart();
+      twistyPlayer.timeline.play();
+    }
+  },
+});
+
+(async () => {
+  const kb = await debugKeyboardConnect();
+  kb.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = new Alg();
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+})();
+
+let lastTimestamp = performance.now();
+const ROTATION_RATE = (2 * Math.PI) / 15;
+let haveTriedToSetSpriteURL = false;
+function rotate() {
+  if (twistyPlayer.twisty3D && !haveTriedToSetSpriteURL) {
+    haveTriedToSetSpriteURL = true;
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetStickerSpriteURL(
+      spriteURL,
+    );
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetHintStickerSpriteURL(
+      hintSpriteURL,
+    );
+  }
+
+  const newTimestamp = performance.now();
+  twistyPlayer.twisty3D?.rotateY(
+    ((newTimestamp - lastTimestamp) / 1000) * ROTATION_RATE,
+  );
+  if (
+    !(twistyPlayer.viewerElems[0] as any)?.orbitControls
+      .experimentalHasBeenMoved
+  ) {
+    requestAnimationFrame(rotate);
+    lastTimestamp = newTimestamp;
+  }
+}
+requestAnimationFrame(rotate);
+
+async function connectBluetooth(): Promise<void> {
+  const bluetoothPuzzle = await connect();
+  document.body.removeEventListener("click", connectBluetooth);
+  bluetoothPuzzle.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = new Alg("");
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+}
+if (new URL(location.href).searchParams.get("bluetooth") === "true") {
+  document.body.addEventListener("click", connectBluetooth);
+}

src/demo/twisty/supercube.html
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf8" />
-    <meta name="theme-color" value="#141414" />
-    <title>cubing.js | Pochmann Supercube</title>
-    <script src="./supercube.ts"></script>
-    <style>
-      html,
-      body {
-        margin: 0;
-        height: 100%;
-      }
-      body {
-        background-color: #303030;
-        background-repeat: repeat;
-        box-shadow: inset 0 0 1em black;
-      }
-      twisty-player {
-        width: 100%;
-        height: 100%;
-      }
-    </style>
-    <script src="./supercube.ts" defer></script>
-  </head>
 
-  <body>
-    <twisty-player
-      alg="(L F2 U2 R2 L' F U2 F D2 B2 U' R U2 R' D2 F2 // inspection
+<head>
+  <meta charset="utf8" />
+  <meta name="theme-color" value="#141414" />
+  <title>cubing.js | Pochmann Supercube</title>
+  <style>
+    html,
+    body {
+      margin: 0;
+      height: 100%;
+    }
+
+    body {
+      background-color: #303030;
+      background-repeat: repeat;
+      box-shadow: inset 0 0 1em black;
+    }
+
+    twisty-player {
+      width: 100%;
+      height: 100%;
+    }
+  </style>
+  <script src="./supercube.ts" defer></script>
+</head>
+
+<body>
+  <twisty-player alg="(L F2 U2 R2 L' F U2 F D2 B2 U' R U2 R' D2 F2 // inspection
 U R2 U' F' L F' U' L' // XX-Cross + EO
 U' R U R' // 3rd slot
 R' U R U2' R' U R // 4th slot
 U R' U' R U' R' U2 R // OLL / ZBLL
 U // AUF
-)4"
-      control-panel="none"
-      experimental-stickering="picture"
-    ></twisty-player>
-  </body>
+)4" control-panel="none" experimental-stickering="picture"></twisty-player>
+</body>
+
 </html>

src/demo/twisty/supercube.ts
@@ -1,2 +1,108 @@
-// This file is a workaround for https://github.com/parcel-bundler/parcel/issues/5683#issuecomment-766450652
-import "../../cubing/twisty"
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import m12Cube from "url:./M12Cube.gif";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import supercubeSprite from "url:./supercube-sprite.png";
+// Parcel-ism.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import mkbhdSpriteURL from "url:./mkbhd-sprite-red.png";
+import { Alg } from "../../cubing/alg";
+import {
+  connect,
+  debugKeyboardConnect,
+  MoveEvent,
+} from "../../cubing/bluetooth";
+import {
+  Cube3D,
+  TimelineActionEvent,
+  TimestampLocationType,
+  TwistyPlayer,
+} from "../../cubing/twisty";
+
+(window as any).m12Cube = m12Cube;
+
+const spriteURL =
+  new URL(location.href).searchParams.get("sprite") ?? supercubeSprite;
+
+const hintSpriteURL =
+  new URL(location.href).searchParams.get("hint-sprite") ?? "";
+
+// Common picture cube demo code.
+
+let haveHadMoveInput = false;
+
+const twistyPlayer = document.querySelector("twisty-player")! as TwistyPlayer;
+twistyPlayer.experimentalSetCursorIndexer("simultaneous");
+twistyPlayer.timeline.jumpToStart();
+setTimeout(() => {
+  twistyPlayer.timeline.play(); // TODO: add autoplay
+}, 1000);
+twistyPlayer.timeline.addActionListener({
+  onTimelineAction: (actionEvent: TimelineActionEvent) => {
+    if (haveHadMoveInput) {
+      return;
+    }
+    if (actionEvent.locationType === TimestampLocationType.EndOfTimeline) {
+      twistyPlayer.timeline.jumpToStart();
+      twistyPlayer.timeline.play();
+    }
+  },
+});
+
+(async () => {
+  const kb = await debugKeyboardConnect();
+  kb.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = new Alg();
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+})();
+
+let lastTimestamp = performance.now();
+const ROTATION_RATE = (2 * Math.PI) / 15;
+let haveTriedToSetSpriteURL = false;
+function rotate() {
+  if (twistyPlayer.twisty3D && !haveTriedToSetSpriteURL) {
+    haveTriedToSetSpriteURL = true;
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetStickerSpriteURL(
+      spriteURL,
+    );
+    (twistyPlayer.twisty3D as Cube3D).experimentalSetHintStickerSpriteURL(
+      hintSpriteURL,
+    );
+  }
+
+  const newTimestamp = performance.now();
+  twistyPlayer.twisty3D?.rotateY(
+    ((newTimestamp - lastTimestamp) / 1000) * ROTATION_RATE,
+  );
+  if (
+    !(twistyPlayer.viewerElems[0] as any)?.orbitControls
+      .experimentalHasBeenMoved
+  ) {
+    requestAnimationFrame(rotate);
+    lastTimestamp = newTimestamp;
+  }
+}
+requestAnimationFrame(rotate);
+
+async function connectBluetooth(): Promise<void> {
+  const bluetoothPuzzle = await connect();
+  document.body.removeEventListener("click", connectBluetooth);
+  bluetoothPuzzle.addMoveListener((e: MoveEvent) => {
+    if (!haveHadMoveInput) {
+      twistyPlayer.timeline.pause();
+      twistyPlayer.alg = new Alg("");
+      haveHadMoveInput = true;
+    }
+    twistyPlayer.experimentalAddMove(e.latestMove);
+  });
+}
+if (new URL(location.href).searchParams.get("bluetooth") === "true") {
+  document.body.addEventListener("click", connectBluetooth);
+}