f627555b877b9fb6ae2afdaec3522c3c274c06f1
Lucas Garron <code@garron.net>
2021-04-03 18:46:02 -0700

    [puzzles] Fix `fto` PG3D reference.

--- src/cubing/puzzles/index.ts
+++ src/cubing/puzzles/index.ts
@@ -32,7 +32,7 @@ export const puzzles: Record<string, PuzzleLoader> = {
   // 4x4x4 Blindfolded
   // 5x5x5 Blindfolded
   /******** End of WCA puzzles ********/
-  "fto": genericPGPuzzleLoader("fto", "Face-Turning Octahedron", {
+  "fto": genericPGPuzzleLoader("FTO", "Face-Turning Octahedron", {
     inventedBy: ["Karl Rohrbach", "David Pitcher"], // http://twistypuzzles.com/cgi-bin/puzzle.cgi?pkey=1663
     inventionYear: 1983, // http://twistypuzzles.com/cgi-bin/puzzle.cgi?pkey=1663
   }),
