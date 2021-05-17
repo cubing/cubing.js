ac766089451de301a93959d3b07984931baf57c3
Lucas Garron <code@garron.net>
2021-05-16 20:02:03 -0700

    [alg] Simplify Commutator/Conjugate implementations now that they don't have amounts.

--- src/cubing/alg/units/containers/Commutator.ts
+++ src/cubing/alg/units/containers/Commutator.ts
@@ -3,76 +3,34 @@ import { AlgCommon, Comparable } from "../../common";
 import { IterationDirection } from "../../iteration";
 import type { LeafUnit } from "../Unit";
 
-export class QuantumCommutator extends Comparable {
-  constructor(public A: Alg, public B: Alg) {
-    super();
-    Object.freeze(this);
-  }
-
-  isIdentical(other: Comparable): boolean {
-    const otherAsQuantumCommutator = other.as(QuantumCommutator);
-    return !!(
-      otherAsQuantumCommutator?.A.isIdentical(this.A) &&
-      otherAsQuantumCommutator?.B.isIdentical(this.B)
-    );
-  }
-
-  toString(): string {
-    return `[${this.A}, ${this.B}]`;
-  }
-
-  // TODO: use a common composite iterator helper.
-  *experimentalExpand(
-    iterDir: IterationDirection = IterationDirection.Forwards,
-    depth: number, // TODO
-  ): Generator<LeafUnit> {
-    if (depth === 0) {
-      throw new Error("cannot expand depth 0 for a quantum");
-    }
-
-    if (iterDir === IterationDirection.Forwards) {
-      yield* this.A.experimentalExpand(IterationDirection.Forwards, depth - 1);
-      yield* this.B.experimentalExpand(IterationDirection.Forwards, depth - 1);
-      yield* this.A.experimentalExpand(IterationDirection.Backwards, depth - 1);
-      yield* this.B.experimentalExpand(IterationDirection.Backwards, depth - 1);
-    } else {
-      yield* this.B.experimentalExpand(IterationDirection.Forwards, depth - 1);
-      yield* this.A.experimentalExpand(IterationDirection.Forwards, depth - 1);
-      yield* this.B.experimentalExpand(IterationDirection.Backwards, depth - 1);
-      yield* this.A.experimentalExpand(IterationDirection.Backwards, depth - 1);
-    }
-  }
-}
-
 export class Commutator extends AlgCommon<Commutator> {
-  readonly #quantum: QuantumCommutator;
+  readonly #A: Alg;
+  readonly #B: Alg;
 
   constructor(aSource: FlexibleAlgSource, bSource: FlexibleAlgSource) {
     super();
-    this.#quantum = new QuantumCommutator(
-      experimentalEnsureAlg(aSource),
-      experimentalEnsureAlg(bSource),
-    ); // TODO
+    this.#A = experimentalEnsureAlg(aSource);
+    this.#B = experimentalEnsureAlg(bSource);
   }
 
   get A(): Alg {
-    return this.#quantum.A;
+    return this.#A;
   }
 
   get B(): Alg {
-    return this.#quantum.B;
+    return this.#B;
   }
 
   isIdentical(other: Comparable): boolean {
     const otherAsCommutator = other.as(Commutator);
-    return (
-      !!otherAsCommutator &&
-      this.#quantum.isIdentical(otherAsCommutator.#quantum)
+    return !!(
+      otherAsCommutator?.A.isIdentical(this.A) &&
+      otherAsCommutator?.B.isIdentical(this.B)
     );
   }
 
   invert(): Commutator {
-    return new Commutator(this.#quantum.B, this.#quantum.A);
+    return new Commutator(this.#B, this.#A);
   }
 
   *experimentalExpand(
@@ -83,20 +41,45 @@ export class Commutator extends AlgCommon<Commutator> {
     if (depth === 0) {
       yield iterDir === IterationDirection.Forwards ? this : this.invert();
     } else {
-      yield* this.#quantum.experimentalExpand(iterDir, depth);
+      if (iterDir === IterationDirection.Forwards) {
+        yield* this.A.experimentalExpand(
+          IterationDirection.Forwards,
+          depth - 1,
+        );
+        yield* this.B.experimentalExpand(
+          IterationDirection.Forwards,
+          depth - 1,
+        );
+        yield* this.A.experimentalExpand(
+          IterationDirection.Backwards,
+          depth - 1,
+        );
+        yield* this.B.experimentalExpand(
+          IterationDirection.Backwards,
+          depth - 1,
+        );
+      } else {
+        yield* this.B.experimentalExpand(
+          IterationDirection.Forwards,
+          depth - 1,
+        );
+        yield* this.A.experimentalExpand(
+          IterationDirection.Forwards,
+          depth - 1,
+        );
+        yield* this.B.experimentalExpand(
+          IterationDirection.Backwards,
+          depth - 1,
+        );
+        yield* this.A.experimentalExpand(
+          IterationDirection.Backwards,
+          depth - 1,
+        );
+      }
     }
   }
 
   toString(): string {
-    return this.#quantum.toString();
+    return `[${this.#A}, ${this.#B}]`;
   }
-
-  // toJSON(): CommutatorJSON {
-  //   return {
-  //     type: "commutator",
-  //     A: this.#quanta.quantum.A.toJSON(),
-  //     B: this.#quanta.quantum.B.toJSON(),
-  //     amount: this.a
-  //   };
-  // }
 }
--- src/cubing/alg/units/containers/Conjugate.ts
+++ src/cubing/alg/units/containers/Conjugate.ts
@@ -3,68 +3,34 @@ import { AlgCommon, Comparable } from "../../common";
 import { IterationDirection } from "../../iteration";
 import type { LeafUnit } from "../Unit";
 
-export class QuantumConjugate extends Comparable {
-  constructor(public A: Alg, public B: Alg) {
-    super();
-    Object.freeze(this);
-  }
-
-  isIdentical(other: Comparable): boolean {
-    const otherAsQuantumCommutator = other.as(QuantumConjugate);
-    return !!(
-      otherAsQuantumCommutator?.A.isIdentical(this.A) &&
-      otherAsQuantumCommutator?.B.isIdentical(this.B)
-    );
-  }
-
-  // TODO: use a common composite iterator helper.
-  *experimentalExpand(
-    iterDir: IterationDirection = IterationDirection.Forwards,
-    depth: number,
-  ): Generator<LeafUnit> {
-    if (depth === 0) {
-      throw new Error("cannot expand depth 0 for a quantum");
-    }
-
-    yield* this.A.experimentalExpand(IterationDirection.Forwards, depth - 1);
-    yield* this.B.experimentalExpand(iterDir, depth - 1);
-    yield* this.A.experimentalExpand(IterationDirection.Backwards, depth - 1);
-  }
-
-  toString(): string {
-    return `[${this.A}: ${this.B}]`;
-  }
-}
-
 export class Conjugate extends AlgCommon<Conjugate> {
-  readonly #quantum: QuantumConjugate;
+  readonly #A: Alg;
+  readonly #B: Alg;
 
   constructor(aSource: FlexibleAlgSource, bSource: FlexibleAlgSource) {
     super();
-    this.#quantum = new QuantumConjugate(
-      experimentalEnsureAlg(aSource),
-      experimentalEnsureAlg(bSource),
-    ); // TODO
+    this.#A = experimentalEnsureAlg(aSource);
+    this.#B = experimentalEnsureAlg(bSource);
   }
 
   get A(): Alg {
-    return this.#quantum.A;
+    return this.#A;
   }
 
   get B(): Alg {
-    return this.#quantum.B;
+    return this.#B;
   }
 
   isIdentical(other: Comparable): boolean {
-    const otherAsConjugate = other as Conjugate;
-    return (
-      other.is(Conjugate) &&
-      this.#quantum.isIdentical(otherAsConjugate.#quantum)
+    const otherAsConjugate = other.as(Conjugate);
+    return !!(
+      otherAsConjugate?.A.isIdentical(this.A) &&
+      otherAsConjugate?.B.isIdentical(this.B)
     );
   }
 
   invert(): Conjugate {
-    return new Conjugate(this.#quantum.A, this.#quantum.B.invert());
+    return new Conjugate(this.#A, this.#B.invert());
   }
 
   *experimentalExpand(
@@ -75,20 +41,13 @@ export class Conjugate extends AlgCommon<Conjugate> {
     if (depth === 0) {
       yield iterDir === IterationDirection.Forwards ? this : this.invert();
     } else {
-      yield* this.#quantum.experimentalExpand(iterDir, depth);
+      yield* this.A.experimentalExpand(IterationDirection.Forwards, depth - 1);
+      yield* this.B.experimentalExpand(iterDir, depth - 1);
+      yield* this.A.experimentalExpand(IterationDirection.Backwards, depth - 1);
     }
   }
 
   toString(): string {
-    return this.#quantum.toString();
+    return `[${this.A}: ${this.B}]`;
   }
-
-  // toJSON(): ConjugateJSON {
-  //   return {
-  //     type: "conjugate",
-  //     A: this.#quanta.quantum.A.toJSON(),
-  //     B: this.#quanta.quantum.B.toJSON(),
-  //     amount: this.a
-  //   };
-  // }
 }
