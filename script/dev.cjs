1a96dbd3364317f1e90253ed1857b700413c9b7f
Lucas Garron <code@garron.net>
2021-06-17 18:32:29 -0700

    FREEZE.indexed

--- script/dev.cjs
+++ script/dev.cjs
@@ -5,28 +5,28 @@ const {
 } = require("snowpack");
 const { build } = require("esbuild");
 
-// const config = createConfiguration({
-//   workspaceRoot: "/",
-//   mount: {
-//     "src/experiments/static": { url: "/", static: true },
-//     "src/experiments/code": { url: "/_code" },
-//   },
-//   packageOptions: {},
-//   devOptions: {
-//     port: 3333,
-//   },
-//   buildOptions: {
-//     out: "dist/experiments",
-//     baseUrl: "/cubing.js",
-//   },
-//   optimize: {
-//     bundle: true,
-//     splitting: true,
-//     minify: true,
-//     target: "es2020",
-//     sourcemap: false,
-//   },
-// });
+const config = createConfiguration({
+  workspaceRoot: "/",
+  mount: {
+    "src/experiments/static": { url: "/", static: true },
+    "src/experiments/code": { url: "/_code" },
+  },
+  packageOptions: {},
+  devOptions: {
+    port: 3333,
+  },
+  buildOptions: {
+    out: "dist/experiments",
+    baseUrl: "/cubing.js",
+  },
+  optimize: {
+    bundle: true,
+    splitting: true,
+    minify: true,
+    target: "es2020",
+    sourcemap: false,
+  },
+});
 
 (async () => {
   console.log("starting?");
