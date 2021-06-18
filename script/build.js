cb4ac6353fe36d264021c016745bc668ecf114f4
Lucas Garron <code@garron.net>
2021-06-18 03:48:15 -0700

    FREEZE.unindexed

--- script/build.js
+++ script/build.js
@@ -1,8 +1,21 @@
+// This file was created because:
+//
+// - Safari and Firefox don't support `modules` workers yet.
+// - `esbuild` and `snowpack` don't automatically handle transpilation to
+//   `classic` web workers.
+// - Parcel has too many show-stopping bugs for us to use.
+//
+// Once Safari and Firefox support module workers, we can hopefully revert to
+// short commandline commands. But maybe this file will stick around because it
+// turns out to be too useful.
+
 import * as snowpack from "snowpack";
 import * as esbuild from "esbuild";
 
 import configSrc from "../snowpack.config.mjs";
 import { execPromise } from "./execPromise.js";
+
+const external = ["three", "web-worker"];
 
 export async function build(target, dev) {
   const depPromises = target.dependencies.map((dependency) =>
@@ -61,24 +74,15 @@ export const cjsTarget = {
       watch: dev,
       logLevel: "info",
       //
-      external: ["three", "web-worker"],
+      external,
     });
     await execPromise("cp -R src/dist-static/cjs/* dist/cjs");
   },
 };
 
-// npm run build-worker; npx esbuild
-// --target=es2020
-// --bundle
-// --external:three
-// --external:web-worker
-// --splitting
-// --format=esm
-// --sourcemap
-// --outdir=dist/esm src/cubing/index.ts src/cubing/alg/index.ts src/cubing/bluetooth/index.ts src/cubing/kpuzzle/index.ts src/cubing/protocol/index.ts src/cubing/puzzle-geometry/index.ts src/cubing/puzzles/index.ts src/cubing/scramble/index.ts src/cubing/stream/index.ts src/cubing/solve/index.ts src/cubing/twisty/index.ts && cp -R src/dist-static/esm/* dist/esm && cp src/cubing/solve/worker/worker-inside-generated.cjs dist/esm/worker-inside-generated.cjs
 export const esmTarget = {
   builtYet: false,
-  dependencies: [],
+  dependencies: [SolveWorkerTarget],
   buildSelf: async (dev) => {
     await esbuild.build({
       entryPoints: [
@@ -101,12 +105,31 @@ export const esmTarget = {
       logLevel: "info",
       sourcemap: true,
       //
-      external: ["three", "web-worker"],
+      external,
     });
     await execPromise("cp -R src/dist-static/esm/* dist/esm");
     await execPromise(
       "cp src/cubing/solve/worker/worker-inside-generated.cjs dist/esm/worker-inside-generated.cjs",
     );
+  },
+};
+
+export const binTarget = {
+  builtYet: false,
+  dependencies: [SolveWorkerTarget],
+  buildSelf: async (dev) => {
+    await esbuild.build({
+      entryPoints: ["src/bin/puzzle-geometry-bin.ts"],
+      outfile: "dist/bin/puzzle-geometry-bin.js",
+      format: "esm",
+      target: "es2020",
+      bundle: true,
+      watch: dev,
+      logLevel: "info",
+      sourcemap: true,
+      //
+      external,
+    });
   },
 };
 
@@ -137,6 +160,7 @@ const targets /*: Record<String, SolverWorker>*/ = {
   "bundle-global": BundleGlobalTarget,
   "cjs": cjsTarget,
   "esm": esmTarget,
+  "bin": binTarget,
 };
 
 (async () => {
