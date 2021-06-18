fcfb119eafd8674df8b7fe98aefbf2ecb104bb2e
Lucas Garron <code@garron.net>
2021-06-17 18:29:30 -0700

    FREEZE.unindexed

--- script/dev.cjs
+++ script/dev.cjs
@@ -3,44 +3,48 @@ const {
   startServer,
   createConfiguration,
 } = require("snowpack");
-const { serve } = require("esbuild");
+const { build } = require("esbuild");
 
-const config = createConfiguration({
-  workspaceRoot: "/",
-  mount: {
-    "src/experiments/static": { url: "/", static: true },
-    "src/experiments/code": { url: "/_code" },
-  },
-  packageOptions: {},
-  devOptions: {
-    port: 3333,
-  },
-  buildOptions: {
-    out: "dist/experiments",
-    baseUrl: "/cubing.js",
-  },
-  optimize: {
-    bundle: true,
-    splitting: true,
-    minify: true,
-    target: "es2020",
-    sourcemap: false,
-  },
-});
+// const config = createConfiguration({
+//   workspaceRoot: "/",
+//   mount: {
+//     "src/experiments/static": { url: "/", static: true },
+//     "src/experiments/code": { url: "/_code" },
+//   },
+//   packageOptions: {},
+//   devOptions: {
+//     port: 3333,
+//   },
+//   buildOptions: {
+//     out: "dist/experiments",
+//     baseUrl: "/cubing.js",
+//   },
+//   optimize: {
+//     bundle: true,
+//     splitting: true,
+//     minify: true,
+//     target: "es2020",
+//     sourcemap: false,
+//   },
+// });
 
 (async () => {
-  const esbuildServerPromise = serve({
+  console.log("starting?");
+  const esbuildServerPromise = build({
     entryPoints: ["src/cubing/twisty/worker/inside/src/worker-inside.ts"],
     outfile: "src/cubing/twisty/worker/inside/generated/worker-inside.js",
     format: "cjs",
     target: "es2015",
     bundle: true,
     watch: true,
+    logLevel: "info",
   });
+  esbuildServerPromise.catch(() => process.exit(1));
 
-  // setTimeout(async () => {
-  //   const snowpackServerPromise = startServer({ config }, { isDev: true });
+  await esbuildServerPromise;
 
-  //   await Promise.all([esbuildServerPromise, snowpackServerPromise]);
-  // }, 1000);
+  setTimeout(async () => {
+    const snowpackServerPromise = startServer({ config }, { isDev: true });
+    await Promise.all([esbuildServerPromise, snowpackServerPromise]);
+  }, 1000);
 })();
