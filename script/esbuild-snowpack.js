95299fa1214f2d0159d00e6eec3890cacfc4b5f1
Lucas Garron <code@garron.net>
2021-06-17 19:31:50 -0700

    FREEZE.unindexed

--- script/esbuild-snowpack.js
+++ script/esbuild-snowpack.js
@@ -4,28 +4,7 @@ import * as esbuild from "esbuild";
 export async function main(options) {
   const { dev } = options;
 
-  const config = snowpack.createConfiguration({
-    workspaceRoot: "/",
-    mount: {
-      "src/experiments/static": { url: "/", static: true },
-      "src/experiments/code": { url: "/_code" },
-    },
-    packageOptions: {},
-    devOptions: {
-      port: 3333,
-    },
-    buildOptions: {
-      out: "dist/experiments",
-      baseUrl: "/cubing.js",
-    },
-    optimize: {
-      bundle: true,
-      splitting: true,
-      minify: true,
-      target: "es2020",
-      sourcemap: false,
-    },
-  });
+  const config = snowpack.createConfiguration();
 
   const esbuildPromise = esbuild.build({
     entryPoints: ["src/cubing/solve/worker/inside/src/worker-inside.ts"],
@@ -42,5 +21,5 @@ export async function main(options) {
   const snowpackPromise = dev
     ? snowpack.startServer({ config }, { isDev: dev })
     : snowpack.build({ config });
-  await Promise.all([esbuildPromise, snowpackPromise]);
+  await snowpackPromise;
 }
