21c94ad49cd9e072d64bd0599f935377f4ab5544
Lucas Garron <code@garron.net>
2021-06-20 04:19:10 -0700

    Revert "[package.json] Unhook the `cjs` build for now."
    This reverts commit dd2fffe0167a7a04e4ca0988a03c24f0ea9eaa5f.

--- src/dist-test/require.cjs
+++ src/dist-test/require.cjs
@@ -1,22 +1,20 @@
-console.log("Skipping require test since we don't have a CJS build anymore.");
+console.log("Attempting to import the entire `cubing` module.");
 
-// console.log("Attempting to import the entire `cubing` module.");
+const cubing = require("cubing");
+console.log(cubing);
 
-// const cubing = require("cubing");
-// console.log(cubing);
+console.log(
+  "Attempting to use `cubing/alg to invert an alg (should return R U' R').",
+);
 
-// console.log(
-//   "Attempting to use `cubing/alg to invert an alg (should return R U' R').",
-// );
+const alg = require("cubing/alg");
+console.log(new alg.Alg("R U R'").invert().toString());
 
-// const alg = require("cubing/alg");
-// console.log(new alg.Alg("R U R'").invert().toString());
-
-// console.log("Importing individual modules.");
-// require("cubing/alg");
-// require("cubing/bluetooth");
-// require("cubing/kpuzzle");
-// require("cubing/protocol");
-// require("cubing/puzzle-geometry");
-// require("cubing/stream");
-// require("cubing/twisty");
+console.log("Importing individual modules.");
+require("cubing/alg");
+require("cubing/bluetooth");
+require("cubing/kpuzzle");
+require("cubing/protocol");
+require("cubing/puzzle-geometry");
+require("cubing/stream");
+require("cubing/twisty");
