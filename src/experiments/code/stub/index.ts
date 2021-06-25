8f836124b943f0030d5ffb3765b3674374924b04
Lucas Garron <code@garron.net>
2021-06-25 03:32:44 -0700

    [experiments/stub] Reset stub code.

--- src/experiments/code/stub/index.ts
+++ src/experiments/code/stub/index.ts
@@ -1,23 +1,6 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
 
-import { randomScrambleForEvent } from "../../../cubing/solve";
-
 // Note: this file needs to contain code to avoid a Snowpack error.
 // So we put a `console.log` here for now.
 console.log("Loading stub file.");
-
-(async () => {
-  console.log("asyncey");
-  console.log("from stub", (await randomScrambleForEvent("333")).toString());
-  console.log("from stub", (await randomScrambleForEvent("333")).toString());
-  console.log("from stub", (await randomScrambleForEvent("333")).toString());
-  console.log("from stub", (await randomScrambleForEvent("333")).toString());
-  console.log("from stub", (await randomScrambleForEvent("clock")).toString());
-  console.log("from stub", (await randomScrambleForEvent("minx")).toString());
-  console.log("from stub", (await randomScrambleForEvent("444")).toString());
-  console.log("from stub", (await randomScrambleForEvent("444")).toString());
-  console.log("from stub", (await randomScrambleForEvent("444")).toString());
-  console.log("from stub", (await randomScrambleForEvent("222")).toString());
-  // console.log("from stub", (await randomScrambleForEvent("444")).toString());
-})();
