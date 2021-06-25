751e41476e8b0ab4a95fbae6c3b146a1115ff022
Lucas Garron <code@garron.net>
2021-06-25 03:57:12 -0700

    [experiments/scramble] Add `scramble` example.

---
+++ src/experiments/code/scramble/index.ts
@@ -0,0 +1,40 @@
+import { randomScrambleForEvent } from "../../../cubing/solve";
+import "../../../cubing/twisty";
+import type { TwistyPlayer } from "../../../cubing/twisty";
+import type { PuzzleID } from "../../../cubing/twisty/dom/TwistyPlayerConfig";
+
+const select = document.querySelector("select") as HTMLSelectElement;
+const scrambleStringDiv = document.querySelector(
+  "#scramble-string",
+) as HTMLDivElement;
+const twistyPlayer = document.querySelector("twisty-player") as TwistyPlayer;
+const button = document.querySelector("button") as HTMLButtonElement;
+
+async function newScramble() {
+  scrambleStringDiv.textContent = "⏳";
+  twistyPlayer.alg = "";
+  const scramble = await randomScrambleForEvent(select.value);
+  scrambleStringDiv.textContent = scramble.toString();
+  twistyPlayer.alg = scramble;
+  twistyPlayer.timeline.jumpToEnd();
+}
+
+window.addEventListener("load", () => {
+  button.addEventListener("click", newScramble);
+  select.addEventListener("change", () => {
+    twistyPlayer.alg = "";
+    try {
+      twistyPlayer.puzzle = {
+        333: "3x3x3",
+        444: "4x4x4",
+        minx: "megaminx",
+        clock: "clock",
+      }[select.value] as PuzzleID;
+    } finally {
+      // TODO
+    }
+    setTimeout(newScramble, 100);
+  });
+
+  newScramble();
+});
