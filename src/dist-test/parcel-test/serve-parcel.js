b165058c787f8dea2b2f33a13fb6b3c48a1bf6cd
Lucas Garron <code@garron.net>
2021-06-26 18:54:05 -0700

    [test-import-restrictions] Properly await `npm install`.

--- src/dist-test/parcel-test/serve-parcel.js
+++ src/dist-test/parcel-test/serve-parcel.js
@@ -4,12 +4,12 @@ export const port = 1236;
 
 const packagePath = new URL("./parcel-package", import.meta.url).pathname;
 
-export function installServer() {
-  execPromise("npm install", { cwd: packagePath });
+export async function installServer() {
+  await execPromise("npm install", { cwd: packagePath });
 }
 
 export function startServer() {
-  execPromise(`./node_modules/.bin/parcel serve --port ${port} index.html`, {
+  execPromise(`npx parcel serve --port ${port} index.html`, {
     cwd: packagePath,
   });
 }
