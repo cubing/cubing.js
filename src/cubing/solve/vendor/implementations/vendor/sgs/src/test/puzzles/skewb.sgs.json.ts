8fc7ff8b748019add8fd14ba1e3bb5552b0c82d5
Lucas Garron <code@garron.net>
2021-06-30 04:01:39 -0700

    FREEZE.indexed

---
+++ src/cubing/solve/vendor/implementations/vendor/sgs/src/test/puzzles/skewb.sgs.json.ts
@@ -0,0 +1,61 @@
+import { puzzles } from "../../../../../../../../puzzles";
+import { parseSGS, SGSCachedData } from "../../sgs";
+
+let cachedData: Promise<SGSCachedData> | null = null;
+export async function cachedSGSDataSkewb() {
+  return (cachedData ??= sgsDataSkewb());
+}
+
+// TODO: Reduce info.
+export async function sgsDataSkewb(): Promise<SGSCachedData> {
+  return parseSGS(
+    await puzzles.skewb.def(),
+    `SetOrder CORNERS 14 3 4 13 12 8 7 5
+SetOrder CENTERS 11 2 10 1 6 9
+Alg U
+Alg U'
+Alg U R
+Alg U R'
+Alg U' L'
+Alg R
+Alg R'
+Alg R L
+Alg R L'
+Alg U L U'
+Alg U L' U'
+Alg U B' U
+Alg U' B U'
+Alg R' L' R
+Alg R' B R'
+Alg U L' U' B'
+Alg U L' B' U
+Alg U' B L U'
+Alg U' B L' U'
+Alg R B L R'
+Alg L
+Alg L'
+Alg L' B
+Alg L' B'
+Alg L' B L
+Alg L' B L'
+Alg L' B' L
+Alg L' B' L'
+Alg B
+Alg B'
+Alg L B L'
+Alg L B' L'
+Alg R L B' L' R'
+Alg B' L B L' B
+Alg B' L B' L' B
+Alg U' L' U' B' U L U
+Alg R' L' R B R' L R
+Alg U B' U' L' U B U' L
+Alg U' B R' L' R B U' B'
+Alg L B L' R L B' L' R'
+Alg R' B U' R' B' L' U B'
+Alg U R' L' R U' B' U B
+Alg U B' L' U' L' B U L
+Alg U' R U L' B' R B R'
+Alg R U R' U' B U B' L'`,
+  );
+}
--- src/experiments/code/stub/index.ts
+++ src/experiments/code/stub/index.ts
@@ -1,6 +1,12 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
 
+import { cachedSGSDataSkewb } from "../../../cubing/solve/vendor/implementations/vendor/sgs/src/test/puzzles/skewb.sgs.json";
+
 // Note: this file needs to contain code to avoid a Snowpack error.
 // So we put a `console.log` here for now.
 console.log("Loading stub file.");
+
+(async () => {
+  console.log(await cachedSGSDataSkewb());
+})();
