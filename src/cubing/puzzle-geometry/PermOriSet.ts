cubing/cubing.js

commit e047135b75dac383dd17ad72c505f4e81b23fc83

If, when generating ksolve or gap output, the external name ends with an apostrophe, remove it and invert the move.

src/cubing/puzzle-geometry/PermOriSet.ts
@@ -86,13 +86,27 @@ export class OrbitsDef {
     result.push("End");
     for (let i = 0; i < this.movenames.length; i++) {
       result.push("");
-      result.push("Move " + externalName(mapper, this.movenames[i]));
+      let name = externalName(mapper, this.movenames[i]);
+      let doinv = false;
+      if (name[name.length - 1] === "'") {
+        doinv = true;
+        name = name.substring(0, name.length-1);
+      }
+      result.push("Move " + name);
       for (let j = 0; j < this.orbitnames.length; j++) {
-        this.moveops[i].orbits[j].appendConciseDefinition(
-          result,
-          this.orbitnames[j],
-          true,
-        );
+        if (doinv) {
+          this.moveops[i].orbits[j].appendConciseDefinition(
+            result,
+            this.orbitnames[j],
+            true,
+          );
+        } else {
+          this.moveops[i].orbits[j].inv().appendConciseDefinition(
+            result,
+            this.orbitnames[j],
+            true,
+          );
+        }
       }
       result.push("End");
     }

src/cubing/puzzle-geometry/PuzzleGeometry.ts
@@ -2015,10 +2015,19 @@ export class PuzzleGeometry {
     const r = [];
     const mvs = [];
     for (let i = 0; i < os.moveops.length; i++) {
-      const movename = "M_" + externalName(this.notationMapper, os.movenames[i]);
+      let movename = "M_" + externalName(this.notationMapper, os.movenames[i]);
+      let doinv = false;
+      if (movename[movename.length-1] === "'") {
+        movename = movename.substring(0, movename.length-1);
+        doinv = true;
+      }
       // gap doesn't like angle brackets in IDs
       mvs.push(movename);
-      r.push(movename + ":=" + os.moveops[i].toPerm().toGap() + ";");
+      if (doinv) {
+        r.push(movename + ":=" + os.moveops[i].toPerm().toGap() + ";");
+      } else {
+        r.push(movename + ":=" + os.moveops[i].toPerm().inv().toGap() + ";");
+      }
     }
     r.push("Gen:=[");
     r.push(mvs.join(","));

commit 9bed5bce402c2d6bd34320c2df6974de6fef87c1

[puzzlegeometry] for gap and ksolve (but not kpuzzle) output, fix the inversion required sometimes by notation mapping.  This fixes a bug introduced in e047135b75dac383dd17ad72c505f4e81b23fc83.

src/cubing/puzzle-geometry/PermOriSet.ts
@@ -95,15 +95,15 @@ export class OrbitsDef {
       result.push("Move " + name);
       for (let j = 0; j < this.orbitnames.length; j++) {
         if (doinv) {
+          this.moveops[i].orbits[j]
+            .inv()
+            .appendConciseDefinition(result, this.orbitnames[j], true);
+        } else {
           this.moveops[i].orbits[j].appendConciseDefinition(
             result,
             this.orbitnames[j],
             true,
           );
-        } else {
-          this.moveops[i].orbits[j]
-            .inv()
-            .appendConciseDefinition(result, this.orbitnames[j], true);
         }
       }
       result.push("End");

src/cubing/puzzle-geometry/PuzzleGeometry.ts
@@ -2033,9 +2033,9 @@ export class PuzzleGeometry {
       // gap doesn't like angle brackets in IDs
       mvs.push(movename);
       if (doinv) {
-        r.push(movename + ":=" + os.moveops[i].toPerm().toGap() + ";");
-      } else {
         r.push(movename + ":=" + os.moveops[i].toPerm().inv().toGap() + ";");
+      } else {
+        r.push(movename + ":=" + os.moveops[i].toPerm().toGap() + ";");
       }
     }
     r.push("Gen:=[");