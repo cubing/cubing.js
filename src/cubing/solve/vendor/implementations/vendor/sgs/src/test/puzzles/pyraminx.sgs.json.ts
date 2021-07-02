77ed4006bded7cbc5c5122d23fc8c680b163d31d
Lucas Garron <code@garron.net>
2021-07-02 03:43:59 -0700

    [puzzles] Update `pyraminx` SVG.

---
+++ src/cubing/solve/vendor/implementations/vendor/sgs/src/test/puzzles/pyraminx.sgs.json.ts
@@ -0,0 +1,94 @@
+import { puzzles } from "../../../../../../../../puzzles";
+import { parseSGS, SGSCachedData } from "../../sgs";
+
+let cachedData: Promise<SGSCachedData> | null = null;
+export async function cachedSGSDataPyraminx() {
+  return (cachedData ??= sgsDataPyraminx());
+}
+
+// TODO: Reduce info.
+export async function sgsDataPyraminx(): Promise<SGSCachedData> {
+  return parseSGS(
+    await puzzles.skewb.def(),
+    `SetOrder CORNERS 14 4 5 1 13 9 8 6
+SetOrder CENTERS 12 3 11 2 7 10
+
+Alg y
+Alg y2
+Alg y'
+Alg F
+Alg F'
+Alg y U
+Alg y U'
+Alg y L
+Alg y L'
+Alg y F
+Alg y F'
+Alg y2 U
+Alg y2 U'
+Alg y2 B
+Alg y2 B'
+Alg y' U
+Alg y' F'
+Alg y U L'
+Alg y U B
+Alg y2 U B
+Alg y2 U B'
+Alg y2 U' B
+Alg y2 U' B'
+
+Alg U
+Alg U'
+Alg U L
+Alg U L'
+Alg U' R'
+
+Alg R
+Alg R'
+Alg R B
+Alg R' L'
+
+Alg U B' U
+Alg U' L U
+Alg U' L' U
+Alg U' B U'
+Alg R L R'
+Alg R B' R
+Alg U B L U'
+Alg U B' U B'
+Alg U B' L' U
+Alg U' L B U'
+Alg R' B L' R'
+
+Alg L
+Alg L'
+Alg L B
+Alg L B'
+Alg L B L
+Alg L B L'
+Alg L B' L
+Alg L B' L'
+
+Alg B
+Alg B'
+
+Alg L' B L
+Alg L' B' L
+Alg R' L' B L R
+
+Alg B L' B L B'
+Alg B L' B' L B'
+Alg U L U B U' L' U'
+Alg R L R' B' R L' R'
+Alg U B' R L R' B' U B
+Alg U' B U L U' B' U L'
+Alg L U' B U L' U' B' U
+Alg R B' U R B L U' B
+
+Alg U L U' B' U L' U' B
+Alg U' B L U L B' U' L'
+
+Alg U R' U' L B R' B' R
+Alg R' U' R U B' U' B L`,
+  );
+}
