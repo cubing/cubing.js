24746b78385eab3f3aad5f9e59c64c83394bcfbf
Lucas Garron <code@garron.net>
2021-07-02 20:47:48 -0700

    [snowpack] Add an env flag to exclude Babylon.

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,6 +1,18 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+// For some reason, Snowpack takes a long time to process Babylon. When measured
+// once, `make build-experiments`, this made the difference between 32 seconds
+// and 9 seconds.
+const EXCLUDE_BABYLON = process.env["EXCLUDE_BABYLON"];
+const babylonExternal = EXCLUDE_BABYLON ? ["@babylonjs/core"] : [];
+if (EXCLUDE_BABYLON) {
+  console.log("Excluding Babylon in Snowpack due to EXCLUDE_BABYLON env var!");
+  console.log(
+    "Hopefully this will make Snowpack dep installation significantly faster.",
+  );
+}
+
 export default {
   workspaceRoot: "/",
   mount: {
@@ -10,7 +22,7 @@ export default {
   exclude: ["**/.DS_Store"],
   packageOptions: {
     knownEntrypoints: ["comlink"],
-    external: ["crypto", "worker_threads"],
+    external: ["crypto", "worker_threads", ...babylonExternal],
   },
   devOptions: {
     port: 3333,
