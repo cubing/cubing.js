fe076f7cb9baa72f2367004fc66f3761b7043748
Lucas Garron <code@garron.net>
2021-07-04 02:19:03 -0700

    [test-dist-esm-node-import] Fix actual test code.

---
+++ script/test/dist/esm/node-import/test.mjs
@@ -0,0 +1,20 @@
+import "cubing/alg";
+import "cubing/bluetooth";
+import "cubing/kpuzzle";
+import "cubing/notation";
+import "cubing/protocol";
+import "cubing/puzzle-geometry";
+import "cubing/puzzles";
+import "cubing/scramble";
+import "cubing/solve";
+import "cubing/stream";
+import "cubing/twisty";
+
+import { randomScrambleForEvent } from "cubing/scramble";
+
+(async () => {
+  (await randomScrambleForEvent("222")).log();
+  (await randomScrambleForEvent("333")).log();
+
+  console.log("Success!");
+})();
