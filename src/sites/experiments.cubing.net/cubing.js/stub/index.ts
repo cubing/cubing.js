ed3f99919b8e2ceaf1abf84465f7c9a6edaac3f5
Lucas Garron <code@garron.net>
2021-08-04 03:57:34 -0700

    [stub] Clear stub.

--- src/sites/experiments.cubing.net/cubing.js/stub/index.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/index.ts
@@ -1,12 +1,6 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
 
-import { randomScrambleForEvent } from "../../../../cubing/scramble";
-
 // Note: this file needs to contain code to avoid a Snowpack error.
 // So we put a `console.log` here for now.
 console.log("Loading stub file.");
-
-(async () => {
-  (await randomScrambleForEvent("sq1")).log();
-})();
