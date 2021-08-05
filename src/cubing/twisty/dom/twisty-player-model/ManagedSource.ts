6b2f33a019c9a6dda6f99dcadd0dd903ad6bcac3
Lucas Garron <code@garron.net>
2021-08-05 06:20:45 -0700

    FREEZE.unindexed

--- src/cubing/twisty/dom/twisty-player-model/ManagedSource.ts
+++ src/cubing/twisty/dom/twisty-player-model/ManagedSource.ts
@@ -1,14 +1,54 @@
+abstract class TwistyPropV2<T> extends EventTarget {
+  // MUST propagate to all children synchronously.
+  latestGeneration: number = 0;
+  propagateLazyPromise(generation: number): void {
+    if (generation <= this.latestGeneration) {
+      return;
+    }
+    this.dispatchEvent(new CustomEvent("update", this.derive.bind(this)));
+    this.latestGeneration = generation;
+  }
+
+  abstract derive(): Promise<T>;
+}
+
+interface GenerationalLazyPromise<T> {
+  generation: number;
+  lazyPromise: Promise<T>;
+}
+
+interface LazyPromiseEventDetail<T> {
+  lazyPromise: GenerationalLazyPromise<T>;
+}
+
+type LazyPromiseEvent<T> = CustomEvent<LazyPromiseEventDetail<T>>;
+
+let globalGenerationCounter = 0;
+
 // TODO: add stale marker?
-export class ManagedSource<T extends EventTarget> extends EventTarget {
-  #target: T; // TODO: | null
-  constructor(target: T, private listener: () => {}) {
+export class ManagedSource<T> extends EventTarget {
+  #lazyPromise: GenerationalLazyPromise<T>;
+
+  #target: TwistyPropV2<T>;
+  constructor(target: TwistyPropV2<T>, private listener: () => {}) {
     super();
     // this.set(target);
     this.#target = target;
-    this.#target.addEventListener("update", this.listener);
+    this.#target.addEventListener("update", this.#onUpdate.bind(this));
   }
 
-  get target(): T {
+  #onUpdate(event: LazyPromiseEvent<T>): void {
+    this.#lazyPromise = event.detail.lazyPromise;
+  }
+
+  get target(): TwistyPropV2<T> {
     return this.#target;
   }
 }
+
+// Principles:
+// - Any time a value is derived, it uses a consistent set of input generations.
+// - Values are immutable.
+// - Don't derive any value unless someone awaits it. To start an expensive operation, get someone to await a derived value that depends on it.
+
+// TODO: Isolating reused objects (e.g. Twisty3D)
