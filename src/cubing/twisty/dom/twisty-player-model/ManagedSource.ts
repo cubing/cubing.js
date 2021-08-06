86cc2e0ddaeb406ca8a96246efec33c82704c87b
Lucas Garron <code@garron.net>
2021-08-06 11:09:07 -0700

    FREEZE.unindexed

--- src/cubing/twisty/dom/twisty-player-model/ManagedSource.ts
+++ src/cubing/twisty/dom/twisty-player-model/ManagedSource.ts
@@ -1,4 +1,9 @@
-abstract class TwistyPropV2<T> extends EventTarget {
+abstract class TwistyPropV2<
+  InputTypes extends Object,
+  OutputType,
+> extends EventTarget {
+  // cachedInputs;
+
   // MUST propagate to all children synchronously.
   latestGeneration: number = 0;
   propagateLazyPromise(generation: number): void {
@@ -9,7 +14,12 @@ abstract class TwistyPropV2<T> extends EventTarget {
     this.latestGeneration = generation;
   }
 
-  abstract derive(): Promise<T>;
+  public abstract derive(options?: {
+    lastValue: OutputType;
+  }): Promise<OutputType>;
+
+  // TODO: Async?
+  protected abstract canReuse(v1: OutputType, v2: OutputType): boolean;
 }
 
 interface GenerationalLazyPromise<T> {
@@ -26,25 +36,25 @@ type LazyPromiseEvent<T> = CustomEvent<LazyPromiseEventDetail<T>>;
 let globalGenerationCounter = 0;
 
 // TODO: add stale marker?
-export class ManagedSource<T> extends EventTarget {
-  #lazyPromise: GenerationalLazyPromise<T>;
+// export class ManagedSource<T> extends EventTarget {
+//   #target: TwistyPropV2<T>;
+//   constructor(target: TwistyPropV2<T>, private listener: () => {}) {
+//     super();
+//     // this.set(target);
+//     this.#target = target;
+//     this.#target.addEventListener("update", this.#onUpdate.bind(this));
+//   }
 
-  #target: TwistyPropV2<T>;
-  constructor(target: TwistyPropV2<T>, private listener: () => {}) {
-    super();
-    // this.set(target);
-    this.#target = target;
-    this.#target.addEventListener("update", this.#onUpdate.bind(this));
-  }
+//   value(): Promise<T> {}
 
-  #onUpdate(event: LazyPromiseEvent<T>): void {
-    this.#lazyPromise = event.detail.lazyPromise;
-  }
+//   #onUpdate(event: LazyPromiseEvent<T>): void {
+//     this.#lazyPromise = event.detail.lazyPromise;
+//   }
 
-  get target(): TwistyPropV2<T> {
-    return this.#target;
-  }
-}
+//   get target(): TwistyPropV2<T> {
+//     return this.#target;
+//   }
+// }
 
 // Principles:
 // - Any time a value is derived, it uses a consistent set of input generations.
