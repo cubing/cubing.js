6ff2ce8d36bc3f94c68953cf61383c664f96a283
Lucas Garron <code@garron.net>
2021-08-06 12:20:47 -0700

    FREEZE.indexed

--- src/cubing/twisty/dom/twisty-player-model/ManagedSource.ts
+++ src/cubing/twisty/dom/twisty-player-model/ManagedSource.ts
@@ -1,39 +1,94 @@
-abstract class TwistyPropV2<
-  InputTypes extends Object,
-  OutputType,
-> extends EventTarget {
-  // cachedInputs;
+type InputProps<T extends Object> = {
+  [s in keyof T]: TwistySourceProp<T[s]>;
+};
 
-  // MUST propagate to all children synchronously.
-  latestGeneration: number = 0;
-  propagateLazyPromise(generation: number): void {
-    if (generation <= this.latestGeneration) {
-      return;
-    }
-    this.dispatchEvent(new CustomEvent("update", this.derive.bind(this)));
-    this.latestGeneration = generation;
+type InputPromises<T extends Object> = {
+  [s in keyof T]: Promise<T[s]>;
+};
+
+export abstract class TwistyPropParent<T> {
+  public abstract get(): Promise<T>;
+
+  // Uses value comparison. Overwrite with a cheap semantic comparison when
+  // possible.
+  canReuse(v1: T, v2: T): boolean {
+    return v1 === v2;
+  }
+}
+
+export abstract class TwistySourceProp<T> extends TwistyPropParent<T> {
+  #value: T;
+  defaultValue: T;
+
+  // TODO: Accept promise?
+  set(t: T): void {
+    this.#value = t;
   }
 
-  public abstract derive(options?: {
-    lastValue: OutputType;
-  }): Promise<OutputType>;
+  async get(): Promise<T> {
+    return this.#value;
+  }
+}
+
+export abstract class TwistyPropV2<
+  InputTypes extends Object,
+  OutputType,
+> extends TwistySourceProp<OutputType> {
+  // cachedInputs:
+  #parents: InputProps<InputTypes>;
+
+  #cachedResult: { inputs: InputTypes; output: OutputType } | null = null;
+
+  constructor(parents: InputProps<InputTypes>) {
+    super();
+    this.#parents = parents;
+  }
+
+  public async get(): Promise<OutputType> {
+    const inputValuePromises: InputPromises<InputTypes> = {} as any; // TODO
+    for (const key in this.#parents) {
+      inputValuePromises[key] = this.#parents[key].get();
+    }
+
+    const inputs: InputTypes = {} as any; // TODO
+    for (const key in this.#parents) {
+      inputs[key] = await inputValuePromises[key];
+    }
+
+    const cachedResult = this.#cachedResult;
+    if (cachedResult) {
+      for (const key in this.#parents) {
+        const parent = this.#parents[key];
+        if (!parent.canReuse(inputs[key], cachedResult.inputs[key])) {
+          continue;
+        }
+      }
+      return cachedResult!.output;
+    }
+
+    const output = await this.derive(inputs);
+    this.#cachedResult = { inputs, output };
+    return output;
+  }
+
+  protected abstract derive(input: InputTypes): Promise<OutputType>;
 
   // TODO: Async?
-  protected abstract canReuse(v1: OutputType, v2: OutputType): boolean;
+  // protected abstract canReuse(v1: OutputType, v2: OutputType): boolean;
 }
 
-interface GenerationalLazyPromise<T> {
-  generation: number;
-  lazyPromise: Promise<T>;
-}
+// interface GenerationalLazyPromise<T> {
+//   generation: number;
+//   lazyPromise: Promise<T>;
+// }
 
-interface LazyPromiseEventDetail<T> {
-  lazyPromise: GenerationalLazyPromise<T>;
-}
+// interface LazyPromiseEventDetail<T> {
+//   lazyPromise: GenerationalLazyPromise<T>;
+// }
 
-type LazyPromiseEvent<T> = CustomEvent<LazyPromiseEventDetail<T>>;
+// type LazyPromiseEvent<T> = CustomEvent<LazyPromiseEventDetail<T>>;
 
-let globalGenerationCounter = 0;
+// let globalGenerationCounter = 0;
 
 // TODO: add stale marker?
 // export class ManagedSource<T> extends EventTarget {
--- src/sites/experiments.cubing.net/cubing.js/stub/index.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/index.ts
@@ -1,77 +1,115 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
 
-import { Alg } from "../../../../cubing/alg";
-import { randomChoiceFactory } from "../../../../cubing/search/inside/solve/vendor/random-uint-below";
-import { TwistyPlayerModel } from "../../../../cubing/twisty/dom/twisty-player-model/TwistyPlayerModel";
-import type { PuzzleID } from "../../../../cubing/twisty/dom/TwistyPlayerConfig";
+import {
+  TwistyPropV2,
+  TwistySourceProp,
+} from "../../../../cubing/twisty/dom/twisty-player-model/ManagedSource";
 
 // Note: this file needs to contain code to avoid a Snowpack error.
 // So we put a `console.log` here for now.
 console.log("Loading stub file.");
 
-const timeoutInput = document.body.appendChild(
-  document.createElement("input"),
-) as HTMLInputElement;
-timeoutInput.type = "number";
-timeoutInput.value = "200";
-timeoutInput.step = "25";
+class A extends TwistySourceProp<number> {
+  defaultValue: 4;
+}
 
-const model = new TwistyPlayerModel();
-
-console.log(model);
-model.algProp.alg = new Alg("R U R'");
-// model.puzzleProp.puzzleID = "skewb";
-
-console.log(model.puzzleProp.puzzleLoader);
-(async () => {
-  model.algProp.setFromString("sdfdfsdf'sdfdsf");
-  (await model.algIssues()).log();
-
-  model.algProp.setFromString("R  F");
-  (await model.algIssues()).log();
-
-  model.algProp.setFromString("notamove");
-  (await model.algIssues()).log();
-
-  model.algProp.setFromString("R++");
-  (await model.algIssues()).log();
-
-  // model.puzzleProp.puzzleID = "megaminx";
-  (await model.algIssues()).log();
-
-  console.log(model.visualizationProp.wrapperElement);
-  document.body.appendChild(model.visualizationProp.wrapperElement);
-
-  model.requestedVisualization = "3D";
-  // model.requestedVisualization = "2D";
-  model.requestedVisualization = "3D";
-  model.alg = "U2 D2";
-
-  model.alg = "L U2 D F";
-  model.puzzle = "3x3x3";
-  model.puzzle = "pyraminx";
-  model.setup = "R2 U R U R' U' R' U' R' U R'";
-
-  (window as any).model = model;
-
-  async function update() {
-    const randomChoice = await randomChoiceFactory<PuzzleID>();
-    // TODO: 3x3x3 only works the first time.
-    let puzzle = model.puzzle;
-    while (puzzle === model.puzzle) {
-      model.puzzle = randomChoice([
-        "3x3x3",
-        "megaminx",
-        "pyraminx",
-        "master_tetraminx",
-        "7x7x7",
-      ]);
-    }
-    console.log("model.puzzle", model.puzzle);
-    setTimeout(update, parseInt(timeoutInput.value));
+class B extends TwistyPropV2<{ a: number }, number> {
+  async derive(input: { a: number }): Promise<number> {
+    await new Promise(async (resolve) =>
+      setTimeout(resolve, Math.random() * 100),
+    );
+    return (input.a * 2) % 17;
   }
-  if (false) {
-    update();
+}
+
+class C extends TwistyPropV2<{ a: number }, number> {
+  async derive(input: { a: number }): Promise<number> {
+    await new Promise(async (resolve) =>
+      setTimeout(resolve, Math.random() * 100),
+    );
+    return (input.a * 3) % 17;
   }
-})();
+}
+
+class D extends TwistyPropV2<{ b: number; c: number }, boolean> {
+  async derive(input: { b: number; c: number }): Promise<boolean> {
+    await new Promise(async (resolve) =>
+      setTimeout(resolve, Math.random() * 100),
+    );
+    return input.b > input.c;
+  }
+}
+
+const a = new A();
+const b = new B({ a });
+const c = new C({ a });
+const d = new D({ b, c });
+
+console.log(await a.get(), await b.get(), await c.get(), await d.get());
+
+// const timeoutInput = document.body.appendChild(
+//   document.createElement("input"),
+// ) as HTMLInputElement;
+// timeoutInput.type = "number";
+// timeoutInput.value = "200";
+// timeoutInput.step = "25";
+
+// const model = new TwistyPlayerModel();
+
+// console.log(model);
+// model.algProp.alg = new Alg("R U R'");
+// // model.puzzleProp.puzzleID = "skewb";
+
+// console.log(model.puzzleProp.puzzleLoader);
+// (async () => {
+//   model.algProp.setFromString("sdfdfsdf'sdfdsf");
+//   (await model.algIssues()).log();
+
+//   model.algProp.setFromString("R  F");
+//   (await model.algIssues()).log();
+
+//   model.algProp.setFromString("notamove");
+//   (await model.algIssues()).log();
+
+//   model.algProp.setFromString("R++");
+//   (await model.algIssues()).log();
+
+//   // model.puzzleProp.puzzleID = "megaminx";
+//   (await model.algIssues()).log();
+
+//   console.log(model.visualizationProp.wrapperElement);
+//   document.body.appendChild(model.visualizationProp.wrapperElement);
+
+//   model.requestedVisualization = "3D";
+//   // model.requestedVisualization = "2D";
+//   model.requestedVisualization = "3D";
+//   model.alg = "U2 D2";
+
+//   model.alg = "L U2 D F";
+//   model.puzzle = "3x3x3";
+//   model.puzzle = "pyraminx";
+//   model.setup = "R2 U R U R' U' R' U' R' U R'";
+
+//   (window as any).model = model;
+
+//   async function update() {
+//     const randomChoice = await randomChoiceFactory<PuzzleID>();
+//     // TODO: 3x3x3 only works the first time.
+//     let puzzle = model.puzzle;
+//     while (puzzle === model.puzzle) {
+//       model.puzzle = randomChoice([
+//         "3x3x3",
+//         "megaminx",
+//         "pyraminx",
+//         "master_tetraminx",
+//         "7x7x7",
+//       ]);
+//     }
+//     console.log("model.puzzle", model.puzzle);
+//     setTimeout(update, parseInt(timeoutInput.value));
+//   }
+//   if (false) {
+//     update();
+//   }
+// })();
