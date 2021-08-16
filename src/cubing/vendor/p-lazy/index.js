2f9f8ae099a2a9cc47f488d2a96108d0b0f29220
Lucas Garron <code@garron.net>
2021-08-15 18:24:23 -0700

    Lint and format `p-lazy` impl.

--- src/cubing/vendor/p-lazy/index.js
+++ src/cubing/vendor/p-lazy/index.js
@@ -1,40 +1,39 @@
 // TODO: Use private class fields when ESLint support it.
 
 export default class PLazy extends Promise {
-	constructor(executor) {
-		super(resolve => {
-			resolve();
-		});
+  constructor(executor) {
+    super((resolve) => {
+      resolve();
+    });
 
-		this._executor = executor;
-	}
+    this._executor = executor;
+  }
 
-	static from(function_) {
-		return new PLazy(resolve => {
-			resolve(function_());
-		});
-	}
+  static from(function_) {
+    return new PLazy((resolve) => {
+      resolve(function_());
+    });
+  }
 
-	static resolve(value) {
-		return new PLazy(resolve => {
-			resolve(value);
-		});
-	}
+  static resolve(value) {
+    return new PLazy((resolve) => {
+      resolve(value);
+    });
+  }
 
-	static reject(error) {
-		return new PLazy((resolve, reject) => {
-			reject(error);
-		});
-	}
+  static reject(error) {
+    return new PLazy((_resolve, reject) => {
+      reject(error);
+    });
+  }
 
-	then(onFulfilled, onRejected) {
-		this._promise = this._promise || new Promise(this._executor);
-		// eslint-disable-next-line promise/prefer-await-to-then
-		return this._promise.then(onFulfilled, onRejected);
-	}
+  then(onFulfilled, onRejected) {
+    this._promise = this._promise || new Promise(this._executor);
+    return this._promise.then(onFulfilled, onRejected);
+  }
 
-	catch(onRejected) {
-		this._promise = this._promise || new Promise(this._executor);
-		return this._promise.catch(onRejected);
-	}
+  catch(onRejected) {
+    this._promise = this._promise || new Promise(this._executor);
+    return this._promise.catch(onRejected);
+  }
 }
