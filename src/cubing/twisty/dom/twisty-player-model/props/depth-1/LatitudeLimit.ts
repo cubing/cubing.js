fba7846b39cd5d7bf552b1812c012200ac59be63
Lucas Garron <code@garron.net>
2021-08-17 00:03:22 -0700

    Change default latitude limit.

--- src/cubing/twisty/dom/twisty-player-model/props/depth-1/LatitudeLimit.ts
+++ src/cubing/twisty/dom/twisty-player-model/props/depth-1/LatitudeLimit.ts
@@ -1,8 +1,11 @@
 import { SimpleTwistyPropSource } from "../TwistyProp";
 import type { CoordinateDegrees } from "./OrbitCoordinatesRequestProp";
 
+// Similar to https://alg.cubing.net/
+const DEFAULT_LATITUDE_LIMIT = 35;
+
 export class LatitudeLimitProp extends SimpleTwistyPropSource<CoordinateDegrees> {
   getDefaultValue(): CoordinateDegrees {
-    return 60;
+    return DEFAULT_LATITUDE_LIMIT;
   }
 }
