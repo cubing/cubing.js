64c6cf9ca307eda64cb7262244ec23deae423101
Lucas Garron <code@garron.net>
2021-08-19 21:42:03 -0700

    <twisty-player-v2>: Factor out setters.
    TypeScript doesn't have `writeonly`, so we're keeping the code very explicit for now:
    https://github.com/microsoft/TypeScript/issues/21759

--- src/cubing/twisty/dom/twisty-player-model/controllers/TwistyPlayerV2.ts
+++ src/cubing/twisty/dom/twisty-player-model/controllers/TwistyPlayerV2.ts
@@ -1,8 +1,5 @@
 import type { ExperimentalStickering } from "../../..";
-import type { Alg } from "../../../../alg";
-import type { MillisecondTimestamp } from "../../../animation/cursor/CursorTypes";
 import { ClassListManager } from "../../element/ClassListManager";
-import { ManagedCustomElement } from "../../element/ManagedCustomElement";
 import { customElementsShim } from "../../element/node-custom-element-shims";
 import { twistyPlayerCSS } from "../../TwistyPlayer.css_";
 import {
@@ -11,11 +8,7 @@ import {
   SetupToLocation,
 } from "../../TwistyPlayerConfig";
 import type { BackgroundThemeWithAuto } from "../props/depth-0/BackgroundProp";
-import type { BackViewLayoutWithAuto } from "../props/depth-0/BackViewProp";
 import type { ControlPanelThemeWithAuto } from "../props/depth-0/ControlPanelProp";
-import type { HintFaceletStyleWithAuto } from "../props/depth-0/HintFaceletProp";
-import type { IndexerStrategyName } from "../props/depth-0/IndexerConstructorRequestProp";
-import type { ViewerLinkPageWithAuto } from "../props/depth-0/ViewerLinkProp";
 import type { VisualizationFormatWithAuto } from "../props/depth-0/VisualizationProp";
 import { TwistyPlayerModel } from "../props/TwistyPlayerModel";
 import { Twisty2DSceneWrapper } from "./2D/Twisty2DSceneWrapper";
@@ -24,9 +17,10 @@ import { TwistyButtonsV2 } from "./control-panel/TwistyButtonsV2";
 import { TwistyScrubberV2 } from "./control-panel/TwistyScrubberV2";
 import type { TwistyAnimationControllerDelegate } from "./TwistyAnimationController";
 import { TwistyPlayerController } from "./TwistyPlayerController";
+import { TwistyPlayerSettable } from "./TwistyPlayerSettable";
 
 export class TwistyPlayerV2
-  extends ManagedCustomElement
+  extends TwistyPlayerSettable
   implements TwistyAnimationControllerDelegate
 {
   model: TwistyPlayerModel = new TwistyPlayerModel();
@@ -117,166 +111,6 @@ export class TwistyPlayerV2
       this.#visualizationWrapper = newWrapper;
       this.#visualizationStrategy = strategy;
     }
-  }
-
-  set alg(newAlg: Alg | string) {
-    this.model.algProp.set(newAlg);
-  }
-
-  get alg(): never {
-    throw new Error("Cannot get `.alg` directly from a `TwistyPlayer`.");
-  }
-
-  set setup(newSetup: Alg | string) {
-    this.model.setupProp.set(newSetup);
-  }
-
-  get setup(): never {
-    throw new Error("Cannot get `.setup` directly from a `TwistyPlayer`.");
-  }
-
-  set experimentalSetupAnchor(anchor: SetupToLocation) {
-    this.model.setupAnchorProp.set(anchor);
-  }
-
-  get experimentalSetupAnchor(): never {
-    throw new Error("Cannot get `.anchor` directly from a `TwistyPlayer`.");
-  }
-
-  set puzzle(puzzleID: PuzzleID) {
-    this.model.puzzleProp.set(puzzleID);
-  }
-
-  get puzzle(): never {
-    throw new Error("Cannot get `.puzzle` directly from a `TwistyPlayer`.");
-  }
-
-  set timestamp(timestamp: MillisecondTimestamp) {
-    this.model.timestampRequestProp.set(timestamp);
-  }
-
-  get timestamp(): never {
-    throw new Error("Cannot get `.timestamp` directly from a `TwistyPlayer`.");
-  }
-
-  set hintFacelets(hintFaceletStyle: HintFaceletStyleWithAuto) {
-    this.model.hintFaceletProp.set(hintFaceletStyle);
-  }
-
-  get hintFacelets(): never {
-    throw new Error(
-      "Cannot get `.hintFacelets` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  set experimentalStickering(stickering: ExperimentalStickering) {
-    this.model.stickeringProp.set(stickering);
-  }
-
-  get experimentalStickering(): never {
-    throw new Error("Cannot get `.stickering` directly from a `TwistyPlayer`.");
-  }
-
-  set backView(backView: BackViewLayoutWithAuto) {
-    this.model.backViewProp.set(backView);
-  }
-
-  get backView(): never {
-    throw new Error("Cannot get `.backView` directly from a `TwistyPlayer`.");
-  }
-
-  set background(backgroundTheme: BackgroundThemeWithAuto) {
-    this.model.backgroundProp.set(backgroundTheme);
-  }
-
-  get background(): never {
-    throw new Error("Cannot get `.background` directly from a `TwistyPlayer`.");
-  }
-
-  set controlPanel(newControlPanel: ControlPanelThemeWithAuto) {
-    this.model.controlPanelProp.set(newControlPanel);
-  }
-
-  get controlPanel(): never {
-    throw new Error(
-      "Cannot get `.controlPanel` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  set visualization(visualizationFormat: VisualizationFormatWithAuto) {
-    this.model.visualizationFormatProp.set(visualizationFormat);
-  }
-
-  get visualization(): never {
-    throw new Error(
-      "Cannot get `.visualization` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  set viewerLink(viewerLinkPage: ViewerLinkPageWithAuto) {
-    this.model.viewerLinkProp.set(viewerLinkPage);
-  }
-
-  get viewerLink(): never {
-    throw new Error("Cannot get `.viewerLink` directly from a `TwistyPlayer`.");
-  }
-
-  set cameraLatitude(latitude: number) {
-    this.model.orbitCoordinatesRequestProp.set({ latitude });
-  }
-
-  get cameraLatitude(): never {
-    throw new Error(
-      "Cannot get `.cameraLatitude` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  set cameraLongitude(longitude: number) {
-    this.model.orbitCoordinatesRequestProp.set({ longitude });
-  }
-
-  get cameraLongitude(): never {
-    throw new Error(
-      "Cannot get `.cameraLongitude` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  set cameraDistance(distance: number) {
-    this.model.orbitCoordinatesRequestProp.set({ distance });
-  }
-
-  get cameraDistance(): never {
-    throw new Error(
-      "Cannot get `.cameraDistance` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  set cameraLatitudeLimit(latitudeLimit: number) {
-    this.model.latitudeLimitProp.set(latitudeLimit);
-  }
-
-  get cameraLatitudeLimit(): never {
-    throw new Error(
-      "Cannot get `.cameraLatitudeLimit` directly from a `TwistyPlayer`.",
-    );
-  }
-
-  // TODO: this needs a much better name.
-  set indexer(indexer: IndexerStrategyName) {
-    this.model.indexerConstructorRequestProp.set(indexer);
-  }
-
-  get indexer(): never {
-    throw new Error("Cannot get `.indexer` directly from a `TwistyPlayer`.");
-  }
-
-  // TODO: this needs a much better name.
-  set tempoScale(newTempoScale: number) {
-    this.model.tempoScaleProp.set(newTempoScale);
-  }
-
-  get tempoScale(): never {
-    throw new Error("Cannot get `.tempoScale` directly from a `TwistyPlayer`.");
   }
 
   jumpToStart(options?: { flash: boolean }): void {
