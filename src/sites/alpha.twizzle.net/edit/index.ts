0381472b5eccb46fb57bbf1c9384a6f342e7c110
Lucas Garron <code@garron.net>
2021-08-31 23:23:57 -0700

    [twizzle/edit] Add a `debug-show-render-stats` param.

--- src/sites/alpha.twizzle.net/edit/index.ts
+++ src/sites/alpha.twizzle.net/edit/index.ts
@@ -3,6 +3,7 @@ import { App } from "./app";
 import { getURLParam } from "./url-params";
 import { useNewFaceNames } from "../../../cubing/puzzle-geometry";
 import { Alg } from "../../../cubing/alg";
+import { showStats } from "../../../cubing/twisty/views/3D/Twisty3DVantage";
 
 useNewFaceNames(true);
 
@@ -10,6 +11,11 @@ window.addEventListener("DOMContentLoaded", () => {
   if (!getURLParam("debug-js")) {
     return;
   }
+
+  if (getURLParam("debug-show-render-stats")) {
+    showStats(true);
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const appElement = document.querySelector("app")!;
   let experimentalSetupAlg: Alg;
