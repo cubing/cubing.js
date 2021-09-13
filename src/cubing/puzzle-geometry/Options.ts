c91a3092c3c7a3fd6da1020305e7d9f7ae6c1bf0
Lucas Garron <code@garron.net>
2021-09-12 20:11:05 -0700

    [puzzle-geometry] Move PG3D option list converter into `Options.ts`.

--- src/cubing/puzzle-geometry/Options.ts
+++ src/cubing/puzzle-geometry/Options.ts
@@ -48,3 +48,125 @@ export function parseoptions(
   }
   return argp;
 }
+
+type FaceName = string;
+type OrientationDirection = [number, number, number];
+export type FaceBasedOrientationDescription = [
+  [FaceName, OrientationDirection],
+  [FaceName, OrientationDirection],
+];
+export type BaseFaceCount = 4 | 6 | 8 | 12 | 20;
+export type FaceBasedOrientationDescriptionLookup = Record<
+  BaseFaceCount,
+  FaceBasedOrientationDescription
+>;
+
+function asstructured(v: any): any {
+  if (typeof v === "string") {
+    return JSON.parse(v);
+  }
+  return v;
+}
+function asboolean(v: any): boolean {
+  if (typeof v === "string") {
+    if (v === "false") {
+      return false;
+    }
+    return true;
+  } else {
+    return v ? true : false;
+  }
+}
+
+export class PuzzleGeometryOptionsObject {
+  verbosity: number = 0; // verbosity (console.log)
+  allMoves: boolean = false; // generate all slice moves in ksolve
+  outerBlockMoves: boolean; // generate outer block moves
+  vertexMoves: boolean; // generate vertex moves
+  addRotations: boolean; // add symmetry information to ksolve output
+  moveList?: string[]; // move list to generate
+  puzzleOrientation: FaceBasedOrientationDescription; // single puzzle orientation from options
+  puzzleOrientations: FaceBasedOrientationDescriptionLookup; // puzzle orientation override object from options // TODO: is this needed?
+
+  // TODO: Group these into a single object?
+  includeCornerOrbits: boolean = true; // include corner orbits
+  includeCenterOrbits: boolean = true; // include center orbits
+  includeEdgeOrbits: boolean = true; // include edge orbits
+  // Overrides the previous options.
+  excludeOrbits: string[] = []; // exclude these orbits
+
+  grayCorners: boolean = false; // make corner sets gray
+  grayCenters: boolean = false; // make center sets gray
+  grayEdges: boolean = false; // make edge sets gray
+
+  fixedOrientation: boolean = false; // eliminate any orientations
+  fixedPieceType: "" | "e" | "v" | "f" = ""; // fix a piece?
+  orientCenters: boolean = false; // orient centers?
+
+  optimizeOrbits: boolean = false; // optimize PermOri
+
+  scrambleAmount: number = 0; // scramble?
+}
+
+export type PuzzleGeometryOptions = Partial<PuzzleGeometryOptionsObject>;
+
+export function optionListToObject(
+  optionlist?: any[],
+): PuzzleGeometryOptionsObject {
+  const options = new PuzzleGeometryOptionsObject();
+  if (optionlist !== undefined) {
+    if (optionlist.length % 2 !== 0) {
+      throw new Error("Odd length in option list?");
+    }
+    for (let i = 0; i < optionlist.length; i += 2) {
+      if (optionlist[i] === "verbose") {
+        options.verbosity++;
+      } else if (optionlist[i] === "quiet") {
+        options.verbosity = 0;
+      } else if (optionlist[i] === "allmoves") {
+        options.allMoves = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "outerblockmoves") {
+        options.outerBlockMoves = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "vertexmoves") {
+        options.vertexMoves = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "rotations") {
+        options.addRotations = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "cornersets") {
+        options.includeCornerOrbits = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "centersets") {
+        options.includeCenterOrbits = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "edgesets") {
+        options.includeEdgeOrbits = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "omit") {
+        options.excludeOrbits = optionlist[i + 1];
+      } else if (optionlist[i] === "graycorners") {
+        options.grayCorners = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "graycenters") {
+        options.grayCenters = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "grayedges") {
+        options.grayEdges = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "movelist") {
+        options.moveList = asstructured(optionlist[i + 1]);
+      } else if (optionlist[i] === "killorientation") {
+        options.fixedOrientation = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "optimize") {
+        options.optimizeOrbits = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "scramble") {
+        options.scrambleAmount = optionlist[i + 1];
+      } else if (optionlist[i] === "fix") {
+        options.fixedPieceType = optionlist[i + 1];
+      } else if (optionlist[i] === "orientcenters") {
+        options.orientCenters = asboolean(optionlist[i + 1]);
+      } else if (optionlist[i] === "puzzleorientation") {
+        options.puzzleOrientation = asstructured(optionlist[i + 1]);
+      } else if (optionlist[i] === "puzzleorientations") {
+        options.puzzleOrientations = asstructured(optionlist[i + 1]);
+      } else {
+        throw new Error(
+          "Bad option while processing option list " + optionlist[i],
+        );
+      }
+    }
+  }
+  return options;
+}
--- src/cubing/puzzle-geometry/PuzzleGeometry.ts
+++ src/cubing/puzzle-geometry/PuzzleGeometry.ts
@@ -21,6 +21,13 @@ import {
   SkewbNotationMapper,
   TetraminxNotationMapper,
 } from "./notation-mapping";
+import {
+  BaseFaceCount,
+  FaceBasedOrientationDescription,
+  FaceBasedOrientationDescriptionLookup,
+  optionListToObject,
+  PuzzleGeometryOptionsObject,
+} from "./Options";
 import { iota, Perm, zeros } from "./Perm";
 import {
   externalName,
@@ -368,19 +375,6 @@ function defaultfaceorders(): any {
  *  this information is explicitly given in the twizzle app file.
  */
 // TODO: change this back to a const JSON definition.
-
-type FaceName = string;
-type OrientationDirection = [number, number, number];
-type FaceBasedOrientationDescription = [
-  [FaceName, OrientationDirection],
-  [FaceName, OrientationDirection],
-];
-type BaseFaceCount = 4 | 6 | 8 | 12 | 20;
-type FaceBasedOrientationDescriptionLookup = Record<
-  BaseFaceCount,
-  FaceBasedOrientationDescription
->;
-
 function defaultOrientations(): FaceBasedOrientationDescriptionLookup {
   return {
     4: [
@@ -547,38 +541,6 @@ function toFaceCoords(q: Face, maxdist: number): number[] {
   return r;
 }
 
-class PuzzleGeometryOptionsObject {
-  verbosity: number = 0; // verbosity (console.log)
-  allMoves: boolean = false; // generate all slice moves in ksolve
-  outerBlockMoves: boolean; // generate outer block moves
-  vertexMoves: boolean; // generate vertex moves
-  addRotations: boolean; // add symmetry information to ksolve output
-  moveList?: string[]; // move list to generate
-  puzzleOrientation: FaceBasedOrientationDescription; // single puzzle orientation from options
-  puzzleOrientations: FaceBasedOrientationDescriptionLookup; // puzzle orientation override object from options // TODO: is this needed?
-
-  // TODO: Group these into a single object?
-  includeCornerOrbits: boolean = true; // include corner orbits
-  includeCenterOrbits: boolean = true; // include center orbits
-  includeEdgeOrbits: boolean = true; // include edge orbits
-  // Overrides the previous options.
-  excludeOrbits: string[] = []; // exclude these orbits
-
-  grayCorners: boolean = false; // make corner sets gray
-  grayCenters: boolean = false; // make center sets gray
-  grayEdges: boolean = false; // make edge sets gray
-
-  fixedOrientation: boolean = false; // eliminate any orientations
-  fixedPieceType: "" | "e" | "v" | "f" = ""; // fix a piece?
-  orientCenters: boolean = false; // orient centers?
-
-  optimizeOrbits: boolean = false; // optimize PermOri
-
-  scrambleAmount: number = 0; // scramble?
-}
-
-export type PuzzleGeometryOptions = Partial<PuzzleGeometryOptionsObject>;
-
 export class PuzzleGeometry {
   public args: string = "";
   public rotations: Quat[]; // all members of the rotation group
@@ -631,79 +593,10 @@ export class PuzzleGeometry {
   public addNotationMapper: string = "";
   public setReidOrder: boolean = false;
 
-  private options = new PuzzleGeometryOptionsObject();
+  private options: PuzzleGeometryOptionsObject;
 
   constructor(shape: string, cuts: string[][], optionlist: any[] | undefined) {
-    function asstructured(v: any): any {
-      if (typeof v === "string") {
-        return JSON.parse(v);
-      }
-      return v;
-    }
-    function asboolean(v: any): boolean {
-      if (typeof v === "string") {
-        if (v === "false") {
-          return false;
-        }
-        return true;
-      } else {
-        return v ? true : false;
-      }
-    }
-    if (optionlist !== undefined) {
-      if (optionlist.length % 2 !== 0) {
-        throw new Error("Odd length in option list?");
-      }
-      for (let i = 0; i < optionlist.length; i += 2) {
-        if (optionlist[i] === "verbose") {
-          this.options.verbosity++;
-        } else if (optionlist[i] === "quiet") {
-          this.options.verbosity = 0;
-        } else if (optionlist[i] === "allmoves") {
-          this.options.allMoves = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "outerblockmoves") {
-          this.options.outerBlockMoves = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "vertexmoves") {
-          this.options.vertexMoves = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "rotations") {
-          this.options.addRotations = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "cornersets") {
-          this.options.includeCornerOrbits = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "centersets") {
-          this.options.includeCenterOrbits = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "edgesets") {
-          this.options.includeEdgeOrbits = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "omit") {
-          this.options.excludeOrbits = optionlist[i + 1];
-        } else if (optionlist[i] === "graycorners") {
-          this.options.grayCorners = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "graycenters") {
-          this.options.grayCenters = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "grayedges") {
-          this.options.grayEdges = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "movelist") {
-          this.options.moveList = asstructured(optionlist[i + 1]);
-        } else if (optionlist[i] === "killorientation") {
-          this.options.fixedOrientation = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "optimize") {
-          this.options.optimizeOrbits = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "scramble") {
-          this.options.scrambleAmount = optionlist[i + 1];
-        } else if (optionlist[i] === "fix") {
-          this.options.fixedPieceType = optionlist[i + 1];
-        } else if (optionlist[i] === "orientcenters") {
-          this.options.orientCenters = asboolean(optionlist[i + 1]);
-        } else if (optionlist[i] === "puzzleorientation") {
-          this.options.puzzleOrientation = asstructured(optionlist[i + 1]);
-        } else if (optionlist[i] === "puzzleorientations") {
-          this.options.puzzleOrientations = asstructured(optionlist[i + 1]);
-        } else {
-          throw new Error(
-            "Bad option while processing option list " + optionlist[i],
-          );
-        }
-      }
-    }
+    this.options = optionListToObject(optionlist);
     this.args = shape + " " + cuts.map((_) => _.join(" ")).join(" ");
     if (optionlist) {
       this.args += " " + optionlist.join(" ");
