21c454d94782d06fd4dbb78196aaae8020a68fcb
Lucas Garron <code@garron.net>
2021-10-01 22:10:09 -0700

    FREEZE.indexed

--- script/dev.js
+++ script/dev.js
@@ -1,3 +1,19 @@
-import { createServer } from "http-server/lib/http-server";
+import { mirrorDirectories } from "mirror-directories";
+import liveServer from "live-server";
 
-createServer();
+mirrorDirectories([
+  {
+    srcDirs: ["src/sites/", "dist/dev/esbuild/"],
+    destDirs: ["dist/dev/serve"],
+    rename: true,
+  },
+]);
+
+var params = {
+  host: "localhost",
+  port: 3333,
+  root: "./dist/dev/serve",
+  open: true,
+  logLevel: 2,
+};
+liveServer.start(params);
