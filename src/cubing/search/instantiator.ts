08982569ace8477dc2b1d0d9421a88d0ee66892d
Lucas Garron <code@garron.net>
2021-10-02 14:12:22 -0700

    FREEZE.indexed

--- src/cubing/search/instantiator.ts
+++ src/cubing/search/instantiator.ts
@@ -43,8 +43,7 @@ export async function relativeURLWorkerTest(): Promise<void> {
 export async function instantiateWorker(): Promise<WorkerInsideAPI> {
   await relativeURLWorkerTest();
 
-  // const { workerSource } = await import("./worker-inside-generated-string.js");
-  const workerSource = "console.log('foo');";
+  const { workerSource } = await import("./worker-inside-generated-string.js");
   // console.log({ workerSource });
 
   const worker = await constructWorkerFromString(workerSource);
