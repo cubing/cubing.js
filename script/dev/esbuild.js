a4b79ca78fcc454b313f6b2d5ca81eadbb5d06ef
Lucas Garron <code@garron.net>
2021-10-02 14:20:39 -0700

    FREEZE.indexed

--- script/dev/esbuild.js
+++ script/dev/esbuild.js
@@ -1,5 +1,6 @@
 import * as esbuild from "esbuild";
 import { join } from "path";
+import { build, searchWorkerTarget } from "../build/index.js";
 import { listFilesWithSuffix } from "./ls.js";
 
 let currentBuildResult = null;
@@ -8,6 +9,9 @@ export async function restartEsbuild(entryRootPath, outputRootPath) {
   if (currentBuildResult) {
     (await currentBuildResult).stop();
   }
+
+  await build(searchWorkerTarget, true);
+
   const entryPoints = await listFilesWithSuffix(
     join(process.cwd(), entryRootPath),
     ".ts",
