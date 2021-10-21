54d94507002f4597c9af06c750f280309997e04d
Lucas Garron <code@garron.net>
2021-10-21 11:21:52 -0700

    [test-dist-esm-parcel] Copy the files to `.temp` for the test.
    This allows us to exclude/clean it more easily.

--- script/test/dist/esm/parcel/serve-parcel.js
+++ script/test/dist/esm/parcel/serve-parcel.js
@@ -1,15 +1,22 @@
+import { mkdirSync } from "fs";
+import { join } from "path";
 import { execPromise } from "../../../../lib/execPromise.js";
 
 export const port = 1236;
 
-const packagePath = new URL("./parcel-package", import.meta.url).pathname;
+const packageSrcPath = new URL("./parcel-package", import.meta.url).pathname; // relative to this file
+const packageTempRoot = new URL("../../../../../.temp", import.meta.url)
+  .pathname;
+const packageTempPath = join(packageTempRoot, "parcel-package");
 
 export async function installServer() {
-  await execPromise("npm install", { cwd: packagePath });
+  mkdirSync(packageTempRoot, { recursive: true });
+  await execPromise(`cp -R ${packageSrcPath} ${packageTempRoot}`); // TODO: cpSync?
+  await execPromise("npm install", { cwd: packageTempPath });
 }
 
 export function startServer() {
   execPromise(`npx parcel serve --port ${port} index.html`, {
-    cwd: packagePath,
-  });
+    cwd: packageTempPath,
+  }).then(console.error);
 }
