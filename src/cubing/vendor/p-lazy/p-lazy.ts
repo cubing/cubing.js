152a14a96952b87259dedb8313fefe2f002bcc0b
Lucas Garron <code@garron.net>
2021-10-21 23:11:40 -0700

    [search] Make `p-lazy.ts` pass the linter.

--- src/cubing/vendor/p-lazy/p-lazy.ts
+++ src/cubing/vendor/p-lazy/p-lazy.ts
@@ -1,62 +1,51 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 // TODO: Use private class fields when ESLint support it.
 
-// @ts-ignore
 export class PLazy<T> extends Promise<T> {
-  // @ts-ignore
   constructor(executor) {
     super((resolve) => {
-      // @ts-ignore
       resolve();
     });
 
-    // @ts-ignore
     this._executor = executor;
   }
 
-  // @ts-ignore
   static from(function_) {
-    // @ts-ignore
     return new PLazy((resolve) => {
       resolve(function_());
     });
   }
 
-  // @ts-ignore
   static resolve(value) {
-    // @ts-ignore
     return new PLazy((resolve) => {
       resolve(value);
     });
   }
 
-  // @ts-ignore
   static reject(error) {
-    // @ts-ignore
     return new PLazy((_resolve, reject) => {
       reject(error);
     });
   }
 
-  // @ts-ignore
   then(onFulfilled, onRejected) {
-    // @ts-ignore
     this._promise = this._promise || new Promise(this._executor);
-    // @ts-ignore
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return this._promise.then(onFulfilled, onRejected);
   }
 
-  // @ts-ignore
   catch(onRejected) {
-    // @ts-ignore
     this._promise = this._promise || new Promise(this._executor);
-    // @ts-ignore
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return this._promise.catch(onRejected);
   }
 }
 
-// @ts-ignore
 export function from<T>(function_): Promise<T> {
-  // @ts-ignore
   return new PLazy((resolve) => {
     resolve(function_());
   });
