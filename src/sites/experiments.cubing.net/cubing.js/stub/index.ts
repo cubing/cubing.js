cafafc78d601952b0b819fbffde67b5aac26dd09
Lucas Garron <code@garron.net>
2021-12-30 02:00:03 -0800

    [experiments] Reset stub page.

--- src/sites/experiments.cubing.net/cubing.js/stub/index.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/index.ts
@@ -1,25 +1,2 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
-// import { Move } from "../../../../cubing/alg";
-// import { TwistyPlayer } from "../../../../cubing/twisty";
-
-// const player = document.body.appendChild(new TwistyPlayer());
-
-// player.experimentalAddMove(new Move("R"));
-
-// setTimeout(() => {
-//   player.experimentalAddMove(new Move("U"));
-// }, 1000);
-
-// setTimeout(() => {
-//   player.experimentalAddMove(new Move("U"));
-// }, 1200);
-
-// setTimeout(() => {
-//   player.experimentalAddMove(new Move("U'"));
-// }, 1300);
-
-// // setTimeout(() => {
-// //   player.experimentalAddMove(new Move("U"));
-// // }, 2000);
