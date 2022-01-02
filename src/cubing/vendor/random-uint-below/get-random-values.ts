c8f3b13271297cb5909ca598c02f04ae335472e9
Lucas Garron <code@garron.net>
2022-01-02 00:53:46 -0800

    [random-uint-below] Use `node`'s new built-in web crypto.

--- src/cubing/vendor/random-uint-below/get-random-values.ts
+++ src/cubing/vendor/random-uint-below/get-random-values.ts
@@ -6,6 +6,11 @@ export type GetRandomValuesFunction = (arr: Uint32Array) => void;
 // So we cach a single import reference. We avoid populating it until we first need it, so that we don't attempt to perform the import in environments that don't need or have it (e.g. browsers, `deno`).
 let cryptoPromise: Promise<typeof import("crypto")> | null = null;
 
+// `@types/node` is... lacking. This type may cause an error in the future, at which point we can hopefully use `@types/node` directly.
+type NodeWebCrypto = typeof import("crypto").webcrypto & {
+  getRandomValues: GetRandomValuesFunction;
+};
+
 // We could use top-level await to define this more statically, but that has limited transpilation support.
 export async function getRandomValuesFactory(): Promise<GetRandomValuesFunction> {
   const hasWebCrypto =
@@ -15,24 +20,8 @@ export async function getRandomValuesFactory(): Promise<GetRandomValuesFunction>
   if (hasWebCrypto) {
     return crypto.getRandomValues.bind(crypto) as GetRandomValuesFunction;
   } else {
-    const nodeCrypto = await (cryptoPromise ??= import("crypto"));
-    return (arr: Uint32Array) => {
-      if (!(arr instanceof Uint32Array)) {
-        throw new Error(
-          "The getRandomValues() shim only takes unsigned 32-bit int arrays",
-        );
-      }
-      const bytes = nodeCrypto.randomBytes(arr.length * 4);
-      const uint32_list = [];
-      for (let i = 0; i < arr.length; i++) {
-        uint32_list.push(
-          (bytes[i * 4 + 0] << 24) +
-            (bytes[i * 4 + 1] << 16) +
-            (bytes[i * 4 + 2] << 8) +
-            (bytes[i * 4 + 3] << 0),
-        );
-      }
-      arr.set(uint32_list);
-    };
+    const nodeWebcrypto = (await (cryptoPromise ??= import("crypto")))
+      .webcrypto as NodeWebCrypto;
+    return nodeWebcrypto.getRandomValues;
   }
 }
