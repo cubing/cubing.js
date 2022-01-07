fd8c0ab9967a20482b8df515c0a4e90baf5bbdd0
Lucas Garron <code@garron.net>
2022-01-06 18:36:38 -0800

    FREEZE.indexed

--- src/sites/alpha.twizzle.net/explore/app.ts
+++ src/sites/alpha.twizzle.net/explore/app.ts
@@ -1,3 +1,4 @@
+import type { PuzzleGeometry } from "../../../cubing/puzzle-geometry";
 import type { TwistyAlgEditor, TwistyPlayer } from "../../../cubing/twisty";
 import { constructTwistyPlayer } from "./config";
 
@@ -10,5 +11,74 @@ export class TwizzleExplorerApp {
 
     this.twistyAlgEditor = document.querySelector("twisty-alg-editor")!;
     this.twistyAlgEditor.twistyPlayer = this.twistyPlayer;
+
+    new ActionsDropdown(this);
+  }
+
+  // TODO: Find out how to avoid the need for this.
+  async puzzleGeometry(): Promise<PuzzleGeometry> {
+    const puzzleLoader =
+      await this.twistyPlayer.experimentalModel.puzzleLoaderProp.get();
+
+    if (!puzzleLoader.pg) {
+      throw new Error("could not get PG from puzzle loader");
+    }
+    return puzzleLoader.pg();
+  }
+
+  showText(text: string): void {
+    // TODO
+    console.log(text);
+    navigator.clipboard.writeText(text);
+  }
+}
+
+class ActionsDropdown {
+  element = document.body.querySelector("#actions") as HTMLInputElement;
+  constructor(private app: TwizzleExplorerApp) {
+    this.element.addEventListener("change", this.onChange.bind(this));
+  }
+
+  async onChange() {
+    const action = this.element.value;
+    switch (action) {
+      case "gap":
+        this.app.showText((await this.app.puzzleGeometry()).writegap());
+        break;
+      case "ss": {
+        const lines: string[] = [];
+        (await this.app.puzzleGeometry()).writeSchreierSims((line) =>
+          lines.push(line),
+        );
+        this.app.showText(lines.join("\n"));
+        break;
+      }
+      case "canon": {
+        const lines: string[] = [];
+        (await this.app.puzzleGeometry()).showcanon((line) => lines.push(line));
+        this.app.showText(lines.join("\n"));
+        break;
+      }
+      case "ksolve":
+        this.app.showText(
+          (await this.app.puzzleGeometry()).writeksolve("TwizzlePuzzle"),
+        );
+        break;
+      case "svgcmd": {
+        const is3D =
+          (await this.app.twistyPlayer.experimentalModel.visualizationFormatProp.get()) !==
+          "2D"; // TODO
+        this.app.showText(
+          (await this.app.puzzleGeometry()).generatesvg(800, 500, 10, is3D),
+        );
+        break;
+      }
+      case "screenshot":
+      case "screenshot-back":
+        this.app.twistyPlayer.experimentalDownloadScreenshot(); // TODO: back!
+        break;
+      default:
+        alert(`Action ${action} not handled yet.`);
+    }
   }
 }
