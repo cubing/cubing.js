ffa19f47b0fb259d26c18c6acc90e928fd1d9f25
Lucas Garron <code@garron.net>
2022-01-19 14:12:20 -0800

    [puzzles] Attach `PGNotation` in `customPGPuzzleLoader`.

--- src/cubing/puzzles/customPGPuzzleLoader.ts
+++ src/cubing/puzzles/customPGPuzzleLoader.ts
@@ -1,6 +1,7 @@
 import { KPuzzle, KPuzzleDefinition } from "../kpuzzle";
 import type { PuzzleGeometry } from "../puzzle-geometry";
 import type { PuzzleDescriptionString } from "../puzzle-geometry/PGPuzzles";
+import { PGNotation } from "../puzzle-geometry/PuzzleGeometry";
 import type { PuzzleLoader } from "./PuzzleLoader";
 
 // TODO: modify this to handle TwistyPlayer options
@@ -15,17 +16,17 @@ export async function descAsyncGetPuzzleGeometry(
   });
 }
 
-// TODO: can we cache the puzzleGeometry to avoid duplicate calls, without
-// wasting memory? Maybe just save the latest one for successive calls about the
-// same puzzle?
-export async function dsecAsyncGetDef(
+// TODO: dedup with `cubing/puzzles`
+export async function asyncGetKPuzzle(
   desc: PuzzleDescriptionString,
-): Promise<KPuzzleDefinition> {
-  const definition = (
-    await descAsyncGetPuzzleGeometry(desc)
-  ).getKPuzzleDefinition(true);
-  definition.name = `description: ${desc}`;
-  return definition;
+): Promise<KPuzzle> {
+  const pg = await descAsyncGetPuzzleGeometry(desc);
+  const kpuzzleDefinition: KPuzzleDefinition = pg.getKPuzzleDefinition(true);
+  kpuzzleDefinition.name = `description: ${desc}`;
+  const pgNotation = new PGNotation(pg, pg.getOrbitsDef(true));
+  return new KPuzzle(kpuzzleDefinition, {
+    experimentalPGNotation: pgNotation,
+  });
 }
 
 // TODO: Can we avoid relying on IDs to deduplicate work at higher levels?
@@ -40,13 +41,12 @@ export function customPGPuzzleLoader(
   },
 ): PuzzleLoader {
   const customID = nextCustomID++;
-  const defPromise = dsecAsyncGetDef(desc);
   let cachedKPuzzle: Promise<KPuzzle> | null = null;
   const puzzleLoader: PuzzleLoader = {
     id: `custom-${customID}`,
     fullName: info?.fullName ?? `Custom Puzzle (instance #${customID})`,
     kpuzzle: async () => {
-      return (cachedKPuzzle ??= (async () => new KPuzzle(await defPromise))());
+      return (cachedKPuzzle ??= asyncGetKPuzzle(desc));
     },
     svg: async () => {
       const pg = await descAsyncGetPuzzleGeometry(desc);
