ae770e0b53d26b04af95273d48a6109745f14ef4
Lucas Garron <code@garron.net>
2022-01-18 21:54:00 -0800

    FREEZE.unindexed

--- src/sites/experiments.cubing.net/cubing.js/stub/index.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/index.ts
@@ -1,14 +1,12 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
 
-import { KPuzzle } from "../../../../cubing/kpuzzle/KPuzzle";
 import { puzzles } from "../../../../cubing/puzzles";
 
 (async () => {
-  const def = await puzzles["4x4x4"].def();
-  const kpuzzle = new KPuzzle(def);
+  const kpuzzle = await puzzles["4x4x4"].kpuzzle();
 
-  const tt = kpuzzle.algToTransformation("R U R'").applyAlg("R U' R'");
+  const tt = kpuzzle.algToTransformation("R U R'").apply("R U'").apply("R'");
 
   console.log(tt.isIdentityTransformation());
 })();
