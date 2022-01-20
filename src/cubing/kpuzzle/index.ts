e039ff5be33cfba5981bdd8346a303afb579e40a
Lucas Garron <code@garron.net>
2022-01-20 02:17:42 -0800

    [kpuzzle] Change `KTransformation` to a value export.

--- src/cubing/kpuzzle/index.ts
+++ src/cubing/kpuzzle/index.ts
@@ -1,12 +1,12 @@
 export { KPuzzle } from "./KPuzzle";
-export type { KTransformation } from "./KTransformation";
-export {
+export type {
   KPuzzleDefinition,
-  KTransformationData,
   KStateData,
+  KTransformationData,
 } from "./KPuzzleDefinition";
 export { KState } from "./KState";
+export { KTransformation } from "./KTransformation";
 
-import { KPuzzle } from "./KPuzzle";
 import { cube3x3x3KPuzzleDefinition } from "./3x3x3/3x3x3.kpuzzle.json";
+import { KPuzzle } from "./KPuzzle";
 export const experimental3x3x3KPuzzle = new KPuzzle(cube3x3x3KPuzzleDefinition);
