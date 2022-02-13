f5c7121aed12e3cb713f60524c24575694c070fc
Lucas Garron <code@garron.net>
2022-02-12 17:09:40 -0800

    Example of syncing a timestamp.

--- src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
@@ -1,2 +1,22 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
+
+import { TwistyPlayer } from "../../../../cubing/twisty";
+
+const player1 = document.body.appendChild(
+  new TwistyPlayer({
+    alg: "[F: [U, R]]",
+  }),
+);
+const player2 = document.body.appendChild(
+  new TwistyPlayer({
+    alg: "[F': [U', L']]",
+    controlPanel: "none",
+  }),
+);
+
+player1.experimentalModel.detailedTimelineInfo.addFreshListener(
+  (detailedTimelineInfo) => {
+    player2.timestamp = detailedTimelineInfo.timestamp;
+  },
+);
