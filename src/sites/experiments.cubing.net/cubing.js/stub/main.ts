748b864d2347ab1e7cf1ac8482cad4e0f022ac0a
Lucas Garron <code@garron.net>
2022-02-16 17:35:59 -0800

    FREEZE.unindexed

--- src/sites/experiments.cubing.net/cubing.js/stub/main.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/main.ts
@@ -1,36 +1,37 @@
-import { Twisty, TwistyAlgViewer } from "../../../../cubing/twisty";
+import { TwistyAlgViewer, TwistyPlayer } from "../../../../cubing/twisty";
 import { randomScrambleForEvent } from "../../../../cubing/scramble";
+import type { AlgWithIssues } from "../../../../cubing/twisty/model/props/puzzle/state/AlgProp";
 class App {
   // Example of getting an element from the page.
-  twistyPlayer: TwistyPlayer = document.querySelector("#twisty-player-left");
-  tpc: TwistyPlayer = document.querySelector("#twisty-player-center");
-  tpr: TwistyPlayer = document.querySelector("#twisty-player-right");
+  twistyPlayer: TwistyPlayer = document.querySelector("#twisty-player-left")!;
+  tpc: TwistyPlayer = document.querySelector("#twisty-player-center")!;
+  tpr: TwistyPlayer = document.querySelector("#twisty-player-right")!;
   // Example of creating a new element and adding it to the page.
   twistyAlgViewer = document.body.appendChild(
-    new TwistyAlgViewer({ twistyPlayer: this.twistyPlayer })
+    new TwistyAlgViewer({ twistyPlayer: this.twistyPlayer }),
   );
   constructor() {
     this.updateScramble();
     this.twistyPlayer.experimentalModel.detailedTimelineInfo.addFreshListener(
-        (detailedTimelineInfo) => {
-       this.tpc.timestamp = detailedTimelineInfo.timestamp;
-       this.tpr.timestamp = detailedTimelineInfo.timestamp;
-    },);
+      (detailedTimelineInfo) => {
+        this.tpc.timestamp = detailedTimelineInfo.timestamp;
+        this.tpr.timestamp = detailedTimelineInfo.timestamp;
+      },
+    );
     this.twistyPlayer.experimentalModel.alg.addFreshListener(
-         (algWithIssues: AlgWithIssues) => {
-       this.tpc.alg = algWithIssues.alg ;
-       this.tpr.alg = algWithIssues.alg ;
-    },);
+      (algWithIssues: AlgWithIssues) => {
+        this.tpc.alg = algWithIssues.alg;
+        this.tpr.alg = algWithIssues.alg;
+      },
+    );
   }
 
   async updateScramble() {
     const alg = await randomScrambleForEvent("333");
-    this.twistyPlayer.alg = alg ;
-    this.tpc.alg = alg ;
-    this.tpr.alg = alg ;
+    this.twistyPlayer.alg = alg;
+    this.tpc.alg = alg;
+    this.tpr.alg = alg;
   }
 }
 
-globalThis.onload = function() {
-   globalThis.app = new App();
-}
+(globalThis as any).app = new App();
