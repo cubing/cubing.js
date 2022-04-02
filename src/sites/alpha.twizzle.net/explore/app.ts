e3e340c05788f3ff409f32cbd7220ab39277c341
Lucas Garron <code@garron.net>
2022-04-02 01:21:43 -0700

    [twizzle/explore] Remove some logging statements.

--- src/sites/alpha.twizzle.net/explore/app.ts
+++ src/sites/alpha.twizzle.net/explore/app.ts
@@ -289,11 +289,9 @@ class SidePanel extends HTMLElement {
   }
 
   showTab(id: string) {
-    console.log(id);
     for (const child of Array.from(
       this.querySelector("panel-tabs")!.children,
     ) as HTMLElement[]) {
-      console.log(child);
       child.hidden = true;
     }
     document.getElementById(id)!.hidden = false;
