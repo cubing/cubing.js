11c0aeb160477fc9c3cdf313b671d6fdc70b145a
Lucas Garron <code@garron.net>
2022-07-08 03:57:45 -0400

    [puzzles] Move 3x3x3 orientation code into `cubing/puzzles`.

--- src/cubing/protocol/index.ts
+++ src/cubing/protocol/index.ts
@@ -4,8 +4,6 @@ export {
 } from "./binary/binary3x3x3";
 export { bufferToSpacedHex, spacedHexToBuffer } from "./binary/hex";
 
-export { normalizePuzzleOrientation as experimentalNormalizePuzzleOrientation } from "./binary/puzzle-orientation";
-
 export {
   binaryComponentsToReid3x3x3 as experimentalBinaryComponentsToReid3x3x3,
   twizzleBinaryToBinaryComponents as experimentalTwizzleBinaryToBinaryComponents,
--- src/cubing/puzzles/cubing-private/index.ts
+++ src/cubing/puzzles/cubing-private/index.ts
@@ -1,9 +1,13 @@
 import { KPuzzle } from "../../kpuzzle";
-
-export { experimentalIs3x3x3Solved } from "../implementations/dynamic/3x3x3/puzzle-orientation";
-export { experimentalStickerings } from "../stickerings/puzzle-stickerings";
-
 import { cube3x3x3KPuzzleDefinition as experimentalCube3x3x3KPuzzleDefinition } from "../implementations/dynamic/3x3x3/3x3x3.kpuzzle.json";
+
+export {
+  experimentalIs3x3x3Solved,
+  normalize3x3x3Orientation as experimentalNormalize3x3x3Orientation,
+  puzzleOrientation3x3x3Cache as experimentalPuzzleOrientation3x3x3Cache,
+  puzzleOrientation3x3x3Idx as experimentalPuzzleOrientation3x3x3Idx,
+} from "../implementations/dynamic/3x3x3/puzzle-orientation";
+export { experimentalStickerings } from "../stickerings/puzzle-stickerings";
 export { experimentalCube3x3x3KPuzzleDefinition };
 
 /** @deprecated */
