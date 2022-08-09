cubing/cubing.js

commit 1fa728e3cb5ce66779bdeb6d55fd2d2d9b1f8196

FREEZE.indexed

src/sites/experiments.cubing.net/cubing.js/bluetooth/index.ts
@@ -1,10 +1,6 @@
 import { Alg } from "../../../../cubing/alg";
 import type { OrientationEvent } from "../../../../cubing/bluetooth";
-import {
-  connectSmartPuzzle,
-  debugKeyboardConnect,
-  MoveEvent,
-} from "../../../../cubing/bluetooth";
+import { debugKeyboardConnect, MoveEvent } from "../../../../cubing/bluetooth";
 import { TwistyAlgViewerV1, TwistyPlayerV1 } from "../../../../cubing/twisty";
 
 async function asyncSetup(twistyPlayer: TwistyPlayerV1): Promise<void> {
@@ -35,7 +31,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     // const acceptAllDevices = (document.querySelector(
     //   "#acceptAllDevices",
     // ) as HTMLInputElement).checked;
-    const puzzle = await connectSmartPuzzle();
+    const puzzle = await debugKeyboardConnect();
     (globalThis as any).puzzle = puzzle;
     try {
       const state = await puzzle.getState();

src/sites/experiments.cubing.net/cubing.js/stub/index.ts
@@ -10,7 +10,9 @@ player.experimentalAddMove(new Move("R"));
 
 setTimeout(() => {
   player.experimentalAddMove(new Move("U"));
+  console.log("pre-end");
   player.timestamp = "end";
+  console.log("post-end");
   player.experimentalModel.catchUpMoveProp.set({
     move: new Move("U"),
     amount: 0,

src/cubing/twisty/controllers/TwistyAnimationController.ts
@@ -120,6 +120,10 @@ export class TwistyAnimationController {
     this.model.catchUpMoveProp.addFreshListener(
       this.onCatchUpMoveProp.bind(this),
     ); // TODO
+
+    this.model.detailedTimelineInfoProp.addFreshListener((v) => {
+      console.log("timestamp", v.timestamp);
+    });
   }
 
   // TODO: Do we need this?
@@ -248,7 +252,7 @@ export class TwistyAnimationController {
       freshenerResult;
 
     // TODO: Get this without wasting time on the others?
-    if (playingInfo.playing === false && !this.catchUpHelper.pendingFrame) {
+    if (playingInfo.playing) {
       this.playing = false;
       // TODO: Ideally we'd cancel the anim frame from the top of this method.
       // But `this.scheduler.cancelAnimFrame();` might accidentally cancel a
@@ -326,6 +330,7 @@ export class TwistyAnimationController {
         });
       }
     }
+    console.log({ newTimestamp });
     this.lastDatestamp = frameDatestamp;
     this.lastTimestampPromise = Promise.resolve(newTimestamp); // TODO: Save this earlier? / Do we need to worry about the effecitve timestamp disagreeing?
     this.model.timestampRequestProp.set(

commit 71e5a5b1cd9ff0e224d30c3513282a2cc02e1562

[experiments/bluetooth] Fix bug that connected keyboard instead of puzzle. Fixes #196.
The bug was introduced while refactoring in 1fa728e3cb5ce66779bdeb6d55fd2d2d9b1f8196.
Thanks to @anicola for reporting.

src/sites/experiments.cubing.net/cubing.js/bluetooth/index.ts
@@ -1,5 +1,8 @@
 import { Alg } from "../../../../cubing/alg";
-import type { OrientationEvent } from "../../../../cubing/bluetooth";
+import {
+  connectSmartPuzzle,
+  OrientationEvent,
+} from "../../../../cubing/bluetooth";
 import { debugKeyboardConnect, MoveEvent } from "../../../../cubing/bluetooth";
 import { TwistyAlgViewer, TwistyPlayer } from "../../../../cubing/twisty";
 
@@ -33,7 +36,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     // const acceptAllDevices = (document.querySelector(
     //   "#acceptAllDevices",
     // ) as HTMLInputElement).checked;
-    const puzzle = await debugKeyboardConnect();
+    const puzzle = await connectSmartPuzzle();
     (globalThis as any).puzzle = puzzle;
     // TODO
     // try {