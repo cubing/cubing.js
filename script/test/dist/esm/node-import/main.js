dbe44d1096c5386690d81ade1d7554259c9d90f2
Lucas Garron <code@garron.net>
2022-09-10 13:56:51 -0700

    [test] Move `.mjs` files to `.js`.
    All our files are module files.

--- script/test/dist/esm/node-import/main.js
+++ script/test/dist/esm/node-import/main.js
@@ -1,8 +1,8 @@
 import { needFolder } from "../../../../lib/need-folder.js";
 
 needFolder(
-  new URL("../../../../../dist/esm/scramble", import.meta.url).pathname,
+  new URL("../../../../../dist/esm/alg", import.meta.url).pathname,
   "make build-esm",
 );
 
-import("./test.mjs");
+import("./test.js");
