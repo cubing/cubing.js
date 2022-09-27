6a0017879a975fedfbf902731fc6b5e3fba8413d
Lucas Garron <code@garron.net>
2022-09-26 20:46:08 -0700

    [script/bin/screenshot] Always `make build-sites`.

--- script/bin/screenshot.js
+++ script/bin/screenshot.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import { build } from "esbuild";
+import { execPromise } from "../lib/execPromise.js";
 
 const TS_FILE = new URL("./screenshot-src/main.ts", import.meta.url).pathname;
 const JS_FILE = new URL("./screenshot-src/main.js", import.meta.url).pathname;
 
+await execPromise("make build-sites"); // TODO: can we detect when *not* to run this?
 await build({
   format: "esm",
   entryPoints: [TS_FILE],
