f7861fdde287699eb21cdfd643e6b69a9ffb891f
Lucas Garron <code@garron.net>
2022-10-30 20:25:37 -0700

    Interior face.

--- src/cubing/puzzles/implementations/dynamic/side-events/melindas2x2x2x2.kpuzzle.svg.ts
+++ src/cubing/puzzles/implementations/dynamic/side-events/melindas2x2x2x2.kpuzzle.svg.ts
@@ -14,9 +14,21 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <g id="sticker-DL">
     <path d="m 00,0 10,10 -10,0 z" stroke-width="0.75px" stroke="black" stroke-linecap="butt" stroke-linejoin="round" />
   </g>
+  <g id="squished-sticker-UL">
+    <path d="m 0,0 5,0 -5,10 z" stroke-width="0.75px" stroke="black" stroke-linecap="butt" stroke-linejoin="round" />
+  </g>
+  <g id="squished-sticker-UR">
+    <path d="m 0,0 5,0 0,10 z" stroke-width="0.75px" stroke="black" stroke-linecap="butt" stroke-linejoin="round" />
+  </g>
+  <g id="squished-sticker-DR">
+    <path d="m 5,0 0,10 -5,0 z" stroke-width="0.75px" stroke="black" stroke-linecap="butt" stroke-linejoin="round" />
+  </g>
+  <g id="squished-sticker-DL">
+    <path d="m 00,0 5,10 -5,0 z" stroke-width="0.75px" stroke="black" stroke-linecap="butt" stroke-linejoin="round" />
+  </g>
 </defs>
 <g>
-<g id="UL" transform="translate(35, 10)">
+<g id="UL" transform="translate(45, 10)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: orange"/>
   <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: white"/>
   <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: white"/>
@@ -26,7 +38,7 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: white"/>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: orange"/>
 </g>
-<g id="UR" transform="translate(55, 10)">
+<g id="UR" transform="translate(65, 10)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
   <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: white"/>
   <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: white"/>
@@ -58,7 +70,7 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: limegreen"/>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: orange"/>
 </g>
-<g id="FR" transform="translate(55, 35)">
+<g id="FR" transform="translate(75, 35)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
   <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: limegreen"/>
   <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: limegreen"/>
@@ -69,7 +81,7 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: red"/>
 </g>
 
-<g id="R" transform="translate(80, 35)">
+<g id="R" transform="translate(100, 35)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
   <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: pink"/>
   <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: pink"/>
@@ -80,7 +92,7 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: red"/>
 </g>
 
-<g id="BR" transform="translate(105, 35)">
+<g id="BR" transform="translate(125, 35)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
   <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: #26f"/>
   <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: #26f"/>
@@ -90,7 +102,7 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: #26f"/>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: red"/>
 </g>
-<g id="BL" transform="translate(125, 35)">
+<g id="BL" transform="translate(145, 35)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: orange"/>
   <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: #26f"/>
   <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: #26f"/>
@@ -101,47 +113,47 @@ export const melindas2x2x2x2OrbitSVG = `<?xml version="1.0" encoding="UTF-8"?>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: orange"/>
 </g>
 
-<g id="DL" transform="translate(35, 60)">
+<g id="DL" transform="translate(45, 60)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: orange"/>
-  <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: white"/>
-  <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: white"/>
+  <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: yellow"/>
+  <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: yellow"/>
   <use id="CORNERS-l3-o0" xlink:href="#sticker-UR" transform="translate(10,  0)" style="fill: orange"/>
   <use id="CORNERS-l4-o0" xlink:href="#sticker-DL" transform="translate( 0, 10)" style="fill: orange"/>
-  <use id="CORNERS-l5-o0" xlink:href="#sticker-UR" transform="translate( 0, 10)" style="fill: white"/>
-  <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: white"/>
+  <use id="CORNERS-l5-o0" xlink:href="#sticker-UR" transform="translate( 0, 10)" style="fill: yellow"/>
+  <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: yellow"/>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: orange"/>
 </g>
-<g id="DR" transform="translate(55, 60)">
+<g id="DR" transform="translate(65, 60)">
   <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
-  <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: white"/>
-  <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: white"/>
+  <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: yellow"/>
+  <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: yellow"/>
   <use id="CORNERS-l3-o0" xlink:href="#sticker-UR" transform="translate(10,  0)" style="fill: red"/>
   <use id="CORNERS-l4-o0" xlink:href="#sticker-DL" transform="translate( 0, 10)" style="fill: red"/>
-  <use id="CORNERS-l5-o0" xlink:href="#sticker-UR" transform="translate( 0, 10)" style="fill: white"/>
-  <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: white"/>
+  <use id="CORNERS-l5-o0" xlink:href="#sticker-UR" transform="translate( 0, 10)" style="fill: yellow"/>
+  <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: yellow"/>
   <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: red"/>
 </g>
 
 <g style="opacity: 0.3;">
-<g id="IL" transform="translate(35, 85)">
-  <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: orange"/>
-  <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: purple"/>
-  <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: purple"/>
-  <use id="CORNERS-l3-o0" xlink:href="#sticker-UR" transform="translate(10,  0)" style="fill: orange"/>
-  <use id="CORNERS-l4-o0" xlink:href="#sticker-DL" transform="translate( 0, 10)" style="fill: orange"/>
-  <use id="CORNERS-l5-o0" xlink:href="#sticker-UR" transform="translate( 0, 10)" style="fill: purple"/>
-  <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: purple"/>
-  <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: orange"/>
+<g id="IL" transform="translate(55, 35)">
+  <use id="CORNERS-l0-o0" xlink:href="#squished-sticker-UL" transform="translate( 0,  0)" style="fill: orange"/>
+  <use id="CORNERS-l1-o0" xlink:href="#squished-sticker-DR" transform="translate( 0,  0)" style="fill: purple"/>
+  <use id="CORNERS-l2-o0" xlink:href="#squished-sticker-DL" transform="translate(5,  0)" style="fill: purple"/>
+  <use id="CORNERS-l3-o0" xlink:href="#squished-sticker-UR" transform="translate(5,  0)" style="fill: orange"/>
+  <use id="CORNERS-l4-o0" xlink:href="#squished-sticker-DL" transform="translate( 0, 10)" style="fill: orange"/>
+  <use id="CORNERS-l5-o0" xlink:href="#squished-sticker-UR" transform="translate( 0, 10)" style="fill: purple"/>
+  <use id="CORNERS-l6-o0" xlink:href="#squished-sticker-UL" transform="translate(5, 10)" style="fill: purple"/>
+  <use id="CORNERS-l7-o0" xlink:href="#squished-sticker-DR" transform="translate(5, 10)" style="fill: orange"/>
 </g>
-<g id="IR" transform="translate(55, 85)">
-  <use id="CORNERS-l0-o0" xlink:href="#sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
-  <use id="CORNERS-l1-o0" xlink:href="#sticker-DR" transform="translate( 0,  0)" style="fill: purple"/>
-  <use id="CORNERS-l2-o0" xlink:href="#sticker-DL" transform="translate(10,  0)" style="fill: purple"/>
-  <use id="CORNERS-l3-o0" xlink:href="#sticker-UR" transform="translate(10,  0)" style="fill: red"/>
-  <use id="CORNERS-l4-o0" xlink:href="#sticker-DL" transform="translate( 0, 10)" style="fill: red"/>
-  <use id="CORNERS-l5-o0" xlink:href="#sticker-UR" transform="translate( 0, 10)" style="fill: purple"/>
-  <use id="CORNERS-l6-o0" xlink:href="#sticker-UL" transform="translate(10, 10)" style="fill: purple"/>
-  <use id="CORNERS-l7-o0" xlink:href="#sticker-DR" transform="translate(10, 10)" style="fill: red"/>
+<g id="IR" transform="translate(65, 35)">
+  <use id="CORNERS-l0-o0" xlink:href="#squished-sticker-UL" transform="translate( 0,  0)" style="fill: red"/>
+  <use id="CORNERS-l1-o0" xlink:href="#squished-sticker-DR" transform="translate( 0,  0)" style="fill: purple"/>
+  <use id="CORNERS-l2-o0" xlink:href="#squished-sticker-DL" transform="translate(5,  0)" style="fill: purple"/>
+  <use id="CORNERS-l3-o0" xlink:href="#squished-sticker-UR" transform="translate(5,  0)" style="fill: red"/>
+  <use id="CORNERS-l4-o0" xlink:href="#squished-sticker-DL" transform="translate( 0, 10)" style="fill: red"/>
+  <use id="CORNERS-l5-o0" xlink:href="#squished-sticker-UR" transform="translate( 0, 10)" style="fill: purple"/>
+  <use id="CORNERS-l6-o0" xlink:href="#squished-sticker-UL" transform="translate(5, 10)" style="fill: purple"/>
+  <use id="CORNERS-l7-o0" xlink:href="#squished-sticker-DR" transform="translate(5, 10)" style="fill: red"/>
 </g>
 </g>
 </g>
