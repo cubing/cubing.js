725a1a0cff8f9cff7bfe63f33ad4e252cb733448
Lucas Garron <code@garron.net>
2022-12-01 22:24:44 -0800

    [experiments/twsearch/benchmark] Snazz up the page.

--- src/sites/experiments.cubing.net/cubing.js/twsearch/benchmark.ts
+++ src/sites/experiments.cubing.net/cubing.js/twsearch/benchmark.ts
@@ -1,11 +1,13 @@
 import { randomScrambleForEvent } from "../../../../cubing/search/outside";
 
-const elem = document.body.appendChild(document.createElement("div"));
+const elem = document.querySelector("#results") as HTMLDivElement;
+const latest = document.querySelector("#latest") as HTMLSpanElement;
 (async () => {
   for (let i = 0; i < 1000; i++) {
     const start = performance.now();
-    await randomScrambleForEvent("222");
+    const scramble = await randomScrambleForEvent("222");
     const ms = Math.floor(performance.now() - start);
+    latest.textContent = scramble.toString();
     elem.textContent += `${ms}ms, `; // This is slow-ish, but we're not measuring it.
   }
 })();
