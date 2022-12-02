ba144d46e58060cbf6b19c8c4a737dcd449ffc39
Lucas Garron <code@garron.net>
2022-12-01 22:59:28 -0800

    [experiments/twsearch/benchmark] Print the number of scrambles.

--- src/sites/experiments.cubing.net/cubing.js/twsearch/benchmark.ts
+++ src/sites/experiments.cubing.net/cubing.js/twsearch/benchmark.ts
@@ -5,13 +5,13 @@ const latest = document.querySelector("#latest") as HTMLSpanElement;
 const mean = document.querySelector("#mean") as HTMLSpanElement;
 (async () => {
   let total = 0;
-  for (let i = 0; i < 1000; i++) {
+  for (let n = 1; n <= 1000; n++) {
     const start = performance.now();
     const scramble = await randomScrambleForEvent("222");
     const ms = Math.floor(performance.now() - start);
     latest.textContent = scramble.toString();
     elem.textContent += `${ms}ms, `; // This is slow-ish, but we're not measuring it.
     total += ms;
-    mean.textContent = `${Math.floor(total / (i + 1))}ms`;
+    mean.textContent = `${Math.floor(total / n)}ms (${n} scrambles)`;
   }
 })();
