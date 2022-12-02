a6be97f66bd7faeacb31beec19ddc08216c633b8
Lucas Garron <code@garron.net>
2022-12-01 23:30:16 -0800

    [experiments/twsearch/benchmark] Record cancelling scrambles.

--- src/sites/experiments.cubing.net/cubing.js/twsearch/benchmark.ts
+++ src/sites/experiments.cubing.net/cubing.js/twsearch/benchmark.ts
@@ -5,6 +5,7 @@ const latest = document.querySelector("#latest") as HTMLSpanElement;
 const mean = document.querySelector("#mean") as HTMLSpanElement;
 (async () => {
   let total = 0;
+  let cancelling = 0;
   for (let n = 1; n <= 1000; n++) {
     const start = performance.now();
     const scramble = await randomScrambleForEvent("222");
@@ -12,6 +13,15 @@ const mean = document.querySelector("#mean") as HTMLSpanElement;
     latest.textContent = scramble.toString();
     elem.textContent += `${ms}ms, `; // This is slow-ish, but we're not measuring it.
     total += ms;
-    mean.textContent = `${Math.floor(total / n)}ms (${n} scrambles)`;
+    const unrepeated = scramble
+      .toString()
+      .replaceAll("2", "")
+      .replaceAll("'", "");
+    if (unrepeated.includes("L R") || unrepeated.includes("R L")) {
+      cancelling++;
+    }
+    mean.textContent = `${Math.floor(
+      total / n,
+    )}ms (${n} scrambles, ${cancelling} cancelling)`;
   }
 })();
