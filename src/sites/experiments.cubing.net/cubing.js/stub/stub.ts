5e293faf3fc25d4189d5923b774ee490e67f6c96
Lucas Garron <code@garron.net>
2022-12-11 02:40:37 -0800

    [experiments/stub] Reset stub code.

--- src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
@@ -1,7 +1,2 @@
 // Stub file for testing.
-
-import { Move } from "../../../../cubing/alg";
-
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
-console.log(new Move("R"));
