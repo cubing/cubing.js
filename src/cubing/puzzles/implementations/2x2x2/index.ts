246e52338f4a1e71d2107425280d7764c7097091
Pierce <pierce.tickle@gmail.com>
2023-01-02 20:30:59 -0500

    reformat

--- src/cubing/puzzles/implementations/2x2x2/index.ts
+++ src/cubing/puzzles/implementations/2x2x2/index.ts
@@ -20,9 +20,13 @@ export const cube2x2x2: PuzzleLoader = {
           .cube2x2x2JSON,
       ),
   ),
-  svg: async () => (await import("../dynamic/side-events/puzzles-dynamic-side-events")).cube2x2x2SVG,
-  llSVG: getCached(async () => (
-    await import("../dynamic/side-events/puzzles-dynamic-side-events")).cube2x2x2LLSVG
+  svg: async () => 
+    (await import("../dynamic/side-events/puzzles-dynamic-side-events"))
+      .cube2x2x2SVG,
+  llSVG: getCached(
+    async () => (
+      await import("../dynamic/side-events/puzzles-dynamic-side-events"))
+        .cube2x2x2LLSVG
   ),
   pg: getCached(async () => {
     return asyncGetPuzzleGeometry("2x2x2");
