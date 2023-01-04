cubing/cubing.js

commit 89e3b30145b7ca919e3abe622b61360fae78367a

[twisty] Add an `experimentalFaceletScale` setter.

src/cubing/twisty/views/3D/puzzles/Cube3D.ts
@@ -32,6 +32,7 @@ import {
   hintFaceletStyles,
 } from "../../../model/props/puzzle/display/HintFaceletProp";
 import type { InitialHintFaceletsAnimation } from "../../../model/props/puzzle/display/InitialHintFaceletsAnimationProp";
+import type { FaceletScale } from "../../../model/props/puzzle/display/FaceletScaleProp";
 import { TAU } from "../TAU";
 import { haveStartedSharingRenderers } from "../Twisty3DVantage";
 import type { Twisty3DPuzzle } from "./Twisty3DPuzzle";
@@ -211,7 +212,7 @@ const familyToAxis: { [s: string]: number } = {
 };
 
 const cubieDimensions = {
-  stickerWidth: 0.85,
+  // stickerWidth: 0.85, // Now `faceletScale` in options.
   stickerElevation: 0.503,
   foundationWidth: 1,
   hintStickerElevation: 1.45,
@@ -233,6 +234,7 @@ export interface Cube3DOptions {
   foundationSprite?: Texture | null;
   hintSprite?: Texture | null;
   initialHintFaceletsAnimation?: InitialHintFaceletsAnimation;
+  faceletScale?: "auto" | number;
 }
 
 const cube3DOptionsDefaults: Cube3DOptions = {
@@ -243,7 +245,19 @@ const cube3DOptionsDefaults: Cube3DOptions = {
   foundationSprite: null,
   hintSprite: null,
   initialHintFaceletsAnimation: "auto",
+  faceletScale: "auto",
 };
+
+const DEFAULT_STICKER_SCALE = 0.85;
+function getFaceletScale(options: Cube3DOptions): number {
+  if (
+    typeof options.faceletScale === "undefined" ||
+    options.faceletScale === "auto"
+  ) {
+    return DEFAULT_STICKER_SCALE;
+  }
+  return options.faceletScale;
+}
 
 // TODO: Make internal foundation faces one-sided, facing to the outside of the cube.
 const blackMesh = new MeshBasicMaterial({
@@ -486,7 +500,7 @@ function sharedCubieFoundationGeometry(): BoxGeometry {
 
 function newStickerGeometry(): BufferGeometry {
   const r = new BufferGeometry();
-  const half = 0.5 * cubieDimensions.stickerWidth;
+  const half = 0.5;
   r.setAttribute(
     "position",
     new BufferAttribute(
@@ -607,6 +621,10 @@ export class Cube3D extends Object3D implements Twisty3DPuzzle {
       this.setStickeringMask(this.options.experimentalStickeringMask);
     }
     this.#animateRaiseHintFacelets();
+
+    if (this.options.faceletScale) {
+      this.experimentalSetFaceletScale(this.options.faceletScale);
+    }
   }
 
   #sharedHintStickerGeometryCache: BufferGeometry | null = null;
@@ -765,6 +783,11 @@ export class Cube3D extends Object3D implements Twisty3DPuzzle {
       this.options.experimentalStickeringMask = experimentalStickeringMask;
       this.setStickeringMask(experimentalStickeringMask);
       this.scheduleRenderCallback!(); // TODO
+    }
+
+    const { faceletScale } = options;
+    if (typeof faceletScale !== "undefined") {
+      this.experimentalSetFaceletScale(faceletScale);
     }
   }
 
@@ -999,12 +1022,14 @@ export class Cube3D extends Object3D implements Twisty3DPuzzle {
   }
 
   /** @deprecated */
-  experimentalSetStickerWidth(width: number): void {
+  experimentalSetFaceletScale(faceletScale: FaceletScale): void {
+    this.options.faceletScale = faceletScale;
     for (const orbitInfo of Object.values(this.kpuzzleFaceletInfo)) {
       for (const pieceInfo of orbitInfo) {
         for (const faceletInfo of pieceInfo) {
-          faceletInfo.facelet.scale.setScalar(
-            width / cubieDimensions.stickerWidth,
+          faceletInfo.facelet.scale.setScalar(getFaceletScale(this.options));
+          faceletInfo.hintFacelet?.scale.setScalar(
+            getFaceletScale(this.options),
           );
           // faceletInfo.facelet.setRotationFromAxisAngle(new Vector3(0, 1, 0), 0);
           // faceletInfo.facelet.rotateOnAxis(new Vector3(1, 0, 1), TAU / 6);
@@ -1013,20 +1038,20 @@ export class Cube3D extends Object3D implements Twisty3DPuzzle {
     }
   }
 
-  /** @deprecated */
-  experimentalSetCenterStickerWidth(width: number): void {
-    for (const orbitInfo of [this.kpuzzleFaceletInfo["CENTERS"]]) {
-      for (const pieceInfo of orbitInfo) {
-        for (const faceletInfo of pieceInfo) {
-          faceletInfo.facelet.scale.setScalar(
-            width / cubieDimensions.stickerWidth,
-          );
-          // faceletInfo.facelet.setRotationFromAxisAngle(new Vector3(0, 1, 0), 0);
-          // faceletInfo.facelet.rotateOnAxis(new Vector3(1, 0, 1), TAU / 6);
-        }
-      }
-    }
-  }
+  // /** @deprecated */
+  // experimentalSetCenterStickerWidth(width: number): void {
+  //   for (const orbitInfo of [this.kpuzzleFaceletInfo["CENTERS"]]) {
+  //     for (const pieceInfo of orbitInfo) {
+  //       for (const faceletInfo of pieceInfo) {
+  //         faceletInfo.facelet.scale.setScalar(
+  //           width / getStickerScale(this.options),
+  //         );
+  //         // faceletInfo.facelet.setRotationFromAxisAngle(new Vector3(0, 1, 0), 0);
+  //         // faceletInfo.facelet.rotateOnAxis(new Vector3(1, 0, 1), TAU / 6);
+  //       }
+  //     }
+  //   }
+  // }
 
   private ease(fraction: number): number {
     return smootherStep(fraction);

src/cubing/twisty/heavy-code-imports/dynamic-entries/twisty-dynamic-3d.ts
@@ -1,5 +1,6 @@
 import { cube3x3x3, PuzzleLoader } from "../../../puzzles";
 import type { HintFaceletStyle } from "../../model/props/puzzle/display/HintFaceletProp";
+import type { FaceletScale } from "../../model/props/puzzle/display/FaceletScaleProp";
 import { Cube3D, Cube3DOptions } from "../../views/3D/puzzles/Cube3D";
 import { PG3D } from "../../views/3D/puzzles/PG3D";
 
@@ -22,6 +23,7 @@ export async function pg3dShim(
   renderCallback: () => void,
   puzzleLoader: PuzzleLoader,
   hintFacelets: HintFaceletStyle,
+  faceletScale: FaceletScale,
 ): Promise<PG3D> {
   return new PG3D(
     renderCallback,
@@ -29,5 +31,7 @@ export async function pg3dShim(
     (await puzzleLoader.pg!()).get3d(),
     true,
     hintFacelets === "floating",
+    undefined,
+    faceletScale,
   );
 }

src/cubing/twisty/model/TwistySceneModel.ts
@@ -6,6 +6,7 @@ import { SpriteProp } from "./props/puzzle/display/SpriteProp";
 import { StickeringMaskProp } from "./props/puzzle/display/StickeringMaskProp";
 import { StickeringMaskRequestProp } from "./props/puzzle/display/StickeringMaskRequestProp";
 import { StickeringRequestProp } from "./props/puzzle/display/StickeringRequestProp";
+import { FaceletScaleProp } from "./props/puzzle/display/FaceletScaleProp";
 import { DragInputProp } from "./props/puzzle/state/DragInputProp";
 import { MovePressCancelOptions } from "./props/puzzle/state/MovePressCancelOptions";
 import { MovePressInputProp } from "./props/puzzle/state/MovePressInputProp";
@@ -32,6 +33,7 @@ export class TwistySceneModel {
   // `stickeringMaskRequest` takes priority over `stickeringRequest`
   stickeringMaskRequest = new StickeringMaskRequestProp();
   stickeringRequest = new StickeringRequestProp();
+  faceletScale = new FaceletScaleProp();
 
   // Depth 1
   foundationStickerSprite = new SpriteProp({

src/cubing/twisty/model/props/puzzle/display/FaceletScaleProp.ts
@@ -1,0 +1,8 @@
+import { SimpleTwistyPropSource } from "../../TwistyProp";
+
+export type FaceletScale = "auto" | number;
+export class FaceletScaleProp extends SimpleTwistyPropSource<FaceletScale> {
+  getDefaultValue(): FaceletScale {
+    return "auto";
+  }
+}

src/cubing/twisty/views/3D/Twisty3DPuzzleWrapper.ts
@@ -83,6 +83,17 @@ export class Twisty3DPuzzleWrapper extends EventTarget implements Schedulable {
         this.scheduleRender();
       },
     );
+    this.#freshListenerManager.addListener(
+      this.model.twistySceneModel.faceletScale,
+      async (faceletScale: "auto" | number) => {
+        (
+          (await this.twisty3DPuzzle()) as Cube3D | PG3D
+        ).experimentalUpdateOptions({
+          faceletScale,
+        });
+        this.scheduleRender();
+      },
+    );
 
     this.#freshListenerManager.addMultiListener3(
       [
@@ -149,15 +160,18 @@ export class Twisty3DPuzzleWrapper extends EventTarget implements Schedulable {
           },
         );
       } else {
-        const [hintFacelets, foundationSprite, hintSprite] = await Promise.all([
-          this.model.twistySceneModel.hintFacelet.get(),
-          this.model.twistySceneModel.foundationStickerSprite.get(),
-          this.model.twistySceneModel.hintStickerSprite.get(),
-        ]);
+        const [hintFacelets, foundationSprite, hintSprite, faceletScale] =
+          await Promise.all([
+            this.model.twistySceneModel.hintFacelet.get(),
+            this.model.twistySceneModel.foundationStickerSprite.get(),
+            this.model.twistySceneModel.hintStickerSprite.get(),
+            this.model.twistySceneModel.faceletScale.get(),
+          ]);
         const pg3d = (await proxyPromise).pg3dShim(
           () => this.schedulable.scheduleRender(),
           this.puzzleLoader,
           hintFacelets === "auto" ? "floating" : hintFacelets,
+          faceletScale,
         );
         // TODO: Figure out how to do this in one place using the listener.
         pg3d.then((p) =>

src/cubing/twisty/views/3D/puzzles/PG3D.ts
@@ -529,6 +529,7 @@ export class PG3D extends Object3D implements Twisty3DPuzzle {
     enableFoundationOpt: boolean = false,
     enableHintStickersOpt: boolean = false,
     hintStickerHeightScale: number = 1,
+    private faceletScale: "auto" | number = 1,
     private params: PG3DOptions = {},
   ) {
     super();
@@ -574,7 +575,10 @@ export class PG3D extends Object3D implements Twisty3DPuzzle {
       normals.push(normal(f.coords));
       totArea += polyarea(f.coords);
     }
-    const colorfrac = DEFAULT_COLOR_FRACTION;
+    const colorfrac =
+      faceletScale !== "auto"
+        ? faceletScale * faceletScale
+        : DEFAULT_COLOR_FRACTION;
     let nonDupStickers = 0;
     for (const sticker of stickers) {
       if (!sticker.isDup) {
@@ -1020,6 +1024,7 @@ export class PG3D extends Object3D implements Twisty3DPuzzle {
     hintFacelets?: HintFaceletStyle;
     showFoundation?: boolean; // TODO: better name
     hintStickerOpacity?: number;
+    faceletScale?: "auto" | number;
   }): void {
     if (options.hintFacelets !== undefined) {
       this.showHintFacelets(options.hintFacelets !== "none");
@@ -1033,6 +1038,14 @@ export class PG3D extends Object3D implements Twisty3DPuzzle {
     this.#pendingStickeringUpdate = true;
     if (this.lastPos) {
       this.onPositionChange(this.lastPos);
+    }
+    if (
+      typeof options.faceletScale !== "undefined" &&
+      options.faceletScale !== this.faceletScale
+    ) {
+      console.warn(
+        "Dynamic facelet scale is not yet supported for PG3D. For now, re-create the TwistyPlayer to change the facelet scale.",
+      );
     }
     this.updateMaterialArrays();
     this.scheduleRenderCallback();

src/cubing/twisty/views/TwistyPlayerSettable.ts
@@ -17,6 +17,7 @@ import type { SetupToLocation } from "../model/props/puzzle/state/SetupAnchorPro
 import type { DragInputMode } from "../model/props/puzzle/state/DragInputProp";
 import type { StickeringMask } from "../../puzzles/stickerings/mask";
 import type { InitialHintFaceletsAnimation } from "../model/props/puzzle/display/InitialHintFaceletsAnimationProp";
+import type { FaceletScale } from "../model/props/puzzle/display/FaceletScaleProp";
 
 function err(propName: string): Error {
   return new Error(
@@ -92,6 +93,14 @@ export abstract class TwistyPlayerSettable extends ManagedCustomElement {
   }
   get experimentalStickeringMaskOrbits(): never {
     throw err("experimentalStickeringMaskOrbits");
+  }
+
+  set experimentalFaceletScale(faceletScale: FaceletScale) {
+    this.experimentalModel.twistySceneModel.faceletScale.set(faceletScale);
+  }
+
+  get experimentalFaceletScale(): never {
+    throw err("experimentalFaceletScale");
   }
 
   set backView(backView: BackViewLayoutWithAuto) {

commit d713480e053121e6210edca468ad7e7433f8609b

[twisty] Fix the default facelet scale for `Cube3D` picture cubes.
This fixesa a regression introduced in 89e3b30145b7ca919e3abe622b61360fae78367a.

src/cubing/twisty/views/3D/puzzles/Cube3D.ts
@@ -1012,6 +1012,7 @@ export class Cube3D extends Object3D implements Twisty3DPuzzle {
           : cubieDimensions.hintStickerElevation
         : cubieDimensions.stickerElevation,
     );
+    stickerMesh.scale.setScalar(getFaceletScale(this.options));
     return stickerMesh;
   }
 