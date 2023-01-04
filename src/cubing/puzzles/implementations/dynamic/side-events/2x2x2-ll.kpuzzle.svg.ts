e8d8dc934275c0f6e75242eb600407cd806259e7
Pierce <pierce.tickle@gmail.com>
2023-01-03 22:57:19 -0500

    update widths

--- src/cubing/puzzles/implementations/dynamic/side-events/2x2x2-ll.kpuzzle.svg.ts
+++ src/cubing/puzzles/implementations/dynamic/side-events/2x2x2-ll.kpuzzle.svg.ts
@@ -9,18 +9,18 @@ export const cube2x2x2LLSVG = `<?xml version="1.0" encoding="UTF-8"?>
     </g>
   </defs>
   <g id="2x2x2-LL" stroke="none" stroke-width="4" style="none" stroke-linejoin="round">
-    <rect    id="CORNERS-l0-o0" stroke="#000000" style="fill: white" x="128" y="128" width="88" height="88"></rect>
-    <polygon id="CORNERS-l0-o1" stroke="#000000" style="fill: red" points="216 128 252 128 252 252 216 216"></polygon>
-    <polygon id="CORNERS-l0-o2" stroke="#000000" style="fill: limegreen" transform="translate(206, 238) scale(1, -1) rotate(-90) translate(-206, -238) " points="184 160 220 160 220 284 184 248"></polygon>
-    <rect    id="CORNERS-l1-o0" stroke="#000000" style="fill: white" x="128" y="40" width="88" height="88"></rect>
-    <polygon id="CORNERS-l1-o1" stroke="#000000" style="fill: #26f" transform="translate(206, 18) rotate(-90) translate(-206, -18) " points="184 -60 220 -60 220 64 184 28"></polygon>
-    <polygon id="CORNERS-l1-o2" stroke="#000000" style="fill: red" transform="translate(238, 50) scale(1, -1) translate(-238, -50) " points="216 -28 252 -28 252 96 216 60"></polygon>
-    <rect    id="CORNERS-l2-o0" stroke="#000000" style="fill: white" x="40" y="40" width="88" height="88"></rect>
-    <polygon id="CORNERS-l2-o1" stroke="#000000" style="fill: orange" transform="translate(18, 50) scale(-1, -1) translate(-18, -50) " points="-4 -28 32 -28 32 96 -4 60"></polygon>
-    <polygon id="CORNERS-l2-o2" stroke="#000000" style="fill: #26f" transform="translate(50, 18) scale(1, -1) rotate(90) translate(-50, -18) " points="28 -60 64 -60 64 64 28 28"></polygon>
-    <rect    id="CORNERS-l3-o0" stroke="#000000" style="fill: white" x="40" y="128" width="88" height="88"></rect>
-    <polygon id="CORNERS-l3-o1" stroke="#000000" style="fill: limegreen" transform="translate(50, 238) rotate(90) translate(-50, -238) " points="28 160 64 160 64 284 28 248"></polygon>
-    <polygon id="CORNERS-l3-o2" stroke="#000000" style="fill: orange" transform="translate(18, 206) scale(-1, 1) translate(-18, -206) " points="-4 128 32 128 32 252 -4 216"></polygon>
+    <rect    id="CORNERS-l0-o0" stroke="#000000" style="fill: white" x="128" y="128" width="76" height="76"></rect>
+    <polygon id="CORNERS-l0-o1" stroke="#000000" style="fill: red" points="204 128 252 128 252 252 204 204"></polygon>
+    <polygon id="CORNERS-l0-o2" stroke="#000000" style="fill: limegreen" transform="translate(206, 238) scale(1, -1) rotate(-90) translate(-206, -238) " points="172 160 220 160 220 284 172 236"></polygon>
+    <rect    id="CORNERS-l1-o0" stroke="#000000" style="fill: white" x="128" y="52" width="76" height="76"></rect>
+    <polygon id="CORNERS-l1-o1" stroke="#000000" style="fill: #26f" transform="translate(206, 18) rotate(-90) translate(-206, -18) " points="172 -60 220 -60 220 64 172 16"></polygon>
+    <polygon id="CORNERS-l1-o2" stroke="#000000" style="fill: red" transform="translate(238, 50) scale(1, -1) translate(-238, -50) " points="204 -28 252 -28 252 96 204 48"></polygon>
+    <rect    id="CORNERS-l2-o0" stroke="#000000" style="fill: white" x="52" y="52" width="76" height="76"></rect>
+    <polygon id="CORNERS-l2-o1" stroke="#000000" style="fill: orange" transform="translate(18, 50) scale(-1, -1) translate(-18, -50) " points="-16 -28 32 -28 32 96 -16 48"></polygon>
+    <polygon id="CORNERS-l2-o2" stroke="#000000" style="fill: #26f" transform="translate(50, 18) scale(1, -1) rotate(90) translate(-50, -18) " points="16 -60 64 -60 64 64 16 16"></polygon>
+    <rect    id="CORNERS-l3-o0" stroke="#000000" style="fill: white" x="52" y="128" width="76" height="76"></rect>
+    <polygon id="CORNERS-l3-o1" stroke="#000000" style="fill: limegreen" transform="translate(50, 238) rotate(90) translate(-50, -238) " points="16 160 64 160 64 284 16 236"></polygon>
+    <polygon id="CORNERS-l3-o2" stroke="#000000" style="fill: orange" transform="translate(18, 206) scale(-1, 1) translate(-18, -206) " points="-16 128 32 128 32 252 -16 204"></polygon>
   </g>
   <g style="opacity: 0">
     <use id="CORNERS-l4-o0" xlink:href="#sticker" style="fill: yellow"/>
