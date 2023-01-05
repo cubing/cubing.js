fba5ae966ae5be4bbc2dda849ec6abe28f25568f
Lucas Garron <code@garron.net>
2023-01-04 22:00:55 -0800

    [experiments/stub] Reset stub code.

--- src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
@@ -1,12 +1,2 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
-import { TwistyPlayer } from "../../../../cubing/twisty";
-
-document.body.appendChild(
-  new TwistyPlayer({
-    puzzle: "2x2x2",
-    visualization: "experimental-2D-LL",
-    experimentalStickering: "OLL",
-  }),
-);
