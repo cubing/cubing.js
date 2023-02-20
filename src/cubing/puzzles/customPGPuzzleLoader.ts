af43e23d3b0b5288f092a4d637ce69e6352568e1
Lucas Garron <code@garron.net>
2023-02-19 20:04:44 -0800

    [puzzles] Add a constant for the Kilominx puzzle description.

--- src/cubing/puzzles/customPGPuzzleLoader.ts
+++ src/cubing/puzzles/customPGPuzzleLoader.ts
@@ -14,7 +14,7 @@ export async function descAsyncGetPuzzleGeometry(
     orientCenters: true,
     addRotations: true,
     includeCenterOrbits: false,
-    includeEdgeOrbits: false
+    includeEdgeOrbits: false,
   });
 }
 
--- src/cubing/puzzles/implementations/kilominx/index.ts
+++ src/cubing/puzzles/implementations/kilominx/index.ts
@@ -1,17 +1,18 @@
 import type { PuzzleLoader } from "../..";
-import type { Move } from "../../../alg";
-import { KPuzzle, KTransformationData } from "../../../kpuzzle";
-import type { ExperimentalPGNotation } from "../../../puzzle-geometry";
-import { asyncGetPuzzleGeometry } from "../../async/async-pg3d";
 import { getCached } from "../../async/lazy-cached";
-import { asyncGetKPuzzle, descAsyncGetPuzzleGeometry } from "../../customPGPuzzleLoader";
+import {
+  asyncGetKPuzzle,
+  descAsyncGetPuzzleGeometry,
+} from "../../customPGPuzzleLoader";
+
+const KILOMINX_PUZZLE_DESCRIPTION = "d f 0.56";
 
 // TODO: Make this consistent with Megaminx corners
 export const kilominx: PuzzleLoader = {
   id: "kilominx",
   fullName: "Kilominx",
-  kpuzzle: getCached(() => asyncGetKPuzzle("d f 0.56")),
-  pg: () => descAsyncGetPuzzleGeometry("d f 0.56"),
+  kpuzzle: getCached(() => asyncGetKPuzzle(KILOMINX_PUZZLE_DESCRIPTION)),
+  pg: () => descAsyncGetPuzzleGeometry(KILOMINX_PUZZLE_DESCRIPTION),
   svg: getCached(async () => {
     return (
       await import("../dynamic/unofficial/puzzles-dynamic-unofficial")
