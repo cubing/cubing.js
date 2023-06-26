ab0fc72cda02095708097edece670d339975af49
Lucas Garron <code@garron.net>
2023-06-26 01:35:20 -0700

    [puzzles] Fix `z` for clock.
    Fixes https://github.com/cubing/cubing.js/issues/281

--- src/cubing/puzzles/implementations/dynamic/side-events/clock.kpuzzle.json.ts
+++ src/cubing/puzzles/implementations/dynamic/side-events/clock.kpuzzle.json.ts
@@ -32,33 +32,6 @@ export const clockJSON: KPuzzleDefinition = {
     },
   },
   moves: {
-    UR_PLUS_: {
-      DIALS: {
-        permutation: p18,
-        orientation: [0, 1, 1, 0, 1, 1, 0, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0],
-      },
-      FACES: t18,
-      FRAME: { permutation: [0], orientation: [0] },
-      HOUR_MARKS: t18,
-    },
-    DR_PLUS_: {
-      DIALS: {
-        permutation: p18,
-        orientation: [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 11, 0, 0],
-      },
-      FACES: t18,
-      FRAME: { permutation: [0], orientation: [0] },
-      HOUR_MARKS: t18,
-    },
-    DL_PLUS_: {
-      DIALS: {
-        permutation: p18,
-        orientation: [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11],
-      },
-      FACES: t18,
-      FRAME: { permutation: [0], orientation: [0] },
-      HOUR_MARKS: t18,
-    },
     UL_PLUS_: {
       DIALS: {
         permutation: p18,
@@ -72,33 +45,6 @@ export const clockJSON: KPuzzleDefinition = {
       DIALS: {
         permutation: p18,
         orientation: [1, 1, 1, 1, 1, 1, 0, 0, 0, 11, 0, 11, 0, 0, 0, 0, 0, 0],
-      },
-      FACES: t18,
-      FRAME: { permutation: [0], orientation: [0] },
-      HOUR_MARKS: t18,
-    },
-    R_PLUS_: {
-      DIALS: {
-        permutation: p18,
-        orientation: [0, 1, 1, 0, 1, 1, 0, 1, 1, 11, 0, 0, 0, 0, 0, 11, 0, 0],
-      },
-      FACES: t18,
-      FRAME: { permutation: [0], orientation: [0] },
-      HOUR_MARKS: t18,
-    },
-    D_PLUS_: {
-      DIALS: {
-        permutation: p18,
-        orientation: [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 11, 0, 11],
-      },
-      FACES: t18,
-      FRAME: { permutation: [0], orientation: [0] },
-      HOUR_MARKS: t18,
-    },
-    L_PLUS_: {
-      DIALS: {
-        permutation: p18,
-        orientation: [1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 11, 0, 0, 0, 0, 0, 11],
       },
       FACES: t18,
       FRAME: { permutation: [0], orientation: [0] },
@@ -137,35 +83,41 @@ export const clockJSON: KPuzzleDefinition = {
     z: {
       DIALS: {
         permutation: [
-          6, 3, 0, 7, 4, 1, 8, 5, 2, 17, 14, 11, 16, 13, 10, 15, 12, 9,
+          6, 3, 0, 7, 4, 1, 8, 5, 2, 11, 14, 17, 10, 13, 16, 9, 12, 15,
         ],
-        orientation: new Array(18).fill(3),
+        orientation: [
+          3, 3, 3, 3, 3, 3, 3, 3, 3, -3, -3, -3, -3, -3, -3, -3, -3, -3,
+        ],
       },
       FACES: {
         permutation: [
-          6, 3, 0, 7, 4, 1, 8, 5, 2, 17, 14, 11, 16, 13, 10, 15, 12, 9,
+          6, 3, 0, 7, 4, 1, 8, 5, 2, 11, 14, 17, 10, 13, 16, 9, 12, 15,
         ],
         orientation: o18,
       },
       FRAME: { permutation: [0], orientation: [0] },
       HOUR_MARKS: {
         permutation: [
-          6, 3, 0, 7, 4, 1, 8, 5, 2, 17, 14, 11, 16, 13, 10, 15, 12, 9,
+          6, 3, 0, 7, 4, 1, 8, 5, 2, 11, 14, 17, 10, 13, 16, 9, 12, 15,
         ],
-        orientation: new Array(18).fill(1),
+        orientation: [
+          1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        ],
       },
     },
     // TODO: define this as `z2 y2`
     x2: {
       DIALS: {
         permutation: [
-          17, 16, 15, 14, 13, 12, 11, 10, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+          17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
         ],
-        orientation: [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        orientation: [
+          6, 6, 6, 6, 6, 6, 6, 6, 6, -6, -6, -6, -6, -6, -6, -6, -6, -6,
+        ],
       },
       FACES: {
         permutation: [
-          17, 16, 15, 14, 13, 12, 11, 10, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+          17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
         ],
         orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       },
@@ -175,13 +127,23 @@ export const clockJSON: KPuzzleDefinition = {
       },
       HOUR_MARKS: {
         permutation: [
-          17, 16, 15, 14, 13, 12, 11, 10, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+          17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
         ],
-        orientation: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        orientation: [
+          2, 2, 2, 2, 2, 2, 2, 2, 2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+        ],
       },
     },
   },
   experimentalDerivedMoves: {
+    UR_PLUS_: "[z': UL_PLUS_]",
+    DR_PLUS_: "[z2: UL_PLUS_]",
+    DL_PLUS_: "[z: UL_PLUS_]",
+
+    R_PLUS_: "[z': U_PLUS_]",
+    D_PLUS_: "[z2: U_PLUS_]",
+    L_PLUS_: "[z: U_PLUS_]",
+
     F_PLUS_: "ALL_PLUS_",
 
     // x2: "y2 z2", // TODO(https://github.com/cubing/cubing.js/issues/279)
