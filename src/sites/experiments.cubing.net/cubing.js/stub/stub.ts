e8a54348eb839d4d944f8751e37b85c9790d4204
Lucas Garron <code@garron.net>
2023-07-18 01:54:40 -0700

    [experiments/stub] Reset stub code.

--- src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
@@ -1,17 +1,2 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
-import { cube3x3x3 } from "../../../../cubing/puzzles";
-
-(async () => {
-  const state = (await cube3x3x3.kpuzzle()).startState();
-  console.log(state.stateData);
-  // state.applyAlg("U");
-  // console.log(state.stateData.CENTERS);
-  // state.applyAlg("U");
-  // console.log(state.stateData.CENTERS);
-  // state.applyAlg("U");
-  // console.log(state.stateData.CENTERS);
-  // state.applyAlg("z'");
-  // console.log(state.stateData.CENTERS);
-})();
