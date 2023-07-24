4e5a42b87ede0709ae5a647b237c6f5492a1f601
Lucas Garron <code@garron.net>
2023-07-24 01:56:31 -0700

    [experiments/stub] Reset stub code.

--- src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
+++ src/sites/experiments.cubing.net/cubing.js/stub/stub.ts
@@ -1,11 +1,2 @@
 // Stub file for testing.
 // Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
-import { randomScrambleForEvent } from "../../../../cubing/scramble";
-import { setSearchDebug } from "../../../../cubing/search";
-
-setSearchDebug({ showWorkerInstantiationWarnings: false });
-
-(async () => {
-  console.log((await randomScrambleForEvent("333")).toString());
-})();
