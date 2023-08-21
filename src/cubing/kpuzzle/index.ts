16c6c0493ec352f6ecc1b1bb34e10093ffb0b9a0
Lucas Garron <code@garron.net>
2023-08-21 02:43:44 -0700

    [kpuzzle] Export `KStateOrbitData` and `KTransformationOrbitData`.

--- src/cubing/kpuzzle/index.ts
+++ src/cubing/kpuzzle/index.ts
@@ -2,7 +2,9 @@ export { KPuzzle } from "./KPuzzle";
 export type {
   KPuzzleDefinition,
   KStateData,
+  KStateOrbitData,
   KTransformationData,
+  KTransformationOrbitData,
 } from "./KPuzzleDefinition";
 export { KState } from "./KState";
 export { KTransformation } from "./KTransformation";
