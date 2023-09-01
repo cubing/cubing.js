d5ec719e7da433a037c23b371e93aaf6e8694e84
Lucas Garron <code@garron.net>
2023-08-31 17:43:15 -0700

    Update docs and try to point people to the right place from a few choice packages.

---
+++ scramble/index.d.ts
@@ -0,0 +1,33 @@
+/*
+If you have come across this file, it may be because your project is not
+configured to resolve `"exports"` in `package.json` for dependencies. Consider
+using the following settings in a `tsconfig.json` file for your project:
+
+```
+{
+  "compilerOptions": {
+    "lib": ["es2020", "dom"],
+    "module": "es2020",
+    "moduleResolution": "bundler"
+  }
+}
+```
+
+For more details, see: https://js.cubing.net/cubing/index.html#javascript
+*/
+
+/** @deprecated If you are seeing this definition, you are not using the correct types for `cubing.js`. Consider
+using the following settings in a `tsconfig.json` file for your project:
+
+```
+{
+  "compilerOptions": {
+    "lib": ["es2020", "dom"],
+    "module": "es2020",
+    "moduleResolution": "bundler"
+  }
+}
+```
+
+For more details, see: https://js.cubing.net/cubing/index.html#javascript */
+export type randomScrambleForEvent = {};
