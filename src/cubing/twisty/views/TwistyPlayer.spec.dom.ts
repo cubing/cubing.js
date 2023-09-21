f60c36cef4908b85efb06aba59c0c666eab9f7bb
Lucas Garron <code@garron.net>
2023-09-20 21:07:44 -0700

    [twisty] Add basic `TwistyPlayer` constructor tests.

---
+++ src/cubing/twisty/views/TwistyPlayer.spec.dom.ts
@@ -0,0 +1,45 @@
+import { Alg } from "../../alg";
+import { TwistyPlayer } from "..";
+import { expect } from "../../../test/chai-workarounds";
+
+const test = it;
+
+test("can construct TwistyPlayer via constructor with default config", () => {
+  new TwistyPlayer();
+});
+
+test("can construct TwistyPlayer via constructor with empty config", () => {
+  new TwistyPlayer({});
+});
+
+test("can construct TwistyPlayer via constructor with fancy config", async () => {
+  // Example from https://js.cubing.net/cubing/twisty/#parameters
+  expect(
+    await new TwistyPlayer({
+      puzzle: "4x4x4",
+      alg: "r U2 x r U2 r U2 r' U2 l U2 r' U2 r U2 r' U2 r'",
+      hintFacelets: "none",
+      backView: "top-right",
+      background: "none",
+    }).experimentalModel.puzzleID.get(),
+  ).to.equal("4x4x4");
+});
+
+test("can construct TwistyPlayer with various algs", () => {
+  new TwistyPlayer({ alg: "R U R'" });
+  new TwistyPlayer({ alg: new Alg("(R y)1260") });
+});
+
+test("can construct TwistyPlayer using `document.createElement(…)` and set attributes", async () => {
+  const player = document.createElement("twisty-player");
+  player.alg = "R U R' U R U2' R'";
+  expect(
+    (await player.experimentalModel.detailedTimelineInfo.get()).timeRange,
+  ).to.deep.equal({ start: 0, end: 7500 });
+});
+
+test("can add a TwistyPlayer to the DOM", () => {
+  document.body.appendChild(new TwistyPlayer());
+});
+
+// TODO: test changing 2D and 3D (and check for some appropriate internal updates to the player)
