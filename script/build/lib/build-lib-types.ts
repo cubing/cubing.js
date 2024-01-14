7f642bed4309ff49c1de359826714504b5bead97
Lucas Garron <code@garron.net>
2024-01-14 04:00:34 -0800

    .

--- script/build/lib/build-lib-types.ts
+++ script/build/lib/build-lib-types.ts
@@ -1,14 +1,34 @@
+import { rename } from "fs/promises";
 import { spawnPromise } from "../../lib/execPromise";
-import { packageEntryPoints } from "../common/package-info";
+import { packageNames } from "../common/package-info";
+
+const UNBUNDLED_TYPES_ROOT = "./.temp/unbundled-types/src/cubing";
 
 console.warn(`
 ⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳
 Note: The \`types\` target is slow. Expect several seconds.
 ⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳⏳
 `);
+await spawnPromise("rm", ["-rf", "./.temp/unbundled-types"]);
+
+await spawnPromise("npx", [
+  "tsc",
+  "--project",
+  "./unbundled-types.tsconfig.json",
+]);
+
+for (const packageName of packageNames) {
+  rename(
+    `${UNBUNDLED_TYPES_ROOT}/${packageName}/index.d.ts`,
+    `${UNBUNDLED_TYPES_ROOT}/${packageName}/index.ts`,
+  );
+}
+
 await spawnPromise("npx", [
   "tsup",
-  ...packageEntryPoints,
+  ...packageNames.map(
+    (packageName) => `${UNBUNDLED_TYPES_ROOT}/${packageName}/index.ts`,
+  ),
   "--dts-only",
   "--format",
   "esm",
