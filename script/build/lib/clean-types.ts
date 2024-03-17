180a8447d7cfa3d0c513aea4abd5d8872b146d5a
Lucas Garron <code@garron.net>
2024-03-17 03:42:52 -0700

    [clean] Avoid importing the entry point for `build-lib-types` from `clean-types`.

--- script/build/lib/clean-types.ts
+++ script/build/lib/clean-types.ts
@@ -2,7 +2,8 @@ import { existsSync } from "node:fs";
 import { rm, rmdir } from "node:fs/promises";
 import { join } from "node:path";
 import { packageNames } from "../common/package-info";
-import { TYPESCRIPT_DECLARATION_INDEX } from "./build-lib-types";
+
+export const TYPESCRIPT_DECLARATION_INDEX = "index.d.ts";
 
 for (const packageName of packageNames) {
   const indexFileName = join(packageName, TYPESCRIPT_DECLARATION_INDEX);
