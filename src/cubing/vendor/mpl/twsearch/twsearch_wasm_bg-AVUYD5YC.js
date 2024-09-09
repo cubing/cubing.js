1f8773883863cc44bf2ae8f56c657ef761f25350
Lucas Garron <code@garron.net>
2024-09-09 15:09:46 -0700

    [vendor/twsearch] Fix 5x5x5 scrambles.
    See https://github.com/cubing/twsearch/issues/57

--- src/cubing/vendor/mpl/twsearch/twsearch_wasm_bg-AVUYD5YC.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf3a839ad170d3f700deef548daedb055fd3dab7ec4f42e18b5f819576257d96
-size 412751
---
+++ src/cubing/vendor/mpl/twsearch/twsearch_wasm_bg-IWATKA5H.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bfb459f950d874289a6d7eea8b0f004a7d6c0925016b614dbc9241048931ff2
+size 412755
