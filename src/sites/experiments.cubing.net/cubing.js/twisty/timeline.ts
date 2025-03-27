b3a4598483a456f3307bbdd47765978aad8cc8c7
Lucas Garron <code@garron.net>
2025-03-26 18:16:45 -0700

    [experiments/timeline] Add a real-time solve.
    Thanks to @bohdancho for the data!

--- src/sites/experiments.cubing.net/cubing.js/twisty/timeline.ts
+++ src/sites/experiments.cubing.net/cubing.js/twisty/timeline.ts
@@ -1,4 +1,4 @@
-import { Alg } from "../../../../cubing/alg";
+import { Alg, Move } from "../../../../cubing/alg";
 import { TwistyAlgViewer, TwistyPlayer } from "../../../../cubing/twisty";
 
 {
@@ -19,5 +19,55 @@ import { TwistyAlgViewer, TwistyPlayer } from "../../../../cubing/twisty";
   document.querySelector(".demo1")!.appendChild(player);
   document
     .querySelector(".demo1")!
+    .appendChild(new TwistyAlgViewer({ twistyPlayer: player }));
+}
+
+{
+  const solutionString = `x' x' y' y U' L' D' F' D' F // Cross (0.572s)
+y' L U' L' // F2L Slot 1 (0.536s)
+y' U' y' U U' R' U' R // F2L Slot 2 (adjacent) (1.289s)
+U U L U' L' // F2L Slot 3 (0.381s)
+y' U R U' R' F' U' F // EOLL (0.991s)
+U U R' F' L F R F' L' F // OLL (1.623s)
+U U // PLL (0.240s)`;
+  const solutionMoveTimestamps = [
+    0, 0, 0, 0, 0, 47, 196, 284, 397, 572, 870, 959, 1034, 1108, 1226, 1518,
+    1693, 1796, 2142, 2249, 2303, 2397, 2416, 2542, 2614, 2682, 2778, 2919,
+    3370, 3454, 3518, 3577, 3667, 3718, 3769, 3912, 4671, 4775, 4843, 4996,
+    5068, 5122, 5187, 5337, 5392, 5526, 5632,
+  ];
+  const duration = solutionMoveTimestamps.at(-1);
+  const parsedAlg = Alg.fromString(solutionString);
+
+  // `.filter(…)` on iterators is not generally available yet: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Iterator/filter
+  const moves = Array.from(parsedAlg.childAlgNodes()).filter((algNode) =>
+    algNode.is(Move),
+  );
+  if (moves.length !== solutionMoveTimestamps.length) {
+    throw new Error("Unexpected mismatch");
+  }
+
+  const animationTimelineLeaves = moves.map((move, i) => {
+    return {
+      animLeaf: move,
+      start:
+        ((solutionMoveTimestamps[i] ?? 0) + solutionMoveTimestamps[i - 1]) / 2,
+      end:
+        (solutionMoveTimestamps[i] +
+          (solutionMoveTimestamps[i + 1] ?? duration)) /
+        2,
+    };
+  });
+
+  const player = new TwistyPlayer({
+    alg: parsedAlg,
+    experimentalSetupAnchor: "end",
+  });
+  player.experimentalModel.animationTimelineLeavesRequest.set(
+    animationTimelineLeaves,
+  );
+  document.querySelector(".demo2")!.appendChild(player);
+  document
+    .querySelector(".demo2")!
     .appendChild(new TwistyAlgViewer({ twistyPlayer: player }));
 }
