bc44903d89f34b2e5168ef14abeae499b196ba4e
Lucas Garron <code@garron.net>
2025-04-04 03:38:43 -0700

    [puzzles] Add a 4x4x4 key mapping.

--- src/cubing/puzzles/implementations/4x4x4/index.ts
+++ src/cubing/puzzles/implementations/4x4x4/index.ts
@@ -1,6 +1,7 @@
 import { CubePGPuzzleLoader } from "../../async/async-pg3d";
 import { getCached } from "../../async/lazy-cached";
 import type { PuzzleLoader } from "../../PuzzleLoader";
+import { cube4x4x4KeyMapping } from "./cube4x4x4KeyMapping";
 
 const cube4x4x4: PuzzleLoader = new CubePGPuzzleLoader({
   id: "4x4x4",
@@ -11,5 +12,6 @@ cube4x4x4.llSVG = getCached(async () => {
   return (await import("../dynamic/4x4x4/puzzles-dynamic-4x4x4"))
     .cube4x4x4LLSVG;
 });
+cube4x4x4.keyMapping = async () => cube4x4x4KeyMapping; // TODO: async loading
 
 export { cube4x4x4 };
