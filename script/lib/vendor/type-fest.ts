72e202f1f2e940b41676ff4027b46f2ef008bbab
Lucas Garron <code@garron.net>
2025-12-19 23:56:51 -0800

    `make format`

--- script/lib/vendor/type-fest.ts
+++ script/lib/vendor/type-fest.ts
@@ -1,8 +1,7 @@
 // From https://github.com/sindresorhus/type-fest/blob/main/source/iterable-element.d.ts
-export type IterableElement<TargetIterable> = TargetIterable extends Iterable<
-  infer ElementType
->
-  ? ElementType
-  : TargetIterable extends AsyncIterable<infer ElementType>
+export type IterableElement<TargetIterable> =
+  TargetIterable extends Iterable<infer ElementType>
     ? ElementType
-    : never;
+    : TargetIterable extends AsyncIterable<infer ElementType>
+      ? ElementType
+      : never;
