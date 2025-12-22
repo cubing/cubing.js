9bb0810b20754ce63ed80e47cee094a9dfa36d7f
Lucas Garron <code@garron.net>
2025-12-21 23:37:26 -0800

    [cubing/puzzles] Mark the Kilominx `x2` test as slow.

--- src/cubing/puzzles/implementations/kilominx/kilominx.test.ts
+++ src/cubing/puzzles/implementations/kilominx/kilominx.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from "bun:test";
-
+import { SKIP_SLOW_TESTS } from "../../../../test/SKIP_SLOW_TESTS";
 import { kilominx } from "./";
 
-test("Kilominx can apply x2", async () => {
+test.skipIf(SKIP_SLOW_TESTS)("Kilominx can apply x2", async () => {
   const kpuzzle = await kilominx.kpuzzle();
   expect(
     kpuzzle.algToTransformation("x2").isIdentityTransformation(),
