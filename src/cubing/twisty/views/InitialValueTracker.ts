ba621c8c1fae6abb9fe0524942f95a20814832a9
Lucas Garron <code@garron.net>
2026-01-10 18:21:33 -0800

    Remove usage of `Promise.withResolvers()`.
    See https://github.com/cubing/cubing.js/issues/409
    
    This is now tested indirectly through transitive imports via `src/bin` type checking.

--- src/cubing/twisty/views/InitialValueTracker.ts
+++ src/cubing/twisty/views/InitialValueTracker.ts
@@ -1,12 +1,14 @@
 export class InitialValueTracker<T> {
+  // @ts-expect-error: We do initialize this synchronously (assuming no one has tampered with the `Promise` constructor).
   #resolve: (t: T) => void;
+  // @ts-expect-error: We do initialize this synchronously (assuming no one has tampered with the `Promise` constructor).
   reject: (e?: Error) => void; // TODO: AbortController?
   promise: Promise<T>;
   constructor() {
-    const { promise, resolve, reject } = Promise.withResolvers<T>();
-    this.promise = promise;
-    this.#resolve = resolve;
-    this.reject = reject;
+    this.promise = new Promise((resolve, reject) => {
+      this.#resolve = resolve;
+      this.reject = reject;
+    });
   }
   handleNewValue(t: T): void {
     this.#resolve(t);
