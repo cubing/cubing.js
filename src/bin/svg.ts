0f81b0b943ab72b018bc212b41deb47a22e4ac74
Lucas Garron <code@garron.net>
2026-03-02 18:02:43 -0800

    [bin/svg] Add `svg` bin.

---
+++ src/bin/svg.ts
@@ -0,0 +1,57 @@
+/**
+
+To run this file directly:
+
+```shell
+bun run -- ./src/bin/svg.ts <program args>
+```
+
+To test completions:
+
+```shell
+# fish (from repo root)
+set PATH (pwd)/src/test/bin-path $PATH
+svg --completions fish | source
+```
+
+```shell
+# zsh (from repo root)
+autoload -Uz compinit
+compinit
+export PATH=$(pwd)/src/test/bin-path:$PATH
+source <(svg --completions zsh)
+```
+
+*/
+
+import { basename } from "node:path";
+import { argv } from "node:process";
+import { argument, choice, object } from "@optique/core";
+import { run } from "@optique/run";
+import { puzzles } from "cubing/puzzles";
+import { packageVersion } from "../metadata/packageVersion";
+
+const args = run(
+  object({
+    puzzleID: argument(choice(Object.keys(puzzles))),
+  }),
+  {
+    programName: basename(argv[1]),
+    help: "option",
+    completion: {
+      mode: "option",
+      name: "plural",
+    },
+    version: {
+      mode: "option",
+      value: packageVersion,
+    },
+  },
+);
+
+const puzzleLoader = puzzles[args.puzzleID];
+if (!puzzleLoader) {
+  throw new Error(`Invalid puzzle ID: ${args.puzzleID}`);
+}
+
+console.log(await puzzleLoader.svg());
