e061bc9be290e1747db558e851a5aed18180d26d
Lucas Garron <code@garron.net>
2026-05-22 20:58:56 -0700

    [fix-web-bluetooth-reference.ts] Rename triple slash directive var name and add a comment.

--- script/build/types/fix-web-bluetooth-reference.ts
+++ script/build/types/fix-web-bluetooth-reference.ts
@@ -2,7 +2,10 @@ import { Path } from "path-class";
 
 const OUTPUT_PATH = new Path("./dist/lib/cubing/bluetooth/index.d.ts");
 
-const prefix = `/// <reference types="web-bluetooth" />
+// We include this at the top of the source for the `cubing/bluetooth` entry
+// point, but it's stripped by `tsdown`. So we have to restore it manually. 🤷
+// TODO: test that the built `.d.ts` files pass the type checker again, I guess? 🤷
+const RESTORED_WEB_BLUETOOTH_REFERENCE = `/// <reference types="web-bluetooth" />
 
 `;
 
@@ -12,11 +15,11 @@ if (!(await OUTPUT_PATH.exists())) {
 }
 
 const currentContents = await OUTPUT_PATH.readText();
-if (currentContents.startsWith(prefix)) {
+if (currentContents.startsWith(RESTORED_WEB_BLUETOOTH_REFERENCE)) {
   console.info(`✅ OK \`web-bluetooth\` path reference prefix: ${OUTPUT_PATH}`);
 } else {
   console.info(
     `✍️ Updating \`web-bluetooth\` path reference prefix: ${OUTPUT_PATH}`,
   );
-  OUTPUT_PATH.write(prefix + currentContents);
+  OUTPUT_PATH.write(RESTORED_WEB_BLUETOOTH_REFERENCE + currentContents);
 }
